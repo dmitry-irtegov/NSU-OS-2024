8579d218c338233be80feeb912b71e1008293104
ArtemChepenkov <achepenkov05@mail.ru>
2024-09-09 14:07:00 +0700

    complete task

---
+++ lab2/main.c
@@ -0,0 +1,20 @@
+﻿#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+extern char* tzname[];
+
+void main(){
+    putenv("TZ=PST8PDT");
+    time_t now;
+    struct tm* sp;
+
+    (void)time(&now);
+    printf("%s", ctime(&now));
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+    exit(0);
+}
