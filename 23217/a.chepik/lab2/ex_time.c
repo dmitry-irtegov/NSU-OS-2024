3b6ae569b04c03a380c960cd7a201b004d4a8710
Andrew_Vetrov <user120715mts@gmail.com>
2024-09-10 10:25:37 +0300

    Good

--- 23217/a.chepik/lab2/ex_time.c
+++ 23217/a.chepik/lab2/ex_time.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <sys/types.h>
 #include <stdio.h>
 #include <time.h>
@@ -10,16 +11,43 @@ main()
 	time_t now;
 	struct tm* sp;
 	
-	(void)time(&now);
-
-	putenv("TZ=PST8");
+	time_t for_err = time(&now);
 	
-	printf("%s", ctime(&now));
+	if (for_err == -1) {
+		printf("time() error: -1\n");
+		exit(-1);
+	}
+
+	int res_putenv = putenv("TZ=America/Los_Angeles");
+
+	if (res_putenv != 0) {
+		printf("putenv() error: %d\n", res_putenv);
+		exit(-1);
+	}
+
+	char* time_ctime = NULL;
+	time_ctime = ctime(&now);
+
+	if (time_ctime == NULL) {
+		printf("ctime() error: NULL\n");
+		exit(-1);
+	}
+
+	else
+		printf("%s", time_ctime);
 	
 	sp = localtime(&now);
-	printf("%d/%d/%02d %d:%02d %s\n",
-		sp->tm_mon + 1, sp->tm_mday,
-		sp->tm_year + 1900, sp->tm_hour,
-		sp->tm_min, tzname[sp->tm_isdst]);
+
+	if (sp == NULL) {
+		printf("localtime() error: NULL\n");
+		exit(-1);
+	}
+
+	else {
+		printf("%d/%d/%02d %d:%02d %s\n",
+			sp->tm_mon + 1, sp->tm_mday,
+			sp->tm_year + 1900, sp->tm_hour,
+			sp->tm_min, tzname[sp->tm_isdst]);
+	}
 	exit(0);
 }
