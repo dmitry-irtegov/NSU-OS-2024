e4be8e1378fbe309e540f8f63c75de4b2128ff74
BarDirus <59092357+BarDirus@users.noreply.github.com>
2024-09-16 11:57:42 +0700

    1

--- lab2/main.c
+++ lab2/main.c
@@ -11,11 +11,8 @@ int main()
     struct tm *sp;
     setenv("TZ", "PST8PDT", 1);
     tzset();
-
     (void) time(&now);
-
     printf("%s", ctime(&now));
-
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, sp->tm_mday,
