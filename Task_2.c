722f81c8ff3bcb85bca697f475bee6476ff9dbee
PavelBun <p.bun@g.nsu.ru>
2024-09-17 15:41:39 +0700

    update of errors

--- Task_2.c
+++ Task_2.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 
 int main(){
-    putenv("TZ=America/Los_Angeles");
+    if(putenv("TZ=America/Los_Angeles") != 0){
+        perror("error changing environment variable");
+        return 1;
+    }
     tzset();
     time_t now = time(NULL);
-    struct tm * timeCalifornia = localtime(&now);
-    printf("date in Californa: %02d.%02d.%04d, time in Californa: %02d:%02d:%02d/n", timeCalifornia->tm_mday, timeCalifornia->tm_mon + 1, timeCalifornia->tm_year + 1900, 
-    timeCalifornia->tm_hour, timeCalifornia->tm_min, timeCalifornia->tm_sec);
+    char * outputTime = ctime(&now);    
+    printf("%s", outputTime);
 }
