0763166a700e642112ed17740cd9a2e8787c432c
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2024-09-17 19:39:22 +0700

    lab3

--- 23216/Faranosov/t3/t3.c
+++ 23216/Faranosov/t3/t3.c
@@ -6,53 +6,35 @@
 
 void check_file() {
 
+    real = getuid();
+    eff = geteuid();
+
+    if (real == -1) {
+        perror("getuid error:");
+        return 1;
+    }
+
+    if (eff) == -1) {
+        perror("geteuid error:");
+        return 1;
+    }
+
+    printf("Real ID = %d\n", real);
+    printf("Effective ID = %d\n\n", eff);
+
     FILE* fp = NULL;
     if (fopen("text3.txt", "r") == NULL) {
         perror("File open error");
-    }
-    else {
-        printf("First open succeseful\n");
     }
 
     if (fp != NULL) fclose(fp);
 }
 
 int main(){
-    uid_t real = getuid();
-    uid_t eff = geteuid();
     
-    if (real == -1) {
-        perror("getuid1 error:");
-        return 1;
-    }
-
-    if (eff) == -1) {
-        perror("geteuid1 error:");
-        return 1;
-    }
-
-    printf("Real ID = %d\n", real);
-    printf("Effective ID = %d\n\n", eff);   
-
     check_file();
 
-    setuid(real);
-
-    real = getuid();
-    eff = geteuid();
-
-    if (real == -1) {
-        perror("getuid2 error:");
-        return 1;
-    }
-
-    if (eff) == -1) {
-        perror("geteuid2 error:");
-        return 1;
-    }
-
-    printf("Real ID = %d\n", real);
-    printf("Effective ID = %d\n\n", eff);   
+    setuid(real); 
 
     check_file();
 
