e2512f437a88aef2b46ca242f0bc31ee4e65aa06
PavelBun <p.bun@g.nsu.ru>
2024-09-17 17:50:32 +0700

    added error handling

--- Task_2.c
+++ Task_2.c
@@ -7,10 +7,18 @@ int main(){
         perror("error changing environment variable.");
         return 1;
     }
-    
+
     tzset();
     time_t now = time(NULL);
-    char * outputTime = ctime(&now);    
+    if (now == -1) {
+        perror("Error getting time");
+        return 1;
+    }
+    char * outputTime = ctime(&now);  
+     if (outputTime == NULL) {
+        perror("Error converting time");
+        return 1;
+    }  
     printf("%s", outputTime);
-
+    return 0;
 }
