468531e4a8fd16efba7750f23a20804cf138d2d4
Tomilov Timofey <t.tomilov@g.nsu.ru>
2024-09-17 15:24:28 +0700

    lab2

--- 23216/t.tomilov/lab2/lab2.c
+++ 23216/t.tomilov/lab2/lab2.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main() {
     if (putenv("TZ=America/Los_Angeles") == -1){
-        printf("Error: Couldn't get the environment variable!\n");
+        printf("Error: Couldn't get the environment variable!\n%d\n", errno);
         exit(-1);
     }
     time_t now;
     struct tm *sp;
-    if (time( &now ) == NULL){
-        printf("Error: Failed to get the system time!\n");
+    if (time(&now) == NULL){
+        printf("Error: Failed to get the system time!\n%d\n", errno);
         exit(-1);
     }
-    printf("%s", ctime( &now ) );
+    printf("%s", ctime(&now));
     exit(0);
 }
