916c82c9c370db16da6c8802f185c22d90d89f43
MCBivis <Artemiybox@mail.ru>
2024-09-17 15:06:28 +0700

    change

--- 23216/a.kuleshov/Lab_2/ex_time.c
+++ 23216/a.kuleshov/Lab_2/ex_time.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-extern char *tzname[];
 
-main()
-{
-    if (putenv("TZ=PST8PDT")==0) {
-         printf("Time change success\n");
+main() {
+    if (putenv("TZ=America/Los_Angeles") != 0) {
+        perror("Time change error");
+        exit(1);
     }
 
     time_t now;
-    struct tm *sp;
 
-    (void) time( &now );
+    if (time(&now) == -1) {
+        perror("Function time error");
+        exit(1);
+    }
 
-    printf("%s", ctime( &now ) );
+    if (ctime(&now) == NULL) {
+        perror("Function ctime error");
+        exit(1);
+    }
 
-    sp = localtime(&now);
-    printf("%02d/%02d/%02d %d:%02d %s\n",
-        sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year + 1900, sp->tm_hour,
-        sp->tm_min, tzname[sp->tm_isdst]);
+    printf("%s", ctime(&now));
+
     exit(0);
 }
