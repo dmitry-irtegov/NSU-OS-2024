618efda1d2945809d9c6a541f0a789b9d9a9727d
Fedor Pivovarov <f3d0rp1v0v4r0v@yandex.ru>
2024-09-17 15:06:30 +0700

    refactor lab2

--- 23216/f.pivovarov/lab2/lab2.c
+++ 23216/f.pivovarov/lab2/lab2.c
@@ -3,11 +3,8 @@
 #include <time.h>
 #include <stdlib.h>
 
-extern char *tzname[];
-
 void main() {
     time_t now;
-    struct tm *sp;
 
     /* As follows from man, timezone getting from
     TZ environment variable by tzset() function.
@@ -16,23 +13,16 @@ void main() {
     convert timestamp into correct for current
     timezone time.
     */
+
     if (putenv("TZ=America/Los_Angeles") != 0) {
         exit(1);
     }
 
-    (void)time(&now);
+    if (time(&now) == NULL) {
+        exit(2);
+    }
 
     printf("%s", ctime(&now));
 
-    sp = localtime(&now);
-    printf(
-        "%d/%d/%02d %d:%02d %s\n",
-        sp->tm_mon + 1,
-        sp->tm_mday,
-        sp->tm_year,
-        sp->tm_hour,
-        sp->tm_min,
-        tzname[sp->tm_isdst]
-    );
     exit(0);
 }
