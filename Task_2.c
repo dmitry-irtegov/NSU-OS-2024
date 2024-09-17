6c85fe72ca36b3e3c00cdf7090bc4928ff326c12
PavelBun <p.bun@g.nsu.ru>
2024-09-17 15:59:55 +0700

    update

---
+++ Task_2.c
@@ -0,0 +1,16 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    if(putenv("TZ=America/Los_Angeles") != 0){
+        perror("error changing environment variable.");
+        return 1;
+    }
+    
+    tzset();
+    time_t now = time(NULL);
+    char * outputTime = ctime(&now);    
+    printf("%s", outputTime);
+
+}
