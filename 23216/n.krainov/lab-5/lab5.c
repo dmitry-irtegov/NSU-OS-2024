765292c9f1a4fe5149e90792e2d4ebc794282920
Nikita Krainov <n.krainov@g.nsu.ru>
2024-09-23 00:09:53 +0700

    Update code

--- 23216/n.krainov/lab-5/lab5.c
+++ 23216/n.krainov/lab-5/lab5.c
@@ -8,146 +8,172 @@
 #define LEN_BUFFER 10
 #endif
 
-typedef struct elem_of_vector_off_t{
+typedef struct elem_of_vector_off_t
+{
     off_t off;
     off_t len;
-}elem_of_vector_off_t;
+} elem_of_vector_off_t;
 
-typedef struct vector_off_t{
-    elem_of_vector_off_t* elems;
+typedef struct vector_off_t
+{
+    elem_of_vector_off_t *elems;
     int cur;
     int cap;
-}vector_off_t;
+} vector_off_t;
 
-int initVector(vector_off_t* ans){
-    ans->cur = 0;
-    ans->elems = malloc(sizeof(elem_of_vector_off_t)*20);
-    if (ans->elems == NULL){
-        perror("malloc failed");
-    }
-    ans->cap = ans->elems == NULL? -1: 20;
-    return ans->elems == NULL? 1: 0;
+vector_off_t vector;
+
+int initVector()
+{
+    vector.cur = 0;
+    vector.cap = 20;
+    vector.elems = malloc(sizeof(elem_of_vector_off_t) * 20);
+    return vector.elems == NULL ? 1 : 0;
 }
 
-int addElem(vector_off_t* vector, off_t off, off_t len){
-    if (vector->cap == vector->cur){
-        vector->cap *= 2;
-        vector->elems = realloc(vector->elems, sizeof(elem_of_vector_off_t) * vector->cap);
-        if (vector->elems == NULL){
-            perror("realloc failed");
+int addElem(off_t off, off_t len)
+{
+    if (vector.cap == vector.cur)
+    {
+        vector.cap *= 2;
+        vector.elems = realloc(vector.elems, sizeof(elem_of_vector_off_t) * vector.cap);
+        if (vector.elems == NULL)
+        {
             return 1;
         }
     }
-    vector->elems[vector->cur].len = len;
-    vector->elems[vector->cur].off = off;
-    vector->cur++;
-    
+    vector.elems[vector.cur].len = len;
+    vector.elems[vector.cur].off = off;
+    vector.cur++;
+
     return 0;
 }
 
-int searchString(vector_off_t* vector, int num_of_line, int file){
-    if (num_of_line >= vector->cur){
+int searchString(int num_of_line, int file)
+{
+    if (num_of_line > vector.cur)
+    {
         puts("Num of line is too big");
         return 0;
     }
-    char* string = calloc(vector->elems[num_of_line-1].len + 1, sizeof(char));
-    if (string == NULL){
-        perror("calloc failed");
+    char *string = calloc(vector.elems[num_of_line - 1].len + 1, sizeof(char));
+    if (string == NULL)
+    {
         return 1;
     }
 
-    if (lseek(file, vector->elems[num_of_line-1].off, SEEK_SET) == -1){
-        perror("lseek failed");
+    if (lseek(file, vector.elems[num_of_line - 1].off, SEEK_SET) == -1)
+    {
         return 1;
     }
 
-    if (read(file, string, vector->elems[num_of_line-1].len) == -1){
-        perror("read failed");
+    if (read(file, string, vector.elems[num_of_line - 1].len) == -1)
+    {
         return 1;
     }
     puts(string);
     free(string);
-    
+
     return 0;
-
 }
 
-void freeVector(vector_off_t* vector){
-    free(vector->elems);
+void freeVector()
+{
+    free(vector.elems);
+    vector.cap = 0;
+    vector.cur = 0;
 }
 
-int main(int argc, char* argv[]){
-    if (argc < 2){
-        perror("missing filename");
-        exit(EXIT_FAILURE);
+
+void exitProgram(int Code, char* message){
+    if (message != NULL){
+        perror(message);
+    }
+    freeVector();
+    exit(Code);
+}
+
+int main(int argc, char *argv[]) {
+    vector.elems = NULL;
+    if (argc < 2)
+    {
+        exitProgram(EXIT_FAILURE, "missing filename");
     }
 
     int file = open(argv[1], O_RDONLY);
-    if (file == -1){
-        perror("open failed");
-        exit(EXIT_FAILURE);
+    if (file == -1)
+    {
+        exitProgram(EXIT_FAILURE, "open failed");
     }
 
-    vector_off_t vector;
-    if (initVector(&vector)){
-        exit(EXIT_FAILURE);
+    if (initVector(&vector))
+    {
+        exitProgram(EXIT_FAILURE, "initVector failed");
     }
-    
+
+
     char buffer[LEN_BUFFER];
-
     ssize_t sym_read;
     off_t cur_len = 0, cur_off = 0;
-    char flag = 0;
-    while (1){
+    while (1)
+    {
         sym_read = read(file, buffer, LEN_BUFFER);
-        if (sym_read == -1){
-            freeVector(&vector);
-            perror("read failed");
-            exit(EXIT_FAILURE);
+        if (sym_read == -1)
+        {
+            exitProgram(EXIT_FAILURE, "read failed");
         }
-        if (sym_read == 0){
+        if (sym_read == 0)
+        {
+            if (addElem(cur_off, cur_len))
+            {
+                exitProgram(EXIT_FAILURE, "addElem failed");
+            }
             break;
         }
-        
-        for (ssize_t i = 0; i<sym_read; i++){
-            if (buffer[i] == '\n'){
+
+        for (ssize_t i = 0; i < sym_read; i++)
+        {
+            if (buffer[i] == '\n')
+            {
                 cur_len++;
-                if (addElem(&vector, cur_off, cur_len)){
-                    freeVector(&vector);
-                    exit(EXIT_FAILURE);
+                if (addElem(cur_off, cur_len))
+                {
+                    exitProgram(EXIT_FAILURE, "addElem failed");
                 }
-                cur_off+=cur_len;
+                cur_off += cur_len;
                 cur_len = 0;
             }
-            else{
+            else
+            {
                 cur_len++;
             }
         }
-        
     }
 
-    if (!flag){
-        if (addElem(&vector, cur_off, cur_len)){
-            freeVector(&vector);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    int num_of_line;
-    while (1){
+    int num_of_line, res;
+    while (1)
+    {
         puts("Enter number of string (for end programm enter 0)");
-        if(!scanf("%d", &num_of_line) || num_of_line == 0){
+        
+        res = scanf("%d", &num_of_line);
+        if (res == EOF){
+            exitProgram(EXIT_FAILURE, "scanf failed");
+        }
+        else if (res == 0) {
+            while(getc(stdin) != '\n');
+            continue;
+        }
+        else if (num_of_line == 0){
             break;
         }
-        searchString(&vector, num_of_line, file);
+        if (searchString(num_of_line, file))
+        {
+            exitProgram(EXIT_FAILURE, "searchString failed");
+        }
     }
 
-    freeVector(&vector);
-    if (close(file) == -1){
+    if (close(file)){
         perror("close failed");
-        exit(EXIT_FAILURE);
     }
-
-    exit(EXIT_SUCCESS);
-
+    exitProgram(EXIT_SUCCESS, NULL);
 }
