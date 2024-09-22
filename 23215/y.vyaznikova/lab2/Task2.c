1c58c86ab9791b159addfe32c96a23fb7b3bc1bc
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2024-09-22 20:20:02 +0700

    23215_y.vyaznikova_lab2

---
+++ 23215/y.vyaznikova/lab2/Task2.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <errno.h>
+
+extern char *tzname[];
+
+int main()
+{
+    time_t now;
+    struct tm *sp;
+
+    if ((now = time(NULL)) == -1)
+    {
+        perror("time");
+        exit(1);
+    }
+
+    if ((setenv("TZ", "America/Los_Angeles", 1) == -1))
+    {
+        perror("setenv");
+        exit(2);
+    }
+
+    sp = localtime(&now);
+
+    printf("%s", ctime(&now));
+
+    printf("%02d/%02d/%04d %02d:%02d %s\n",
+           sp->tm_mon + 1, sp->tm_mday,
+           sp->tm_year + 1900, sp->tm_hour,
+           sp->tm_min, tzname[sp->tm_isdst]);
+
+    exit(0);
+}
