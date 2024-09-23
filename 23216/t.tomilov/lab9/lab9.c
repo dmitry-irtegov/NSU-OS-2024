727efa5d199db917d8d8debcd9880c4aa85c4d0e
Tomilov Timofey <t.tomilov@g.nsu.ru>
2024-09-23 13:56:13 +0700

    lab9

--- 23216/t.tomilov/lab9/lab9.c
+++ 23216/t.tomilov/lab9/lab9.c
@@ -1,18 +1,44 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <wait.h>
+
+void cat(char* filename){
+    FILE* file = fopen(filename, "r");
+    if (file == NULL){
+        perror("Error: can`t open file!");
+        exit(-1);
+    }
+    char sym;
+    while ((sym = fgetc(file)) != EOF) {
+        putchar(sym);
+    }
+    putchar('\n');
+    fclose(file);
+}
 
 int main(int argc, char** argv){
     if (argc > 2 || argc < 2){
-        perror("Error: must be only 1 argument: ./lab10 <file>");
+        perror("Error: must be only 1 argument: ./lab9 <file>");
         exit(-1);
     }
-    printf("Child procces starded!\n");
-    fork();
-    FILE* file = open(argv[1], "r");
-    if (file == NULL){
-        perror("Error: can`t open file. Check it in your derictory.");
+    printf("Child process starded!\n");
+    pid_t child_process = fork();
+    if (child_process == -1){
+        perror("Error: Cant`t creat child process!");
         exit(-1);
     }
-    cat();
+    else if(child_process == 0){
+        cat(argv[1]);
+    }
+    else{
+        int status;
+        if (wait(&status) == -1){
+            perror("Error: failed to wait!");
+        }
+        printf("Perent process ended!\n");
+        exit(0);
+    }
+    exit(0);
 }
