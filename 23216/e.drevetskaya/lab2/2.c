9f9ebcb16edd63506407895c48cf09e403d65e4f
Elina Drevetskaya <e.drevetskaya@g.nsu.ru>
2024-09-23 21:33:27 +0700

    complete

---
+++ 23216/e.drevetskaya/lab2/2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    if(setenv("TZ", "America/Los_Angeles", 1) != 0){
+        perror("Set enviroment failed");
+        exit(1);
+    }
+    time_t now;
+    if (time(&now)!=0){
+        perror("Getting time failed");
+        exit(1);
+    }
+    struct tm *sp;
+    sp = localtime(&now);
+    printf("%s",asctime(sp));
+    return 6;
+}
