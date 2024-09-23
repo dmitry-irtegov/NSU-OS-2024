12259806b441c33416fe994bf6f866ec75156a66
Fedor Pivovarov <f3d0rp1v0v4r0v@yandex.ru>
2024-09-24 00:05:53 +0700

    refactor lab3.c

--- 23216/f.pivovarov/lab3/lab3.c
+++ 23216/f.pivovarov/lab3/lab3.c
@@ -1,47 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-void fileOpenClose(char *filename);
+int fileOpenClose(char *filename);
 
-int main(int argc, char **argv) {
+void main(int argc, char **argv) {
     // 1: prints RUID and EUID
     printf("RUID: %d, EUID: %d\n", getuid(), geteuid());
 
     // 2: try to open file by name (and close)
     if (argc < 2) {
-        printf("Usage: ./lab3 <filename>\n");
-        return 1;
+        printf("Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_SUCCESS);
     }
-    char *filename = argv[1];
-    fileOpenClose(filename);
+
+    if (fileOpenClose(argv[1]) != 0) {
+        exit(EXIT_FAILURE);
+    }
 
     // 3: set effective user id (EUID) equal to real user id (RUID)
     if (setuid(getuid()) != 0) {
         perror("setuid");
+        exit(EXIT_FAILURE);
     }
 
     // 4: prints RUID and EUID
     printf("RUID: %d, EUID: %d\n", getuid(), geteuid());
 
     // 5: try to open file by name (and close)
-    fileOpenClose(filename);
+    if (fileOpenClose(argv[1]) != 0) {
+      exit(EXIT_FAILURE);
+    }
 
-    return 0;
+    exit(EXIT_SUCCESS);
 }
 
-void fileOpenClose(char *filename) {
-    // try to open
+int fileOpenClose(char *filename) {
+    // try to open file in r-mode
     FILE *file = fopen(filename, "r");
 
     if (file == NULL) {
         perror("Cannot open file");
+        return 1;
     } else {
-
         // try to close
-        int status = fclose(file);
-
-        if (status != 0) {
+        if (fclose(file) != 0) {
             perror("Cannot close file");
+            return 1;
         }
     }
+
+    return 0;
 }
