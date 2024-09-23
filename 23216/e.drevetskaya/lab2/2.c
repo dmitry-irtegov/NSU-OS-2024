eab9d362e94b4c6c98fdd28f378b5a7001d88144
Elina Drevetskaya <e.drevetskaya@g.nsu.ru>
2024-09-23 21:20:20 +0700

    new vers comlete task

--- 23216/e.drevetskaya/lab2/2.c
+++ 23216/e.drevetskaya/lab2/2.c
@@ -3,17 +3,17 @@
 #include <time.h>
 
 int main() {
-    setenv("TZ", "America/Los_Angeles", 1);
-    tzset(); // Переменная окружения TZ была изменена, обновляем данные о времени
+    if(setenv("TZ", "America/Los_Angeles", 1) != 0){
+        perror("Set enviroment failed");
+        exit(1);
+    }
     time_t now;
-    (void)time(&now); 
-    //printf("current Califor time: %s", ctime(&now));
+    if (time(&now)!=0){
+        perror("Getting time failed");
+        exit(1);
+    }
     struct tm *sp;
     sp = localtime(&now);
-    printf("cur time Califor: %d/%d/%04d %02d:%02d\n",
-           sp->tm_mon + 1, sp->tm_mday,
-           sp->tm_year + 1900,  // Исправлено на правильный год
-           sp->tm_hour,
-           sp->tm_min);
-    return 0;
+    printf("%s",asctime(sp));
+    return 6;
 }
