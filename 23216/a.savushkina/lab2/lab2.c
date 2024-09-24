04bb231e8bde6b23f86a942cb59a3c3087af4cfd
Alena Savushkina <a.savushkina@g.nsu.ru>
2024-09-24 11:23:53 +0700

    delete unused things

--- 23216/a.savushkina/lab2/lab2.c
+++ 23216/a.savushkina/lab2/lab2.c
@@ -2,20 +2,15 @@
 #include <time.h>
 #include <stdlib.h>
 
-extern char *tzname[];
-
 int main() {
     setenv("TZ", "America/Los_Angeles", 1);
 
     time_t now;
-    struct tm *sp;
     
     if (time(&now) == (time_t)(-1)){
         perror("Current calendar time has not been encoded as time_t object");
         exit(1);
     }
-
-    sp = localtime(&now);
 
     char* t = ctime(&now);
     if(t == NULL){
