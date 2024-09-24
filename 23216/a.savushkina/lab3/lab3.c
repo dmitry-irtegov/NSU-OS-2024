eec1f41f8fa26fb06f731e522b8f4353d55fd324
Alena Savushkina <a.savushkina@g.nsu.ru>
2024-09-24 11:50:48 +0700

    change gile name to argv

--- 23216/a.savushkina/lab3/lab3.c
+++ 23216/a.savushkina/lab3/lab3.c
@@ -14,21 +14,32 @@ void open_and_close(char *fileName, char *mode){
     }   
     else{
         perror("error opening");
+        exit(EXIT_FAILURE);
     }
 }
 
-int main (){
+int main (int argc, char * argv[]){
+
+    if(argc != 2){
+        printf("wrong number of parametrs");
+        exit(EXIT_FAILURE);
+    }
+
     printf("pid : %d\n", getuid());
     printf("euid: %d\n", geteuid());
 
-    open_and_close("test_file.txt", "r");
+    open_and_close(argv[1], "r");
 
-    setuid(getuid());
+    int uid_set = setuid(getuid());
+    if(uid_set == -1){
+        perror("setuid was unsucsesfull");
+        exit(EXIT_FAILURE);
+    }
     printf("After setuid\npid : %d\n", getuid());
     printf("euid: %d\n", geteuid());
 
 
-    open_and_close("test_file.txt", "r");
+    open_and_close(argv[1], "r");
 
     exit(0);
 }
