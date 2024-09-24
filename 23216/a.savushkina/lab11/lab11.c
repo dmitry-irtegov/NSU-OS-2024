13565ba75d23149e173798435ae780b6f52b2ab7
Alena Savushkina <a.savushkina@g.nsu.ru>
2024-09-24 10:31:57 +0700

    add lab11

---
+++ 23216/a.savushkina/lab11/lab11.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <ulimit.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+extern char **environ;
+
+int execvpe(char *const file, char *const argv[], char * envp)
+{
+    char **previous_env = environ;
+    putenv(envp);
+    int new_process = execvp(file, argv);
+    if (new_process == -1)
+    {
+        perror("error in execvp");
+        exit(EXIT_FAILURE);
+    }
+    environ = previous_env;
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        printf("too few arguments\n");
+        exit(EXIT_FAILURE);
+    }
+    char * env = {"TZ=Asia/Novosibirsk"};
+
+    int new_process = execvpe(argv[1], &argv[1], env);
+    if (new_process)
+    {
+        printf("Error in creating a new process\n");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
