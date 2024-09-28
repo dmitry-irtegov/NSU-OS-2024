32a0abba55dab9c84adb538e6e6977b3bda7bce8
Alena Savushkina <a.savushkina@g.nsu.ru>
2024-09-28 14:04:10 +0700

    add lab9

--- 23216/a.savushkina/lab9/lab9.c
+++ 23216/a.savushkina/lab9/lab9.c
@@ -3,53 +3,42 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     pid_t fork_process, wait_child_process;
     int wstatus = 0;
 
     fork_process = fork();
 
-    switch (fork_process)
-    {
+    switch (fork_process){
     case -1:
         perror("error in forking process.");
         exit(EXIT_FAILURE);
     case 0:
         printf("Child process is existing\n");
-        if (!argv[1])
-        {
+        if (!argv[1]) {
             printf("no file for cat\n");
             exit(EXIT_FAILURE);
         }
         int cat_do = execlp("cat", "cat", argv[1], NULL);
-        if (cat_do == -1)
-        {
+        if (cat_do == -1) {
             perror("execlp error");
             exit(EXIT_FAILURE);
         }
         exit(EXIT_SUCCESS);
     default:
         wait_child_process = waitpid(fork_process, &wstatus, 0);
-        if (wait_child_process == -1)
-        {
+        if (wait_child_process == -1) {
             perror("error in waitpid.");
             exit(EXIT_FAILURE);
         }
-        if (WIFEXITED(wstatus))
-        {
+        if (WIFEXITED(wstatus)){
             int exit_code = WEXITSTATUS(wstatus);
-            if (exit_code == EXIT_SUCCESS)
-            {
+            if (exit_code == EXIT_SUCCESS) {
                 printf("Child process exited successfully\n");
-            }
-            else
-            {
+            } else {
                 printf("Child process exit was unsuccessful with code: %d\n", exit_code);
             }
-        }
-        else
-        {
+        } else {
             printf("Child process did not terminate normally\n");
         }
         break;
