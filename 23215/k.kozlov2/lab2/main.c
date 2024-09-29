7d62454e285b7e716514bfd2823904d774727528
Kirill Kozlov <k.kozlov2@g.nsu.ru>
2024-09-29 23:33:39 +0700

    changes

--- 23215/k.kozlov2/lab2/main.c
+++ 23215/k.kozlov2/lab2/main.c
@@ -18,7 +18,7 @@ int main()
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year, sp->tm_hour,
-       sp->tm_min, tzname[sp->tm_isdst]);
+        sp->tm_year % 100, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
     exit(0);
 }
