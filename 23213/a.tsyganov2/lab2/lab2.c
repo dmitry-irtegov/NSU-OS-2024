44e85e8a542a7518734b3e776d43758eb0d403b1
Alexander Tsyganov <a.tsyganov2@g.nsu.ru>
2024-09-29 22:29:39 +0700

    added lab2

---
+++ 23213/a.tsyganov2/lab2/lab2.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+extern char *tzname[];
+
+int main()
+{
+    time_t now;
+    struct tm *sp;
+
+    putenv("TZ=America/Los_Angeles");
+
+    (void) time( &now );
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year % 100, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+    exit(0);
+}
