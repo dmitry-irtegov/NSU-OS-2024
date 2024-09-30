d0a0bbeb5ca5f25cad5c8aa28430f118713b060d
Chernikov Grigory <g.chernikov@g.nsu.ru>
2024-09-30 11:51:20 +0700

    lab3 done

---
+++ 23215/g.chernikov/lab3/lab3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void step(){
+    printf("real user id: %d\neffective user id: %d\n", getuid(), geteuid());
+    FILE *file = fopen("file.txt", "r");
+
+    if (file == NULL){
+        perror("Error cant open file");
+    } else {
+        fclose(file);
+    }
+
+}
+int main() {
+
+    step();
+
+    if (setuid(geteuid()) == -1){
+        perror("Error failed setuid");
+    };
+
+    step();
+
+    return(0);
+}
