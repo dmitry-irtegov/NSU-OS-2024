67706ca1df61762d29e03db9a72593c1983f5bed
MCBivis <Artemiybox@mail.ru>
2024-09-30 18:24:02 +0700

    Lab_9

---
+++ 23216/a.kuleshov/Lab_9/2processes.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
+
+int main() {
+    // Создаем дочерний процесс
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        // Ошибка создания процесса
+        perror("Failed to fork");
+        return 1;
+    } else if (pid == 0) {
+        // Дочерний процесс: выполняем команду cat для вывода содержимого файла
+        execlp("cat", "cat", "voina_i_mir.txt", NULL);
+
+        // Если execlp не выполнится, выводим ошибку
+        perror("Failed to execute execlp");
+        return 1;
+    } else {
+        // Родительский процесс: ожидаем завершения дочернего процесса
+        int status;
+        pid_t wait_result = wait(&status);
+
+        if (wait_result == -1) {
+            // Ошибка при ожидании дочернего процесса
+            perror("Failed to wait for the child process");
+            return 1;
+        }
+
+        // Проверяем, завершился ли дочерний процесс нормально
+        if (WIFEXITED(status)) {
+            // Печать после завершения дочернего процесса
+            printf("\nChild process exited with code %d\n", WEXITSTATUS(status));
+        } else {
+            // Сообщение о завершении дочернего процесса с ошибкой
+            printf("\nChild process exited with error\n");
+        }
+    }
+
+    return 0;
+}
