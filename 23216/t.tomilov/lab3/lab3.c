adf6476e0b93ffeec3b4d006838c7fd0c192c86d
Tomilov Timofey <t.tomilov@g.nsu.ru>
2024-10-02 01:31:51 +0700

    lab3

--- 23216/t.tomilov/lab3/lab3.c
+++ 23216/t.tomilov/lab3/lab3.c
@@ -3,15 +3,15 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-char openFile(char* fileName){
+int openFile(char* fileName){
     FILE* file = fopen(fileName, "r");
     if (file == NULL){
         perror("   ERROR: file didn`t opened!");
-        return '0';
+        return -1;
     }
     printf("   File %s opened!\n", fileName);
     fclose(file);
-    return '1';
+    return 0;
 }
 
 int main(int argc, char** argv){;
@@ -20,12 +20,12 @@ int main(int argc, char** argv){;
         exit(EXIT_FAILURE);
     }
     printf("1. Real UID: %d\n   Effective UID: %d\n", getuid(), geteuid());
-    if (openFile(argv[1]) == '0') exit(EXIT_FAILURE);
+    if (openFile(argv[1]) == -1) exit(EXIT_FAILURE);
     if (setuid(getuid())){
         perror("ERROR: failed to setuid!");
         exit(EXIT_FAILURE);
     }
     printf("2. Real UID: %d\n   Effective UID: %d\n", getuid(), geteuid());
-    if (openFile(argv[1]) == '0') exit(EXIT_FAILURE);
+    if (openFile(argv[1]) == -1) exit(EXIT_FAILURE);
     exit(EXIT_SUCCESS);
 }
