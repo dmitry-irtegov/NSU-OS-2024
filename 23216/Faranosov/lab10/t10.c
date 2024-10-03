55e41c47d1e310c1a5e57259dd996a521e2f737b
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2024-10-03 13:56:45 +0700

    lab10

---
+++ 23216/Faranosov/lab10/t10.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+
+    char** arguments = NULL;
+    arguments = malloc(sizeof(char*) * (argc));
+    if (arguments == NULL) {
+        perror("malloc error");
+        exit(1);
+    }
+    arguments[0] = argv[1];
+    for (int i = 1; i < argc - 1; i++) {
+        arguments[i] = argv[i+1];
+    }
+    arguments[argc - 1] = NULL;
+
+    pid_t child_id, finished_procces;
+    int status;
+
+    child_id = fork();
+    switch(child_id) {
+    case 0:
+
+        execvp(argv[1], arguments);    
+        perror("exec error");
+        exit(1);
+    
+    case -1:
+        perror("fork error");
+        exit(1);
+
+    default:
+        finished_procces = wait(&status);
+        
+        if (finished_procces == -1) {
+            perror("wait error");
+            exit(1);
+        }
+
+        if (WIFEXITED(status)){
+            printf("Child`s ending code = %d\n", WEXITSTATUS(status));
+        }
+
+    }
+    free(arguments);
+    exit(0);
+}
