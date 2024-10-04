81958d52fa4f0889b648bcd3ac2b2909ee1b852f
MCBivis <Artemiybox@mail.ru>
2024-10-04 16:20:01 +0700

    added switch

--- 23216/a.kuleshov/Lab_10/CommandEndCode.c
+++ 23216/a.kuleshov/Lab_10/CommandEndCode.c
@@ -6,28 +6,31 @@
 int main(int argc, char *argv[]) {
     // Проверяем, что передана хотя бы одна команда
     if (argc < 2) {
-        printf( "There have to be at least 1 argument\n");
+        printf("There have to be at least 1 argument\n");
         return 1;
     }
 
     // Создаем дочерний процесс
     pid_t pid = fork();
 
-    if (pid < 0) {
-        // Ошибка создания процесса
-        perror("Failed to fork");
-        return 1;
-    } else if (pid == 0) {
-        // Дочерний процесс: выполняем команду argv[1] с  аргументами &argv[1]
-        execvp(argv[1], &argv[1]);
+    switch (pid) {
+        case -1:
+            // Ошибка создания процесса
+            perror("Failed to fork");
+            return 1;
 
-        // Если execvp не выполнится, выводим ошибку
-        perror("Failed to execute execvp");
-        return 1;
-    } else {
-        // Родительский процесс: ожидаем завершения дочернего процесса
-        int status;
-        pid_t wait_result = wait(&status);
+        case 0:
+            // Дочерний процесс: выполняем команду argv[1] с аргументами &argv[1]
+            execvp(argv[1], &argv[1]);
+
+            // Если execvp не выполнится, выводим ошибку
+            perror("Failed to execute execvp");
+            return 1;
+
+        default:
+            // Родительский процесс: ожидаем завершения дочернего процесса
+            int status;
+            pid_t wait_result = wait(&status);
 
         if (wait_result == -1) {
             // Ошибка при ожидании дочернего процесса
@@ -39,10 +42,10 @@ int main(int argc, char *argv[]) {
         if (WIFEXITED(status)) {
             // Печать после завершения дочернего процесса
             printf("\nChild process exited with code %d\n", WEXITSTATUS(status));
-        } else {
-            // Сообщение о завершении дочернего процесса с ошибкой
-            printf("\nChild process exited with error\n");
+            return 0;
         }
+        // Сообщение о завершении дочернего процесса с ошибкой
+        printf("\nChild process exited with error\n");
     }
 
     return 0;
