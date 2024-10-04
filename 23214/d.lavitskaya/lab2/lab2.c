5e6ceaa5faa4ff81430d5e086203eaae3e667e48
darialavitskaya <117648558+dariamukomel@users.noreply.github.com>
2024-10-04 12:39:58 +0700

    lab2 (#43)

---
+++ 23214/d.lavitskaya/lab2/lab2.c
@@ -0,0 +1,28 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+extern char *tzname[];
+
+int main() 
+{ 
+    time_t now; 
+    struct tm *sp; 
+
+    setenv("TZ", "PST8PDT", 1);
+    
+    (void) time(&now);
+
+    
+    sp = localtime(&now); 
+    printf("%02d/%02d/%04d %02d:%02d %s\n", 
+        sp->tm_mon + 1, 
+        sp->tm_mday, 
+        sp->tm_year + 1900, 
+        sp->tm_hour, 
+        sp->tm_min, 
+        tzname[sp->tm_isdst]); 
+
+    exit(0); 
+}
