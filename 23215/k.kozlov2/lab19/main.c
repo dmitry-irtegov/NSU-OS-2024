09fb503086050bf6922857fe4a30b8bb23f50ba6
Kirill Kozlov <k.kozlov2@g.nsu.ru>
2024-10-06 21:45:13 +0700

    lab19

---
+++ 23215/k.kozlov2/lab19/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+
+int match(char *name, char *pattern);
+
+int main() {
+    char pattern[1000];
+    char flag = 0;
+    struct dirent *entry;
+    DIR *dir = opendir(".");
+
+    printf("Enter a pattern: ");
+    (void)fgets(pattern, 1000, stdin);
+
+    int len = strlen(pattern);
+
+    if (pattern[len-1] == '\n')
+        pattern[len-1] = '\0';
+    else
+        printf("\n");
+
+    while((entry = readdir(dir)))
+        if (match(entry->d_name, pattern)) {
+            flag = 1;
+            printf("%s\n", entry->d_name);
+        }
+
+    if (!flag)
+        printf("%s\n", pattern);
+
+    closedir(dir);
+    return 0;
+}
+
+int match(char *name, char *pattern) {
+    int questionNumber;
+    while(*pattern) {
+        if (*pattern == '*') {
+            questionNumber = 0;
+            while (*pattern == '*' || *pattern == '?') {
+                if (*pattern == '?')
+                    questionNumber++;
+                pattern++;
+            }
+            if (*pattern) {
+                // not the end of the pattern.
+                while (*name) {
+                    if (*name == *pattern && questionNumber <= 0 && match(name, pattern))
+                        return 1;
+                    name++;
+                    questionNumber--;
+                }
+                return 0;
+            } else {
+                //end of the pattern.
+                while (*name) {
+                    name++;
+                    questionNumber--;
+                }
+                if (questionNumber <= 0)
+                    return 1;
+            }
+        } else if (*pattern == '?') {
+            if (*name) {
+                pattern++;
+                name++;
+            } else
+                return 0;
+        } else {
+            if (*pattern == *name) {
+                pattern++;
+                name++;
+            } else
+                return 0;
+        }
+    }
+
+    if (*name)
+        return 0;
+
+    return 1;
+}
