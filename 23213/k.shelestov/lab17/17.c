c2a19d5b9d569f65e6a9d016e070b856582f6fdc
Kirill <k.shelestov@g.nsu.ru>
2024-10-06 21:30:44 +0700

    shelestov lab17

---
+++ 23213/k.shelestov/lab17/17.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <signal.h>
+#include <fcntl.h>
+
+#define MAX_LINE_LENGTH 40
+#define CTRL_G "\a"
+#define ENTER '\n'
+
+struct termios original_termios;
+
+void disable_raw_mode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+void enable_raw_mode() {
+    if (tcgetattr(STDIN_FILENO, &original_termios) == -1) {
+       perror("tcgetattr");
+       exit(EXIT_FAILURE);
+    }
+    atexit(disable_raw_mode);
+    struct termios raw = original_termios;
+    raw.c_lflag &= ~(ICANON | ECHO); 
+    raw.c_cflag &= ~(CSIZE | PARENB);
+    raw.c_cflag |= CS8;
+    raw.c_cc[VMIN] = 1;
+    raw.c_cc[VTIME] = 0;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+       perror("tcsetattr");
+       exit(EXIT_FAILURE);
+    }
+}
+
+void beep() {
+    if (write(STDOUT_FILENO, CTRL_G, 1) == -1) {
+       perror("write");
+       exit(EXIT_FAILURE);
+    }
+}
+
+int main() {
+    enable_raw_mode();
+    
+    char line[MAX_LINE_LENGTH + 1] = {0};
+    int position = 0;
+    char c;
+
+    char CTRL_D = original_termios.c_cc[VEOF];
+    char ERASE = original_termios.c_cc[VERASE];
+    char KILL = original_termios.c_cc[VKILL];
+    char CTRL_W = original_termios.c_cc[VWERASE];
+    printf("Write text (CTRL-D to exit):\n");
+
+    while (1) {
+        if (read(STDIN_FILENO, &c, 1) == -1) {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+        if (c == CTRL_D && position == 0) {
+            break;
+        }
+        if (c == CTRL_W) {
+            while (position > 0 && line[position - 1] == ' ') {
+                position--;
+            }
+            while (position > 0 && line[position - 1] != ' ') {
+                position--;
+            }
+            while (position < MAX_LINE_LENGTH && line[position] == ' ') {
+                position++;
+            }
+            line[position] = '\0';
+            char buffer[MAX_LINE_LENGTH + 5];
+            int len = snprintf(buffer, sizeof(buffer), "\r\033[K%s", line);
+            if (len > 0) {
+                if (write(STDOUT_FILENO, buffer, len) == -1) {
+                    perror("write");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            continue;
+        }
+        if (c == ERASE) {
+            if (position > 0) {
+                position--;
+                line[position] = '\0';
+                char buffer[MAX_LINE_LENGTH + 5];
+                int len = snprintf(buffer, sizeof(buffer), "\r\033[K%s", line);
+                if (len > 0) {
+                    if (write(STDOUT_FILENO, buffer, len) == -1) {
+                        perror("write");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+            }
+            continue;
+        }
+        if (c == KILL) {
+            position = 0;
+            line[position] = '\0';
+            const char *clear_line = "\r\033[K\r";
+            if (write(STDOUT_FILENO, clear_line, strlen(clear_line)) == -1) {
+                perror("write");
+                exit(EXIT_FAILURE);
+            }
+            continue;
+        }
+        if (position < MAX_LINE_LENGTH && c >= 32 && c <= 126) {
+            line[position++] = c;
+            line[position] = '\0';
+            if (write(STDOUT_FILENO, &c, 1) == -1) {
+                perror("write");
+                exit(EXIT_FAILURE);
+            }
+        } else if (c < 32 || c > 126) {
+            beep();
+        }
+        if (position >= MAX_LINE_LENGTH) {
+            position = 0;
+            const char *newline = "\n";
+            if (write(STDOUT_FILENO, newline, 1) == -1) {
+                perror("write");
+                exit(EXIT_FAILURE);
+            }
+        }
+        if (c == ENTER || c == '\r') {  
+           const char *newline = "\n";
+           if (write(STDOUT_FILENO, newline, 1) == -1) {
+               perror("write");
+               exit(EXIT_FAILURE);
+           }
+           line[position] = '\0';  
+           memset(line, 0, sizeof(line));
+           position = 0;
+           continue;
+        }
+    }
+    return 0;
+}
