3dfad7ae6ab5a3e83556d9973d0ea307198b975b
MCBivis <Artemiybox@mail.ru>
2024-10-07 15:34:33 +0700

    switch

--- 23216/a.kuleshov/Lab_11/execvpe.c
+++ 23216/a.kuleshov/Lab_11/execvpe.c
@@ -18,13 +18,13 @@ int execvpe(const char *file, char *const argv[], char *const envp[]) {
     // Если execvp завершился с ошибкой, восстанавливаем старую среду
     environ = old_environ;
 
-    // Возвращаем результат выполнения execvp (он не возвращает, если команда успешна)
+    // Возвращаем результат выполнения execvp (не возвращает, если команда успешна)
     return result;
 }
 
 int main() {
     // Создаем массив аргументов для команды
-    char *args[] = {"env", NULL}; // Команда "env" выводит все переменные окружения
+    char *args[] = {"env", NULL};
 
     // Задаём новое окружение
     char *new_envp[] = {"PATH=/bin:/usr/bin", NULL};
@@ -32,36 +32,39 @@ int main() {
     // Создаем дочерний процесс
     pid_t pid = fork();
 
-    if (pid < 0) {
-        // Ошибка создания процесса
-        perror("Failed to fork");
-        return 1;
-    } else if (pid == 0) {
-        // Дочерний процесс: проверяем работоспособность функции
-        execvpe("env", args, new_envp);
+    switch (pid) {
+        case -1:
+                // Ошибка создания процесса
+                perror("Failed to fork");
+                return 1;
 
-        // Если execvpe не выполнится, выводим ошибку
-        perror("Failed to execute execvpe");
-        return 1;
-    } else {
-        // Родительский процесс: ожидаем завершения дочернего процесса
-        int status;
-        pid_t wait_result = wait(&status);
+        case 0:
+            // Дочерний процесс: проверяем работоспособность функции
+                execvpe("env", args, new_envp); // Команда "env" выводит все переменные окружения
 
-        if (wait_result == -1) {
-            // Ошибка при ожидании дочернего процесса
-            perror("Failed to wait for the child process");
+            // Если execvpe не выполнится, выводим ошибку
+            perror("Failed to execute execvpe");
             return 1;
-        }
 
-        // Проверяем, завершился ли дочерний процесс нормально
-        if (WIFEXITED(status)) {
-            // Печать после завершения дочернего процесса
-            printf("\nChild process exited with code %d\n", WEXITSTATUS(status));
-        } else {
+        default:
+            // Родительский процесс: ожидаем завершения дочернего процесса
+            int status;
+            pid_t wait_result = wait(&status);
+
+            if (wait_result == -1) {
+                // Ошибка при ожидании дочернего процесса
+                perror("Failed to wait for the child process");
+                return 1;
+            }
+
+            // Проверяем, завершился ли дочерний процесс нормально
+            if (WIFEXITED(status)) {
+                // Печать после завершения дочернего процесса
+                printf("\nChild process exited with code %d\n", WEXITSTATUS(status));
+                return 0;
+            }
             // Сообщение о завершении дочернего процесса с ошибкой
             printf("\nChild process exited with error\n");
-        }
     }
     return 0;
 }
