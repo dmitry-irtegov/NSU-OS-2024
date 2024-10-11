3fba894d9f153b4db4c68fd2f61aa038231c349b
Andrew_Vetrov <user120715mts@gmail.com>
2024-10-11 09:49:41 +0300

    -

---
+++ 23217/a.chepik/sem2-lab1/sem2-lab1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* thread_function(void* arg) {
+    for (int i = 0; i < 10; i++) {
+        printf("Child.\n");
+    }
+
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    int pc_code = pthread_create(&thread, NULL, thread_function, NULL);
+
+    if (pc_code != 0) {
+        printf("pthread_create() returned a non-zero value.\n");
+        exit(-1);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("Parent.\n");
+    }
+
+    pthread_exit(NULL);
+}
