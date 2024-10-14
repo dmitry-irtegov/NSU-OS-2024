c577e9913d22da0c2c75903032b646a20f4ca49b
Dimandr <Dmitry221556@yandex.ru>
2024-10-14 09:43:35 +0700

    lab8

---
+++ 23215/d.stolyarov/lab8/lab8.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>      
+#include <string.h>
+#include <unistd.h>                   
+int main(int argc, char *argv[]){
+
+    if(argc == 1){
+        perror("Missing arguement");
+        exit(1);
+    }
+    int file;                   
+    if((file = open(argv[1], O_RDWR)) == -1){
+        perror("File opening error");
+        exit(2);        
+    }
+    else{
+        printf("File opened successfully\n");
+    }
+
+    struct flock lock;
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = 0;
+    lock.l_len = 0;
+
+    if (fcntl(file, F_SETLK , &lock) == -1) {
+        perror("Locking error");
+        exit(3);
+    }
+    else{
+        printf("Locked successfully\n");
+    }
+
+    char cmd[100] = "nano " ;
+    int offset = strlen(cmd);
+    int fileNameSize = strlen(argv[1]);
+    for(int i = 0; i < fileNameSize; i++){
+        cmd[offset + i] = argv[1][i];   
+    }
+    cmd[offset + fileNameSize] = 0;
+    
+    if(system(cmd) == -1){
+        perror("Editing error");
+        exit(4);
+    }
+
+    lock.l_type = F_UNLCK;
+    if(fcntl(file, F_SETLK, &lock) == -1){
+        perror("Unlocking error");
+        exit(5);
+    }
+
+    close(file);
+    exit(0);
+}   
