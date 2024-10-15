06eb2f053a2b4daef24bda1bc78564577b7f8da7
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-10-16 02:25:50 +0700

    lab3_solution

---
+++ 23215/n.laptev1/lab3/lab3.c
@@ -0,0 +1,57 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define MAX_SIZE 257
+void enter_filename(char* filename);
+void print_ID(uid_t efctv_ID);
+int set_alike_ID(uid_t ID);
+void open_file(char* filename);
+
+int main() {
+    char filename[MAX_SIZE];
+    uid_t efctv_ID = geteuid();
+    
+    //first time
+    print_ID(efctv_ID);
+    enter_filename(filename);
+    open_file(filename);
+
+    int flag = set_alike_ID(efctv_ID);
+    if (flag == 0) {
+        printf("Everything is fine! ID changed correctly!\n");
+    } else {
+        perror("You can't change ID.");
+        exit(0);
+    }
+    
+    //second time
+    print_ID(efctv_ID);
+    enter_filename(filename);
+    open_file(filename);
+}
+
+void print_ID(uid_t efctv_ID) {
+    uid_t real_ID = getuid();
+    printf("Your real ID is: %d\n", real_ID);
+    printf("Your effective ID is: %d\n", efctv_ID);
+}
+
+int set_alike_ID(uid_t ID) {
+    return setuid(ID);
+}
+
+void open_file(char* filename) {
+    FILE* in = fopen(filename, "r");
+    if (in == NULL) {
+        perror("Error: ");
+    } else {
+        fclose(in);
+    }
+}
+void enter_filename(char* filename) { 
+    printf("Enter filename: ");
+    scanf("%s", filename);
+}
