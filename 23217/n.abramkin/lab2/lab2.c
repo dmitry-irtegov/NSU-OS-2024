34f7c8d0106b5a49f10a095f5e63a141d87661d6
Nikita Abramkin <n.abramkin@g.nsu.ru>
2024-10-15 14:30:09 +0700

    23217_n.abramkin_lab2

--- 23217/n.abramkin/lab2/lab2.c
+++ 23217/n.abramkin/lab2/lab2.c
@@ -11,17 +11,14 @@ int main()
 
     (void)time(&now);
 
-    setenv("TZ", "Asia/Novosibirsk", 1);
+    setenv("TZ", "America/Los_Angeles", 1);
 
     printf("%s", ctime(&now));
-
-    setenv("TZ", "America/Los_Angeles", 1);
 
     sp = localtime(&now);
     printf("%d/%d/%02d %d:%02d %s\n",
         sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year, sp->tm_hour,
+        sp->tm_year + 1900, sp->tm_hour,
         sp->tm_min, tzname[sp->tm_isdst]);
     exit(0);
-
 }
