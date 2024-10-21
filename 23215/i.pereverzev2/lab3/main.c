f75bc0ff2686326fa2b3b9778c21304d95178b26
Ilya Pereverzev <141811344+Takymu@users.noreply.github.com>
2024-10-21 09:21:18 +0700

    i.pereverzev2 lab3 (#247)
    * i.pereverzev2 lab3
    
    * printing added
    
    * setuid check added
    
    * fclose added
    
    * fclose added2
    
    * fclose added22

---
+++ 23215/i.pereverzev2/lab3/main.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void opentry()
+{
+    FILE* fd = fopen("testfile", "r");
+    if (fd == NULL) {
+        perror("can't open the file");
+        exit(1);
+    } else {
+	    printf("file opened successfully\n");
+        fclose(fd);
+    }
+}
+
+int main()
+{
+    uid_t uid = getuid();
+    uid_t euid = geteuid();
+    printf("uid: %d\neuid: %d\n", uid, euid);
+    opentry();
+    if(setuid(uid) == -1) {
+    	perror("setuid failed");
+    } else {
+        printf("setuid(uid) executed\n");
+    }
+    uid = getuid();
+    euid = geteuid();
+    printf("uid: %d\neuid: %d\n", uid, euid);
+    opentry();
+    return 0;
+}
