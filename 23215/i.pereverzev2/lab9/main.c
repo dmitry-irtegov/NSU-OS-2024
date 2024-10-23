4f4755b2785896e0c4ab5d2cc89e039d35f145eb
Takumy <Talumy@duck.com>
2024-10-23 17:14:25 +0700

    lab9 completed

---
+++ 23215/i.pereverzev2/lab9/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main()
+{
+    pid_t chpid = fork();
+    if(chpid == -1) {
+    	perror("unable to create child process");
+	return 1;
+    }
+    if(chpid == 0) {
+	execlp("cat", "cat", " testfile", NULL);
+	perror("cat wasn't executed");
+	return 2;
+    } else if(waitpid(chpid, NULL, 0) == -1) {
+        perror("waitpid error");
+	return 3;
+    } else {
+        printf("sample text from parent process\n");
+    }
+    return 0;
+}
