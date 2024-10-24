e36c799174b899b04ffd7fd79b4b6d8799caa71a
Takumy <Talumy@duck.com>
2024-10-24 20:13:54 +0700

    codestyle correctives

--- 23215/i.pereverzev2/lab9/main.c
+++ 23215/i.pereverzev2/lab9/main.c
@@ -7,17 +7,17 @@
 int main()
 {
     pid_t chpid = fork();
-    if(chpid == -1) {
-    	perror("unable to create child process");
-	return 1;
+    if (chpid == -1){
+        perror("unable to create child process");
+        return 1;
     }
-    if(chpid == 0) {
-	execlp("cat", "cat", "testfile", NULL);
-	perror("cat wasn't executed");
-	return 2;
-    } else if(waitpid(chpid, NULL, 0) == -1) {
+    if (chpid == 0) {
+        execlp("cat", "cat", "testfile", NULL);
+        perror("cat wasn't executed");
+        return 2;
+    } else if (waitpid(chpid, NULL, 0) == -1) {
         perror("waitpid error");
-	return 3;
+        return 3;
     } else {
         printf("sample text from parent process\n");
     }
