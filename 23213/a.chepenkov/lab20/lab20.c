efe940bb49368a0c029ecfeee1442cac2237cffe
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-10-28 00:42:25 +0700

    Artem Chepenkov 23213 lab20

---
+++ 23213/a.chepenkov/lab20/lab20.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <glob.h>
+
+int main() {
+    glob_t glob_struct;
+    int glob_res;
+    char** matched;
+    char pattern[BUFSIZ];
+    fgets(pattern, BUFSIZ, stdin);
+
+    if (pattern[strlen(pattern) - 1] == '\n') {
+        pattern[strlen(pattern) - 1] = '\0';
+    }
+
+    glob_res = glob(pattern, 0, NULL, &glob_struct);
+    if (glob_res == GLOB_NOMATCH) {
+        printf("No files found with pattern: %s\n", pattern);
+        return -1;
+    }
+    if (glob_res != 0) {
+        fprintf(stderr, "An error occured while glob function\n");
+        return -1;
+    }
+
+    matched = glob_struct.gl_pathv;
+    while (*matched) {
+        printf("%s\n", *matched);
+        matched++;
+    }
+    globfree(&glob_struct);
+    return 0;
+}
