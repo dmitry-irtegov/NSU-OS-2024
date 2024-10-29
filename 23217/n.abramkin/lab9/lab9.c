862f13269cf7340c851dbb56282cbcd0a2e9fbec
Nikita Abramkin <n.abramkin@g.nsu.ru>
2024-10-29 12:49:12 +0700

    add lab9

--- 23217/n.abramkin/lab9/lab9.c
+++ 23217/n.abramkin/lab9/lab9.c
@@ -4,21 +4,28 @@
 #include <sys/wait.h>
 
 int main() {
-    pid_t pid;
+    pid_t pid = fork(); // Create a child process
     char *filename = "largefile.txt"; 
 
-    pid = fork(); 
-
-    if (pid == -1) {
-        perror("Ошибка создания подпроцесса");
+    if (pid < 0) {
+        perror("Fork failed");
         return 1;
-    } else if (pid == 0) {
-        execlp("cat", "cat", filename, (char *) NULL);
-        perror("Ошибка выполнения команды cat");
+    }
+    else if (pid == 0) { // Child process
+        printf("Child process: Executing 'cat' on a long file.\n");
+        
+        execlp("cat", "cat", filename, (char *)NULL);
+        
+        perror("execlp failed");
         exit(1);
-    } else {
-        wait(NULL);
-        printf("Подпроцесс завершен, сообщение от родителя.\n");
+    }
+    else { // Parent process
+        printf("Parent process: My child process has PID %d.\n", pid);
+
+        // Wait for the child process to complete
+        waitpid(pid, NULL, 0);
+
+        printf("Parent process: Child process completed.\n");
     }
 
     return 0;
