031a82f4377d1902382f32479f2d619585c1f2e3
KolyaPronin <145115145+KolyaPronin@users.noreply.github.com>
2024-11-04 19:20:32 +0700

    California_Love.c

--- 23217/n.pronin/lab2/California_Love.c
+++ 23217/n.pronin/lab2/California_Love.c
@@ -4,27 +4,21 @@
 #include <stdlib.h>
 
 int main() {
-    time_t now;         // количество секунд, прошедших с 1 января 1970 года
-    struct tm *sp;      // указатель на структуру типа tm, которая используется 
-                        // для хранения локального времени в более удобном для нас виде.
-
-                        // Устанавливаем временную зону на Pacific Time (PST/PDT)
-    putenv("TZ=PST8PDT");  // Для Windows вместо setenv, устанавливает переменную окружения 
-    tzset();            // Применить новое значение TZ
-                        // обновляет часовой пояс на основе переменной окружения TZ, применяя её новое значение.
-    (void) time(&now);  // возвращает текущее время в секундах с момента эпохи Unix
+    time_t now;        
+    struct tm *sp;      
+                       
+    setenv("TZ", "PST8PDT", 1);   
+    tzset();            
+                        
+    (void) time(&now);  
 
     printf("UTC time: %s", ctime(&now));  // Выводим время в формате UTC
-                       // ctime(&now) — преобразует значение time_t в строку с форматированным представлением времени в формате UTC.
-    sp = localtime(&now);  // Преобразуем в местное время (Pacific Time)
+                       
+    sp = localtime(&now);  
     printf("Pacific Time: %d/%d/%02d %d:%02d %s\n",
            sp->tm_mon + 1, sp->tm_mday,
-           sp->tm_year + 1900,  // Год корректируется на +1900
+           sp->tm_year + 1900,  
            sp->tm_hour, sp->tm_min, sp->tm_isdst ? "PDT" : "PST");
-
-   
-    printf("Press Enter to exit...");
-    getchar();  
-
+ 
     return 0;
 }
