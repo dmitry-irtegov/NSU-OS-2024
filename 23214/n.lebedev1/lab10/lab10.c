2c444bd6541841b34bb3fcd38bb386b1f3b372ea
Nikolay56615 <nikolay56615@mail.ru>
2024-11-05 13:07:49 +0700

    lab10

---
+++ 23214/n.lebedev1/lab10/lab10.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main(int argc, char** argv) {
+    int status;
+    pid_t current_pid = getpid();
+    pid_t parent_pid = getppid();
+    printf("Before fork:\n current pid = %d\n parent pid = %d\n", current_pid, parent_pid);
+    pid_t fork_pid = fork();
+    parent_pid = getppid();
+    printf("After fork:\n fork pid = %d\n parent pid = %d\n", fork_pid, parent_pid);
+    printf("\n");
+    if (fork_pid == -1) {
+        perror("Failed to create child by fork");
+        exit(-1);
+    }
+    else if (fork_pid == 0) {
+        if (argc < 2) {
+            printf("Not enough arguments for execution\\n\n");
+            exit(-1);
+        }
+        char* command = argv[1];
+        char** childArg = argv + 1;
+        printf("Executing %s...\n", command);
+        if (execvp(command, childArg) == -1) {
+            perror("Failed to execute");
+            exit(-1);
+        }
+    }
+    pid_t wait_pid = waitpid(fork_pid, &status, 0);
+    if (wait_pid == -1) {
+        perror("Error with waiting the child process");
+        exit(-1);
+    }
+    if (WIFEXITED(status)) {
+        printf("\nchild pid = %d \nChild finished with exit status %d\nEverything success\n",
+               wait_pid, WEXITSTATUS(status));
+    }
+    return 0;
+}
