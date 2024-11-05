12908669a76816190519a08f310135091f802dbd
Ochir Khubanov <o.khubanov@g.nsu.ru>
2024-11-05 11:44:04 +0700

    LAB21

--- 23216/o.khubanov/lab21/23216_o.khubanov_lab21.c
+++ 23216/o.khubanov/lab21/23216_o.khubanov_lab21.c
@@ -3,25 +3,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static int beep_counter = 0; // Статическая переменная для подсчета звуковых сигналов
+static int beep_counter = 0; // Обычный int для счётчика
 
 void signal_handler(int signal_number) {
     switch (signal_number) {
         case SIGINT:
             // При SIGINT издать звуковой сигнал и увеличить счетчик
             write(STDOUT_FILENO, "\a", 1);
-            beep_counter++;
+            beep_counter++;  // Увеличиваем счетчик
             break;
-        case SIGQUIT:
-            // При SIGQUIT вывести количество звуковых сигналов и завершить программу
-            dprintf(STDOUT_FILENO, "\nTotal number of beeps: %d\n", beep_counter);
-            _exit(EXIT_SUCCESS);
+        case SIGQUIT: {
+            // Конвертируем beep_counter в строку вручную
+            int counter = beep_counter;
+            char buffer[50];
+            int i = 0;
+
+            // Обратное заполнение buffer значением counter
+            if (counter == 0) {
+                buffer[i++] = '0';
+            } else {
+                while (counter > 0) {
+                    buffer[i++] = '0' + (counter % 10);
+                    counter /= 10;
+                }
+            }
+            
+            // Разворачиваем число в прямой порядок
+            for (int j = 0; j < i / 2; j++) {
+                char temp = buffer[j];
+                buffer[j] = buffer[i - j - 1];
+                buffer[i - j - 1] = temp;
+            }
+
+            buffer[i++] = '\n';  // Добавляем перенос строки
+            write(STDOUT_FILENO, "Total number of beeps: ", 23);
+            write(STDOUT_FILENO, buffer, i);
+            _exit(EXIT_SUCCESS); // Используем _exit для немедленного завершения
+        }
         default:
-            // Игнорировать все остальные сигналы
+            // Игнорируем другие сигналы
             break;
     }
 }
-
 int main() {
     // Установка пользовательских обработчиков сигналов
     if (signal(SIGINT, signal_handler) == SIG_ERR) {
@@ -40,3 +63,4 @@ int main() {
 
     return 0; // Программа сюда никогда не дойдет, но добавлено для завершенности
 }
+
