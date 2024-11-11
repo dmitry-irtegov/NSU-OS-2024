1e3cee3ac7a694d1532fb688df662b3acbb538de
Takumy <Talumy@duck.com>
2024-11-11 10:15:18 +0700

    minor changes

--- 23215/i.pereverzev2/lab26/main.c
+++ 23215/i.pereverzev2/lab26/main.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
     if(argc < 2) {
-        char *cmd = malloc(strlen(argv[0] + 2));
+        char *cmd = malloc(strlen(argv[0] + 3));
         strcpy(cmd, argv[0]);	
         strcat(cmd, " w");
         FILE* fpt = popen(cmd, "r");
