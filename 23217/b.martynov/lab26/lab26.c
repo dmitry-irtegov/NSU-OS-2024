b555481e5625d2b9ca73a728a4ece2ae27436863
Bogdan Martynov <b.martynov@g.nsu.ru>
2024-11-12 11:55:04 +0700

    First attempt.

---
+++ 23217/b.martynov/lab26/lab26.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define STR_SIZE 1024
+#define BUFET_SIZE 1024
+
+int main(int argc, char** argv)
+{
+    int child = 0;
+    if (argc >= 2) {
+        child = 1;
+    }
+
+    unsigned char bufet[] = "SASha was WaLkInG on HIGHway and SUCKed BAGel.";
+
+    if (child == 1) {
+        ssize_t write_res = write(stdout, (const void*)bufet, sizeof(bufet));
+        if (write_res == -1) {
+            perror("write() unsuccess");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else {
+        char str[STR_SIZE] = { 0 };
+        strcat(str, argv[0]);
+        strcat(str, " biba boba");
+
+        FILE* input = popen(str, "r");
+
+        unsigned char bufet_for_read[BUFET_SIZE] = { 0 };
+
+        ssize_t read_res = read(stdin, (void*)bufet_for_read, BUFET_SIZE);
+        if (read_res == -1) {
+            perror("read() unsuccess");
+            exit(EXIT_FAILURE);
+        }
+
+        pclose(input);
+    }
+
+    return 0;
+}
