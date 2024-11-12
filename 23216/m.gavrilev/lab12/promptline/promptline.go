b86935e9062e842584c8f1f2b991fad155171b82
Maxim <m.gavrilev@g.nsu.ru>
2024-11-12 14:50:46 +0700

    some changes

--- 23216/m.gavrilev/lab12/promptline/promptline.go
+++ 23216/m.gavrilev/lab12/promptline/promptline.go
@@ -1,31 +1,31 @@
 package promptline
 
 import (
+	"fmt"
+	"os"
 	"syscall"
 )
 
-func Promptline(prompt []byte, line []byte, sizline int64) int64 {
-
-	var len int64 = 0
-	tmpLine := make([]byte, 1024)
-	_, err := syscall.Write(1, prompt)
+func Promptline(prompt []byte, line []byte) []byte {
+	_, err := syscall.Write(int(os.Stdout.Fd()), prompt)
 	if err != nil {
-		panic("Write Problem")
+		fmt.Println("write problem")
+		os.Exit(1)
 	}
+	str := make([]byte, 1024)
 	for {
-		n, err := syscall.Read(0, tmpLine)
-		line = append(line[:len], tmpLine[:n]...)
+		n, err := syscall.Read(int(os.Stdin.Fd()), str)
 		if err != nil {
-			panic("Read Problem")
+			fmt.Println("read problem")
+			os.Exit(2)
 		}
-		b := int64(n)
-		len += b
-		if line[len-2] == '\\' && line[len-1] == '\n' {
-			line[len-1] = ' '
-			line[len-2] = ' '
+		line = append(line, str[:n]...)
+		if len(line) >= 2 && line[len(line)-2] == '\\' && line[len(line)-1] == '\n' {
+			line = line[:len(line)-2]
+			fmt.Print("> ")
 			continue
 		}
-		line[len-1] = 0
-		return (len)
+		line[len(line)-1] = 0
+		return line
 	}
 }
--- 23216/m.gavrilev/lab12/shell.go
+++ 23216/m.gavrilev/lab12/shell.go
@@ -2,62 +2,66 @@ package main
 
 import (
 	"fmt"
-	head "lab12/header"
 	parc "lab12/parceline"
 	prom "lab12/promptline"
 	"os"
 	"os/exec"
 	"strings"
 	"syscall"
+	"unsafe"
 )
 
-func nullstrskip(line []string) int {
-	var id int = 0
-	for strings.Compare(line[id], "") != 0 {
-		id++
-	}
-	return id
-}
+func main() { os.Exit(mainWithReturn()) }
 
-func main() {
-	prompt := []byte(fmt.Sprintf("[%s] ", os.Args[0]))
-	line := make([]byte, 1024)
+func mainWithReturn() int {
+	prompt := ([]byte)(fmt.Sprintf("[%s] ", os.Args[0]))
 
 	/* PLACE SIGNAL CODE HERE */
 
-	for prom.Promptline(prompt, line, 1024) > 0 {
-		ncmds := parc.Parceline(line)
-		if ncmds <= 0 {
+	for {
+		var line []byte
+		line = prom.Promptline(prompt, line)
+		if len(line) <= 0 {
+			break
+		}
+		var stat parc.Status
+		var cmds []parc.Command
+		cmds, stat = parc.Parceline(line, cmds, stat)
+		if len(cmds) <= 0 {
 			continue
 		}
-		for i := 0; i < ncmds; i++ {
-			if len(head.Cmds[i].Cmdargs) == 0 {
+		for i := 0; i < len(cmds); i++ {
+			if len(cmds[i].GetArgs()) == 0 {
 				continue
 			}
-			binary, err := exec.LookPath(head.Cmds[i].Cmdargs[0])
-			if err != nil {
-				fmt.Printf("Command not found: %s\n", head.Cmds[i].Cmdargs[0])
-				continue
+			if strings.Compare(cmds[i].GetArgs()[0], ("exit")) == 0 {
+				return 0
 			}
 
-			env := os.Environ()
-			pid, err := syscall.ForkExec(binary, head.Cmds[i].Cmdargs[:nullstrskip(head.Cmds[i].Cmdargs)], &syscall.ProcAttr{
-				Dir:   "",
-				Env:   env,
-				Files: []uintptr{0, 1, 2},
-				Sys:   &syscall.SysProcAttr{},
-			})
-
+			binary, err := exec.LookPath(cmds[i].GetArgs()[0])
 			if err != nil {
-				fmt.Println("Error during ForkExec:", err)
+				fmt.Printf("command not found: %s\n", cmds[i].GetArgs()[0])
 				continue
 			}
-
-			var ws syscall.WaitStatus
-			_, err = syscall.Wait4(pid, &ws, 0, nil)
-			if err != nil {
-				fmt.Println("Error waiting for process to complete:", err)
+			ppid, _, errno := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+			pid := *(*int)(unsafe.Pointer(&ppid))
+			if errno != 0 {
+				err = errno
+				fmt.Println("error during fork", err)
+				continue
+			}
+			if pid == 0 {
+				err := syscall.Exec(binary, cmds[i].GetArgs(), os.Environ())
+				fmt.Println("error during exec, it doesnt return here ", err)
+				return 1
+			} else {
+				var ws syscall.WaitStatus
+				_, err = syscall.Wait4(pid, &ws, 0, nil)
+				if err != nil {
+					fmt.Println("error waiting for process to complete:", err)
+				}
 			}
 		}
 	}
+	return 0
 }
