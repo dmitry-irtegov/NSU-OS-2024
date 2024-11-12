d6b3b2d809b18e621615851c808b67ad4e40654c
Bogdan Martynov <b.martynov@g.nsu.ru>
2024-11-12 12:07:07 +0700

    Fourth attempt. Debug.

--- 23217/b.martynov/lab26/lab26.c
+++ 23217/b.martynov/lab26/lab26.c
@@ -27,6 +27,8 @@ int main(int argc, char** argv)
         strcat(str, argv[0]);
         strcat(str, " biba boba");
 
+        printf("'%s'\n", str);
+
         FILE* input = popen(str, "r");
 
         unsigned char bufet_for_read[BUFET_SIZE] = { 0 };
