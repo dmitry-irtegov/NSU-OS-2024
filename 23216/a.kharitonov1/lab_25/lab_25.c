5fd94e5e8efd220912711eb72fa06a9766596d9a
AlexCharitonov <alexcharitonov307@mail.ru>
2024-11-14 11:41:33 +0700

    lab_25

--- 23216/a.kharitonov1/lab_25/lab_25.c
+++ 23216/a.kharitonov1/lab_25/lab_25.c
@@ -8,24 +8,16 @@
 int main(){
     int fd[2]; pid_t pid;
     if (pipe(fd) == -1) {
-        perror("problem in pipe");
+        perror("problem in pipecreate");
         exit(EXIT_FAILURE);
     }
     if ((pid=fork()) > 0) {  /* parent */
-        char msgout[MSGSIZE]="tESt LiNe fOr laB_25 hElLO woRld\n";
-        if (puts(msgout) <0){
-            perror("problem in puts");
-            exit(EXIT_FAILURE);
-        }
-        if(write(fd[1], msgout, MSGSIZE)==-1){
-            perror("problem in write");
-            exit(EXIT_FAILURE);
-        }
-        exit(EXIT_SUCCESS);
-    }
-    else if (pid == 0) {      /* child */
         char msgin[MSGSIZE];
         ssize_t msglen;
+        if (close(pipefd[1]) == -1) {
+            perror("problem in pipeclose");
+            exit(EXIT_FAILURE);
+        }
         if((msglen = read(fd[0], msgin, MSGSIZE))==-1){
             perror("problem in read");
             exit(EXIT_FAILURE);
@@ -35,6 +27,30 @@ int main(){
         }
         if (puts(msgin) <0){
             perror("problem in puts");
+            exit(EXIT_FAILURE);
+        }
+        if (close(pipefd[0]) == -1) {
+            perror("problem in pipeclose");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    else if (pid == 0) {      /* child */
+        char msgout[MSGSIZE]="tESt LiNe fOr laB_25 hElLO woRld\n";
+        if (puts(msgout) <0){
+            perror("problem in puts");
+            exit(EXIT_FAILURE);
+        }
+        if (close(pipefd[0]) == -1) {
+            perror("problem in pipeclose");
+            exit(EXIT_FAILURE);
+        }
+        if(write(fd[1], msgout, MSGSIZE)==-1){
+            perror("problem in write");
+            exit(EXIT_FAILURE);
+        }
+        if (close(pipefd[1]) == -1) {
+            perror("problem in pipeclose");
             exit(EXIT_FAILURE);
         }
         exit(EXIT_SUCCESS);
