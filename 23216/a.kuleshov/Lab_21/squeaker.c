b668f7dc1c5c137aef6a863ae89e0a60e9dd9671
MCBivis <Artemiybox@mail.ru>
2024-11-14 15:06:38 +0700

    all_fixed

--- 23216/a.kuleshov/Lab_21/squeaker.c
+++ 23216/a.kuleshov/Lab_21/squeaker.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
 #include <unistd.h>
 
 // Переменная для хранения количества сигналов SIGINT
@@ -7,10 +8,10 @@ volatile sig_atomic_t signal_count = 0;
 
 // Простая функция для преобразования числа в строку
 void int_to_string(int num, char* str) {
-    int i = 0;
+    int i = 1;
 
     // Определим количество цифр в числе
-    for (int temp = num; temp != 0; temp /= 10) {
+    for (int temp = num; temp >= 10; temp /= 10) {
         i++;
     }
 
@@ -28,17 +29,16 @@ void handle_signal(int sig) {
     switch (sig) {
         case SIGINT:
             signal_count++;
-        write(STDOUT_FILENO, "\a", 1); // Звуковой сигнал
-        break;
-        default: {
+            write(STDOUT_FILENO, "\a", sizeof(char)); // Звуковой сигнал
+            break;
+        default:
             char buffer[100] = "\nПрограмма завершена. Количество SIGINT - ";
             char count_str[10] = {0}; // Для конвертации числа в строку
             int_to_string(signal_count, count_str); // Преобразуем счётчик в строку
-            write(STDOUT_FILENO, buffer, 100);
-            write(STDOUT_FILENO, count_str, 10);
-            write(STDOUT_FILENO, "\n", 1);
-        }
-        _exit(0); // Завершаем программу
+            write(STDOUT_FILENO, buffer, strlen(buffer));
+            write(STDOUT_FILENO, count_str, strlen(count_str));
+            write(STDOUT_FILENO, "\n", sizeof(char));
+            _exit(0); // Завершаем программу
     }
 }
 
