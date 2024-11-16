62627ec15b40fc051d1dfd5af2fd9886e14ec663
Takumy <Talumy@duck.com>
2024-11-16 13:04:52 +0700

    shell.h added. For some reason it was missed

---
+++ 23215/i.pereverzev2/shell/shell.h
@@ -0,0 +1,25 @@
+#define MAXARGS 256
+#define MAXCMDS 50
+
+struct command {
+    char *cmdargs[MAXARGS];
+    char cmdflag;
+};
+
+struct job {
+    pid_t pgid;
+    pid_t lidpid;
+    char fgrnd;
+    char stopped;
+};
+
+/*  cmdflag's  */
+#define OUTPIP  01
+#define INPIP   02
+
+extern struct command cmds[];
+extern char *infile, *outfile, *appfile;
+extern char bkgrnd;
+
+int parseline(char *);
+int promptline(char *, char *, int);
