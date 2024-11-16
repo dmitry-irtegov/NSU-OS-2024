fa4040226db88a46d192ef772ac2a5fbcc6c731d
Coilyun <n.borodkin@g.nsu.ru>
2024-11-16 11:37:40 +0700

    Nothing

--- 23217/n.borodkin/lab2/lab2.c
+++ 23217/n.borodkin/lab2/lab2.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 int main() {
-    time_t current_time;          
-    struct tm local_time;
+    time_t current_time;         
+    struct tm *utc_time;          
+    struct tm pst_time;          
 
-    setenv("TZ", "PST8PDT", 1);
-    tzset(); 
+    current_time = time(NULL);      // Текущее время в сек от 1 января 1970
+    
+    utc_time = gmtime(&current_time);     //Преобразуем в UTC
+    
+    pst_time = *utc_time;    //Копируем
+    
+    pst_time.tm_hour -= 8;      // Переводим в PST (UTC-8)
+    
+//Если время стало отрицательным, то
+    if (pst_time.tm_hour < 0) {
+        pst_time.tm_hour += 24;   
+        pst_time.tm_mday -= 1;   
+    }
 
-    current_time = time(NULL);
-
-    localtime_r(&current_time, &local_time);
-
-    printf("Current time in California (PST): %02d:%02d:%02d %02d-%02d-%04dn", 
-        local_time.tm_hour, 
-        local_time.tm_min, 
-        local_time.tm_sec, 
-        local_time.tm_mday, 
-        local_time.tm_mon + 1,  
-        local_time.tm_year + 1900);  
+    //Выводим время
+    printf("Current time in California (PST): %02d:%02d:%02d %02d-%02d-%04d\n", 
+        pst_time.tm_hour, 
+        pst_time.tm_min, 
+        pst_time.tm_sec, 
+        pst_time.tm_mday, 
+        pst_time.tm_mon + 1,  // tm_mon начинается с 0, поэтому добавляем 1
+        pst_time.tm_year + 1900);  // в tm_year лежит число лет прошедших с 1900 , поэтому добавляем 1900
 
     return 0;
 }
