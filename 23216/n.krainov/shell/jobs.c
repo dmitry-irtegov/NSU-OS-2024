7e60275844992fd8b865f44324656870382c5494
Nikita Krainov <n.krainov@g.nsu.ru>
2024-11-19 14:37:11 +0700

    Shell (#429)
    accepted

---
+++ 23216/n.krainov/shell/jobs.c
@@ -0,0 +1,244 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <termios.h>
+#include "shell.h"
+
+Job* head = NULL;
+extern int flagTTY;
+
+void createProcess(int in, int out, Command* cmd, pid_t pgid) {
+    pid_t pid = getpid();
+    if (pgid == 0) {
+        if (setpgid(pid, pid)) {
+            perror("createProcess");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else {
+        if (setpgid(pid, pgid)) {
+            perror("createProcess");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (in != -1) {
+        if (dup2(in, STDIN_FILENO)) {
+            perror("createProcess");
+            exit(EXIT_FAILURE);
+        }
+        close(in);
+    } 
+    else if (cmd->infile != NULL) {
+        int newIn = open(cmd->infile, O_RDONLY);
+        if (newIn == -1 || dup2(newIn, STDIN_FILENO) == -1) {
+            perror("createProcess");
+            exit(EXIT_FAILURE);
+        }
+        close(newIn);
+    }
+
+    if (out != -1) {
+        if (dup2(out, STDOUT_FILENO) == -1) {
+            perror("createProcess");
+            exit(EXIT_FAILURE);
+        }
+        close(out);
+    } 
+    else if (cmd->outfile != NULL) {
+        int newOut;
+        if (cmd->flags & APP) {
+            newOut = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+        }
+        else {
+            newOut = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        }
+        
+        if (newOut == -1 || dup2(newOut, STDOUT_FILENO) == -1) {
+            perror("createProcess");
+            exit(EXIT_FAILURE);
+        }
+        close(newOut);
+    }
+
+    sigset(SIGINT, SIG_DFL);
+    sigset(SIGTSTP, SIG_DFL);
+    sigset(SIGQUIT, SIG_DFL);
+    sigset(SIGTTOU, SIG_DFL);
+    sigset(SIGTTIN, SIG_DFL);
+
+    switch (cmd->isShellSpecific) {
+        case JOBS:
+            jobs();
+            break;
+        case FG:
+            fg(cmd);
+            break;
+        case BG:
+            bg(cmd);
+            break;
+    }
+    execvp(cmd->cmdargs[0], cmd->cmdargs);
+    perror("exec");
+    exit(EXIT_FAILURE);
+}
+
+void waitJob(Job* j) {
+    int status;
+    pid_t pid;
+    do {
+        pid = waitpid(-j->pgid, &status, WUNTRACED);
+    } while (!updateInfoPid(j, pid, status) && !isStoppedJob(j) && !isCompletedJob(j));
+}
+
+int sendSIGCONT(pid_t pgid) {
+    return kill(-pgid, SIGCONT);
+}
+
+int foregroundJob(Job* j, int continueJob) {
+    if (flagTTY && tcsetpgrp(STDIN_FILENO, j->pgid)) {
+        return -1;
+    }
+    
+    if (continueJob) {
+        if (sendSIGCONT(j->pgid)) {
+            return -1;
+        }
+    }
+
+    waitJob(j);
+    if (flagTTY && tcsetpgrp(STDIN_FILENO, getpgrp())) {
+        return -1;
+    }
+
+    if (isCompletedJob(j)) {
+        freeJob(j);
+    }
+    return 0;
+}
+
+void createJobs(Conv* conv) {
+    Job* tail;
+    Process* curproc = NULL;
+    for (Conv* curconv = conv; curconv; curconv = curconv->next) {
+        if (conv->cmd->next == NULL) {
+            if (conv->cmd->isShellSpecific == FG) {
+                fg(conv->cmd);
+                continue;
+            }
+            else if (conv->cmd->isShellSpecific == BG) {
+                bg(conv->cmd);
+                continue;
+            }
+        }
+
+        if (head == NULL) {
+            head = calloc(1, sizeof(Job));
+            if (head == NULL) {
+                perror("createJobs failed");
+                return;
+            }   
+            head->number = 1;
+            tail = head;
+        }
+        else {
+            tail = head;
+            while (tail->next != NULL) {
+                tail = tail->next;
+            }
+            tail->next = calloc(1, sizeof(Job));
+            if (tail->next == NULL) {
+                perror("createJobs failed");
+                return;
+            }
+            tail->next->number = tail->number+1;
+            tail->next->prev = tail;
+            tail = tail->next;
+        }
+
+        
+        int filedes[2];
+        filedes[0] = -1;
+        filedes[1] = -1;
+        int in = -1, out = -1;
+        for (Command* cmd = curconv->cmd; cmd; cmd = cmd->next) {
+            if (cmd->next) {
+                pipe(filedes);
+                out = filedes[1];
+            }
+
+            pid_t pid = fork();
+
+            switch (pid) {
+                case -1:
+                    perror("createJobs failed");
+                    return;
+                case 0:
+                    createProcess(in, out, cmd, tail->pgid);
+                    break;
+                default:
+                    if (tail->pgid == 0) {
+                        tail->pgid = pid;
+                    }
+                    if (tail->p == NULL) {
+                        tail->p = calloc(1, sizeof(Process));
+                        if (tail->p == NULL) {
+                            perror("createJobs failed");
+                            return;
+                        }
+                        tail->p->pid = pid;
+                        curproc = tail->p;
+                    }
+                    else {
+                        curproc->next = calloc(1, sizeof(Process));
+                        if (curproc->next == NULL) {
+                            perror("createJobs failed");
+                            return;
+                        }
+                        curproc = curproc->next;
+                        curproc->pid = pid;
+                    }
+                    if (setpgid(pid, tail->pgid)) {
+                        perror("createJobs failed");
+                        return;
+                    }
+
+                    for (int i = 0; i < cmd->count_args; i++) {
+                        curproc->cmdargs[i] = calloc(strlen(cmd->cmdargs[i]) + 1, sizeof(char));
+                        if (curproc->cmdargs[i] == NULL) {
+                            perror("createJobs failed");
+                            return;
+                        }
+                        strcpy(curproc->cmdargs[i], cmd->cmdargs[i]);
+                    }
+                    break;
+            }
+            if (in != -1 && close(in) == -1) {
+                perror(NULL);
+            }
+            in = filedes[0];
+            if (out != -1 && close(out) == -1) {
+                perror(NULL);
+            }
+            
+            out = -1;
+        }
+        
+        if (!curconv->bg) {
+            foregroundJob(tail, 0);
+        }
+        else {
+            fprintf(stderr, "[%d] ", tail->number);
+            for (Process* p = tail->p; p; p = p->next) {
+                if (p->next == NULL) {
+                    fprintf(stderr, "%d\n", p->pid);
+                }   
+            }
+        }
+    }
+}
---
+++ 23216/n.krainov/shell/shell.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <sys/types.h>
+
+
+#define MAXARGS 256
+
+#define RUNNING 0
+#define DONE 1
+#define STOP 2
+#define KILLEDBYSIGNAL 3
+
+typedef struct command{
+    char *cmdargs[MAXARGS];
+    int count_args;
+    char* infile;
+    char* outfile;
+    char isShellSpecific; 
+    char flags; 
+    struct command* next;
+    struct command* prev;
+}Command;
+
+typedef struct conv {
+    struct conv* next;
+    Command* cmd;
+    char bg;
+}Conv;
+
+typedef struct process{
+    struct process* next;
+    char *cmdargs[MAXARGS];
+    pid_t pid;
+    int state;
+    int status;
+}Process;
+
+typedef struct job{
+    int number;
+    struct job* next;
+    struct job* prev;
+    Process* p;
+    pid_t pgid;
+    char notified;
+} Job;
+
+#define OUT  1
+#define APP   2
+
+#define FG 1
+#define BG 2
+#define JOBS 3
+
+int parseline(char* line, Conv* task);
+int promptline(char *line, int sizline);
+
+void createJobs(Conv* conv);
+void updateInfoJobs(int printInfo);
+
+int isStoppedJob(Job* j);
+int isCompletedJob(Job* j);
+
+int updateInfoPid(Job* j, pid_t pid, int status);
+
+int foregroundJob(Job* j, int continueJob);
+
+int sendSIGCONT(pid_t pgid);
+
+void fg(Command* cmd);
+void bg(Command* cmd);
+void jobs();
+
+void freeJob(Job* j);
+
+void freeSpace(Conv* task);
