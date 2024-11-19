fbda4204f7b4d536b6da4d347773d27a7f28b071
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2024-11-19 15:34:34 +0700

    lab31

--- 23216/Faranosov/lab31/server.c
+++ 23216/Faranosov/lab31/server.c
@@ -115,7 +115,7 @@ void readInfoFromClient(int fd, int pfdNum) {
 				break;
 			}
 
-			
+			printf("write %d\n", fd);
 			for (int i = 0; i < cntOfBytesGetted; i++) {
 				str[i] = toupper(str[i]);
 			}
