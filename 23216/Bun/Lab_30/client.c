7ad87ab918c37c5d9bca565b6c30b72fdc2691c1
PavelBun <144594950+PavelBun@users.noreply.github.com>
2024-11-21 17:40:52 +0700

    Update client.c

--- 23216/Bun/Lab_30/client.c
+++ 23216/Bun/Lab_30/client.c
@@ -5,19 +5,26 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <signal.h>
 
 #define SOCKET_PATH "/tmp/socket"
 #define BUFFER_SIZE 1024
+
+void lostConnection() {
+    write(STDERR_FILENO, "Lost server connection\n", strlen("Lost server connection\n") + 1);
+    _exit(EXIT_FAILURE);
+}
 
 int main() {
     int client_fd;
     struct sockaddr_un server_addr;
     char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
 
     // create a socket
     client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (client_fd == -1) {
-        perror("Failed to create socket");
+        perror("Failed to create socket\n");
         exit(EXIT_FAILURE);
     }
 
@@ -28,26 +35,37 @@ int main() {
 
     // connect to the server
     if (connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Failed to connect to server");
+        perror("Failed to connect to server\n");
         close(client_fd);
         exit(EXIT_FAILURE);
     }
 
-    printf("Enter a string to send to the server: ");
-     if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
-        perror("Failed to read the string");
-        close(client_fd);
-        exit(EXIT_FAILURE);
+    signal(SIGPIPE, lostConnection);
+    printf("Enter a string to send to the server (type 'exit' to quit):\n");
+
+    while (1) {
+        bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        if (bytes_read == -1) {
+            perror("Failed to read input\n");
+            break;
+        } else if (bytes_read == 0) {
+            fprintf(stderr, "EOF\n");
+            break;
+        }
+
+        // if you want to exit
+        if (strncmp(buffer, "exit", 4) == 0) {
+            break;
+        }
+
+        // send the string to the server
+        if (write(client_fd, buffer, bytes_read) == -1) {
+            perror("Failed to send data to server\n");
+            close(client_fd);
+            exit(EXIT_FAILURE);
+        }
     }
 
-    // send the string to the server
-    if (write(client_fd, buffer, strlen(buffer)) == -1) {
-        perror("Failed to send data to server");
-        close(client_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    // close the socket
     close(client_fd);
 
     exit(EXIT_SUCCESS);
