0454d5aadaa8aeb2ab228faa642bc953ac886470
Alena Savushkina <a.savushkina@g.nsu.ru>
2024-11-26 03:08:17 +0700

    add data chunks??? remove port connecting (now its just a file) and buffer now is much smaller

--- 23216/a.savushkina/lab30/lab30-client.c
+++ 23216/a.savushkina/lab30/lab30-client.c
@@ -5,15 +5,29 @@
 #include <sys/un.h>
 #include <unistd.h>
 
-#define BUF_SIZE 500
+#define BUF_SIZE 30
+
+void send_in_chunks(int sfd, const char *msg, size_t len) {
+    size_t offset = 0;
+
+    while (offset < len) {
+        size_t chunk_size = (len - offset) > BUF_SIZE ? BUF_SIZE : (len - offset);
+        if (write(sfd, msg + offset, chunk_size) != (ssize_t)chunk_size) {
+            fprintf(stderr, "partial/failed write\n");
+            close(sfd);
+            exit(EXIT_FAILURE);
+        }
+        offset += chunk_size;
+    }
+}
 
 int main(int argc, char *argv[]) {
     int sfd;
-    size_t len;
+    char name[13] = "test_af_unix";
     struct sockaddr_un addr;
 
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s socket_path msg...\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s msg...\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -25,7 +39,7 @@ int main(int argc, char *argv[]) {
 
     memset(&addr, 0, sizeof(struct sockaddr_un));
     addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, argv[1], sizeof(addr.sun_path) - 1);
+    strncpy(addr.sun_path, name, sizeof(addr.sun_path) - 1);
 
     if (connect(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
         perror("connect");
@@ -33,20 +47,11 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    for (size_t j = 2; j < (size_t)argc; j++) {
-        len = strlen(argv[j]) + 1;
+    for (size_t j = 1; j < (size_t)argc; j++) {
+        size_t len = strlen(argv[j]) + 1;
 
-        if (len > BUF_SIZE) {
-            fprintf(stderr,
-                    "Ignoring long message in argument %zu\n", j);
-            continue;
-        }
+        send_in_chunks(sfd, argv[j], len);
 
-        if (write(sfd, argv[j], len) != (ssize_t)len) {
-            fprintf(stderr, "partial/failed write\n");
-            close(sfd);
-            exit(EXIT_FAILURE);
-        }
     }
 
     if (write(sfd, "end", 4) != 4) {
--- 23216/a.savushkina/lab30/lab30-server.c
+++ 23216/a.savushkina/lab30/lab30-server.c
@@ -5,21 +5,17 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include <ctype.h>
-#define BUF_SIZE 500
+#define BUF_SIZE 30
 
-int main(int argc, char *argv[]) {
+int main() {
     int sfd;
     char buf[BUF_SIZE];
+    char name[13] = "test_af_unix";
     ssize_t nread;
     socklen_t peer_addrlen;
     struct sockaddr_un addr;
     struct sockaddr_un peer_addr;
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s socket_path\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
+    
     sfd = socket(AF_UNIX, SOCK_DGRAM, 0);
     if (sfd == -1) {
         perror("socket");
@@ -28,8 +24,8 @@ int main(int argc, char *argv[]) {
 
     memset(&addr, 0, sizeof(struct sockaddr_un));
     addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, argv[1], sizeof(addr.sun_path) - 1);
-    unlink(argv[1]); 
+    strncpy(addr.sun_path, name, sizeof(addr.sun_path) - 1);
+    unlink(name); 
     if (bind(sfd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) == -1) {
         perror("bind");
         close(sfd);
@@ -37,6 +33,7 @@ int main(int argc, char *argv[]) {
     }
 
     for (;;) {
+        memset(&buf, 0, sizeof(buf));
         peer_addrlen = sizeof(peer_addr);
         nread = recvfrom(sfd, buf, BUF_SIZE, 0,
                          (struct sockaddr *)&peer_addr, &peer_addrlen);
@@ -48,26 +45,26 @@ int main(int argc, char *argv[]) {
         if (strcmp(buf, "end") == 0) {
             printf("end of server\n");
             close(sfd);
-            unlink(argv[1]);
+            unlink(name);
             exit(EXIT_SUCCESS);
         }
 
         for (size_t i = 0; i < strlen(buf); i++) {
             buf[i] = toupper(buf[i]);
         }
-        if (strlen(buf) < 500) {
+        if (strlen(buf) < 30) {
             buf[strlen(buf)] = '\n';
         }
 
         if (write(fileno(stdin), buf, strlen(buf)) == -1) {
             perror("error in write");
             close(sfd);
-            unlink(argv[1]); 
+            unlink(name); 
             exit(EXIT_FAILURE);
         }
     }
 
     close(sfd);
-    unlink(argv[1]);
+    unlink(name);
     return 0;
 }
