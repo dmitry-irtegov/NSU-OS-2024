59e341f70b6e934c3bbf92165b55b29958661930
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2024-12-02 15:58:35 +0700

    lab32

---
+++ 23213/v.abramenko/lab32/client.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/un.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+char *socket_path = "./socket";
+
+int main(int argc, char *argv[]) {
+    struct sockaddr_un addr;
+    int fd;
+
+    if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket failed");
+        exit(-1);
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+
+    if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("connection failed");
+        close(fd);
+        exit(-1);
+    }
+
+    char buf[BUF_SIZE];
+    ssize_t bytes_read;
+    while ((bytes_read = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+        ssize_t current_sent = write(fd, buf, MIN(bytes_read, sizeof(buf)));
+        if (current_sent == -1) {
+            perror("Failed to write to server socket");
+            exit(-1);
+        }
+    }
+
+    if (bytes_read == -1) {
+        perror("read failed");
+        close(fd);
+        exit(-1);
+    }
+
+    close(fd);
+    exit(0);
+} 
---
+++ 23213/v.abramenko/lab32/server.c
@@ -0,0 +1,186 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <errno.h>
+#include <aio.h>
+#include <signal.h>
+#include <setjmp.h>
+#include <sys/siginfo.h>
+
+#define MAX_CLIENTS 10
+#define BUF_SIZE 1024
+
+typedef struct request_s {
+    int completed;
+    struct aiocb* req;
+}request;
+
+char* socket_path = "./socket";
+
+void close_handler(int sig) {
+	unlink(socket_path);
+	_exit(0);
+}
+
+sigjmp_buf toprocess;
+
+void sigiohandler(int signo, siginfo_t* info, void* context){
+    if (signo != SIGIO || info->si_signo != SIGIO){
+        return;
+    }
+    
+    request* req = (request*)info->si_value.sival_ptr;
+    if (aio_error(req->req) == 0) {
+        req->completed = 1;
+        siglongjmp(toprocess, 1);
+    }
+}
+
+request* create_request(int cl) {
+    request* request = malloc(sizeof(request));
+    if (request == NULL) {
+        perror("malloc failed");
+        close(cl);
+        return NULL;
+    }
+    request->completed = 0;
+
+    request->req = malloc(sizeof(struct aiocb));
+    if (request->req == NULL) {
+        perror("malloc failed");
+        free(request);
+        close(cl);
+        return NULL;
+    }
+
+    request->req->aio_fildes = cl;
+    request->req->aio_offset = 0;
+    request->req->aio_buf = malloc(BUF_SIZE);
+    if (request->req->aio_buf == NULL) {
+        perror("malloc failed");
+        free(request->req);
+        free(request);
+        close(cl);
+        return NULL;
+    }
+
+    request->req->aio_nbytes = BUF_SIZE - 1;
+    request->req->aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+    request->req->aio_sigevent.sigev_signo = SIGIO;
+    request->req->aio_sigevent.sigev_value.sival_ptr = request;
+
+    if (aio_read(request->req) == -1) {
+        perror("aio_read failed");
+        char* buf = (char*)request->req->aio_buf;
+        free(buf);
+        free(request->req);
+        free(request);
+        close(cl);
+        return NULL;
+    }
+
+    return request;
+}
+
+void clear_request(request* request) {
+    char* buf = (char*)request->req->aio_buf;
+    free(buf);
+    close(request->req->aio_fildes);
+    free(request->req);
+    free(request);
+    request = NULL;
+}
+
+int main() {
+    struct sockaddr_un addr;
+    int fd;
+
+    if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("socket failed");
+        exit(-1);
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+
+    if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("binding failed");
+        close(fd);
+        exit(-1);
+    }
+
+    if (listen(fd, MAX_CLIENTS) == -1) {
+        perror("listen failed");
+        unlink(socket_path);
+        close(fd);
+        exit(-1);
+    }
+
+    signal(SIGINT, close_handler);
+
+    struct sigaction sigiohandleraction;
+    sigemptyset(&sigiohandleraction.sa_mask);
+    sigaddset(&sigiohandleraction.sa_mask, SIGIO);
+
+    sigiohandleraction.sa_sigaction = sigiohandler;
+    sigiohandleraction.sa_flags = SA_SIGINFO;
+    sigaction(SIGIO, &sigiohandleraction, NULL);
+
+    int cl;
+    int cnt = 0;
+    request* requests[MAX_CLIENTS];
+    memset(requests, 0, sizeof(requests));
+    while (1) {
+        sigprocmask(SIG_BLOCK, &sigiohandleraction.sa_mask, NULL);
+        if (sigsetjmp(toprocess, 1) != 0) {
+            for (int i = 0; i < cnt; i++) {
+                if (!requests[i]->completed) {
+                    continue;
+                }
+                
+                int rc = aio_return(requests[i]->req);
+                if (rc <= 0) {
+                    if (rc == -1) {
+                        perror("return failed");
+                    }
+                    clear_request(requests[i]);
+                    requests[i] = requests[cnt - 1];
+                    requests[--cnt] = NULL;
+                    i--;
+                } else {
+                    char* buf = (char*)requests[i]->req->aio_buf;
+                    buf[rc] = 0;
+                    for (int j = 0; j < rc; j++) {
+                        putchar(toupper((unsigned char)buf[j]));
+                    }
+                    requests[i]->completed = 0;
+                    if (aio_read(requests[i]->req) == -1) {
+                        clear_request(requests[i]);
+                        requests[i] = requests[cnt - 1];
+                        requests[--cnt] = NULL;
+                        i--;
+                    }
+                }
+            }
+        }
+        sigprocmask(SIG_UNBLOCK, &sigiohandleraction.sa_mask, NULL);
+
+        if ((cl = accept(fd, NULL, NULL)) == -1) {
+            perror("accept failed");
+            continue;
+        }
+
+        sigprocmask(SIG_BLOCK, &sigiohandleraction.sa_mask, NULL);
+        requests[cnt] = create_request(cl);
+        if (requests[cnt] != NULL) {
+            cnt++;
+        }
+        sigprocmask(SIG_UNBLOCK, &sigiohandleraction.sa_mask, NULL);
+    }
+}
