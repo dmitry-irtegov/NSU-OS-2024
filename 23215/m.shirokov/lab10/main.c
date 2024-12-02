2441c8083ba2907a423cd56b7e41844e07d606cb
BarDirus <59092357+BarDirus@users.noreply.github.com>
2024-12-02 20:28:28 +0700

    add

--- 23215/m.shirokov/lab10/main.c
+++ 23215/m.shirokov/lab10/main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <command> [args...]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     pid_t pid;
     int status;
     pid = fork();
@@ -15,13 +22,19 @@ int main(int argc, char *argv[]) {
         perror("execvp failed");
         exit(EXIT_FAILURE);
     } else {
+        printf("Parent process PID: %d, Child process PID: %d\n", getpid(), pid);
         if (waitpid(pid, &status, 0) == -1) {
             perror("waitpid failed");
             exit(EXIT_FAILURE);
         }
         if (WIFEXITED(status)) {
             printf("Child process exited with code %d\n", WEXITSTATUS(status));
+        } else if (WIFSIGNALED(status)) {
+            printf("Child process terminated by signal %d\n", WTERMSIG(status));
+        } else {
+            printf("Child process terminated abnormally\n");
         }
     }
+
     return 0;
 }
