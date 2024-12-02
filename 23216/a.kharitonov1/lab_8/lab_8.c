fcf33d91096fd636ee83e00dc061ea0c5e1ce3bc
AlexCharitonov <alexcharitonov307@mail.ru>
2024-12-02 21:54:00 +0700

    lab_8

--- 23216/a.kharitonov1/lab_8/lab_8.c
+++ 23216/a.kharitonov1/lab_8/lab_8.c
@@ -10,7 +10,7 @@ int main(int argc, char** argv){
         exit(EXIT_FAILURE);
     }
     struct flock lock;
-    int file = open(argv[1], O_RDWR | O_CREAT);
+    int file = open(argv[1], O_RDWR | O_CREAT, 0777);
     if (file == -1){
         perror("problem in file open");
         exit(EXIT_FAILURE);
@@ -27,18 +27,36 @@ int main(int argc, char** argv){
     char* cmd = malloc(strlen("nano ") + strlen(argv[1]) + 1);
     if (!cmd){
         perror("problem in malloc");
+        lock.l_type = F_UNLCK;
+        if (fcntl(file, F_SETLK, &lock) == -1){
+            perror("problem in unlock");
+            close(file);
+            exit(EXIT_FAILURE);
+        }
         close(file);
         exit(EXIT_FAILURE);
     }
     if (snprintf(cmd, strlen("nano ") + strlen(argv[1]) + 1, "nano %s", argv[1]) < 0){
         perror("problem in snprintf");
         free(cmd);
+        lock.l_type = F_UNLCK;
+        if (fcntl(file, F_SETLK, &lock) == -1){
+            perror("problem in unlock");
+            close(file);
+            exit(EXIT_FAILURE);
+        }
         close(file);
         exit(EXIT_FAILURE);
     }
     if (system(cmd) == -1){
         perror("failed in nano");
         free(cmd);
+        lock.l_type = F_UNLCK;
+        if (fcntl(file, F_SETLK, &lock) == -1){
+            perror("problem in unlock");
+            close(file);
+            exit(EXIT_FAILURE);
+        }
         close(file);
         exit(EXIT_FAILURE);
     }
@@ -46,6 +64,12 @@ int main(int argc, char** argv){
     if (fcntl(file, F_SETLK, &lock) == -1){
         perror("problem in unlock");
         free(cmd);
+        lock.l_type = F_UNLCK;
+        if (fcntl(file, F_SETLK, &lock) == -1){
+            perror("problem in unlock");
+            close(file);
+            exit(EXIT_FAILURE);
+        }
         close(file);
         exit(EXIT_FAILURE);
     }
