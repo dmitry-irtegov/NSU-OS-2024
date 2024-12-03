2c9be8605fa0ead6e2849974262523b40702573c
KolyaPronin <145115145+KolyaPronin@users.noreply.github.com>
2024-12-03 12:39:07 +0700

    23217\n.pronin\lab9 (#451)
    * lab9
    
    * Update lab9.c

---
+++ 23217/n.pronin/lab9/lab9.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(){
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+
+        perror("fork error");
+        exit(1);
+    }
+
+    else if ( pid == 0) {
+
+        execlp("cat", "cat", "longfile.txt", NULL); 
+        perror("execlp failed");
+        exit(1);
+    }
+
+     else { 
+
+        printf("This is the parent process.\n");
+        wait(NULL); 
+        printf("child process executed");
+    }
+    return 0;
+}
