373643857046f0ea8396c5adc25550b9278d76fd
ttomilov <t.tomilov@g.nsu.ru>
2024-12-03 15:34:11 +0700

    lab8

--- 23216/t.tomilov/lab8/lab8.c
+++ 23216/t.tomilov/lab8/lab8.c
@@ -12,6 +12,18 @@ int main(int argc, char** argv){
 
     struct flock lock;
 
+    char* nano = malloc(strlen("nano ") + strlen(argv[1]) + 1);
+    if (!nano){
+        perror("ERROR: Memory allocation failed!");
+        exit(EXIT_FAILURE);
+    }
+
+    if (snprintf(nano, strlen("nano ") + strlen(argv[1]) + 1, "nano %s", argv[1]) < 0){
+        perror("ERROR: Failed in sprintf!");
+        free(nano);
+        exit(EXIT_FAILURE);
+    }
+
     int fd = open(argv[1], O_RDWR);
     if (fd == -1){
         perror("ERROR: File doesn't exist or access denied!");
@@ -25,24 +37,6 @@ int main(int argc, char** argv){
 
     if (fcntl(fd, F_SETLKW, &lock) == -1){
         perror("ERROR: Failed to lock the file!");
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    char* nano = malloc(strlen("nano ") + strlen(argv[1]) + 1);
-    if (!nano){
-        perror("ERROR: Memory allocation failed!");
-        lock.l_type = F_UNLCK;
-        fcntl(fd, F_SETLK, &lock);
-        close(fd);
-        exit(EXIT_FAILURE);
-    }
-
-    if (snprintf(nano, strlen("nano ") + strlen(argv[1]) + 1, "nano %s", argv[1]) < 0){
-        perror("ERROR: Failed in sprintf!");
-        lock.l_type = F_UNLCK;
-        fcntl(fd, F_SETLK, &lock);
-        free(nano);
         close(fd);
         exit(EXIT_FAILURE);
     }
