cb7549354d7a3a51aab56c22b225ed88062aa41e
A-GigaC <90515245+A-GigaC@users.noreply.github.com>
2024-12-03 15:08:47 +0700

    Create lab21.c

---
+++ 23216/f.pivovarov/lab21.c
@@ -0,0 +1,35 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int counter = 0;
+
+void sigHandler(int sig) {
+    switch (sig) { 
+    case SIGQUIT: ;
+        printf("Total num of beeps: %d", counter);
+        return ;
+    case SIGINT: ;
+        printf("\a");
+        counter++;
+    }
+}
+
+int main() {
+    if (sigset(SIGINT, sigHandler) == -1) {
+        perror("Failed to set SIGINT handler");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sigset(SIGQUIT, sigHandler) == -1) {
+        perror("Failed to set SIGQUIT handler");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        pause();
+    }
+
+    exit(EXIT_SUCCESS);
+}
