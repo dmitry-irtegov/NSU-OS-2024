5cc8d7ac30c232e9ee9e50de91c8c3a234ba84bc
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2024-12-05 11:39:00 +0700

    made lab20

---
+++ 23213/a.chuvashov/lab20/lab20.c
@@ -0,0 +1,28 @@
+#include <glob.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+
+int errfunc(const char* msg, int c) {
+    if (c != ENOTDIR) {
+        fprintf(stderr, "%s: %s\n", msg, strerror(c));
+    }
+    return 0;
+}
+
+int main() {
+    glob_t storage;
+    char buffer[BUFSIZ];
+    scanf("%s", buffer);
+    
+    if (glob(buffer, 0, &errfunc, &storage) == GLOB_NOMATCH) {
+        printf("No files matched with pattern %s\n", buffer);
+        return -1;
+    }
+
+    for (size_t i = 0; i < storage.gl_pathc; i++) {
+        printf("%s\n", storage.gl_pathv[i]);
+    }    
+    globfree(&storage);
+    return 0;
+}
