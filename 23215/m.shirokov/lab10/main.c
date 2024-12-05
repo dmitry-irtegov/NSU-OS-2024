9bafe62ba8b33b0273f21ce4afa52d7187f6cb49
BarDirus <59092357+BarDirus@users.noreply.github.com>
2024-12-05 20:28:56 +0700

    Shirokov_23215_lab10 (#570)
    * Create hello.txt
    
    * AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    
    * add
    
    * add
    
    * add

---
+++ 23215/m.shirokov/lab10/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <command> [args...]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t pid;
+    int status;
+    pid = fork();
+    if (pid < 0) {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        execvp(argv[1], &argv[1]);
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+    } else {
+        printf("Parent process ID: %d, Child process ID: %d\n", getpid(), pid);
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid failed");
+            exit(EXIT_FAILURE);
+        }
+        if (WIFEXITED(status)) {
+            printf("Child process exited with code %d\n", WEXITSTATUS(status));
+        } else if (WIFSIGNALED(status)) {
+            printf("Child process terminated by signal %d\n", WTERMSIG(status));
+        } else {
+            printf("Child process terminated abnormally\n");
+        }
+    }
+
+    return 0;
+}
