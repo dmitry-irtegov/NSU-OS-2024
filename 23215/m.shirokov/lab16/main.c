7eaa2ec6903bf813a0b36304ab1b4944d3490908
BarDirus <59092357+BarDirus@users.noreply.github.com>
2024-12-07 20:23:56 +0700

    add

--- 23215/m.shirokov/lab16/main.c
+++ 23215/m.shirokov/lab16/main.c
@@ -26,18 +26,18 @@ void restore_terminal_mode(struct termios *old_tio) {
 }
 int main() {
     struct termios old_tio;
-    char answer;
     set_terminal_mode(&old_tio);
     printf("Yes or no? (y/n): ");
-    answer = getchar();
-    printf("\n");
+    static char answer[2];
+    fgets(answer,2,stdin);
     restore_terminal_mode(&old_tio);
-    if (answer == 'y' || answer == 'Y') {
+    printf("\n");
+    if (answer[0] == 'y' || answer[0] == 'Y') {
         printf("Yes!\n");
-    } else if (answer == 'n' || answer == 'N') {
+    } else if (answer[0] == 'n' || answer[0] == 'N') {
         printf("No!\n");
     } else {
-        printf("Invalid response: %c\n", answer);
+        printf("Invalid response: %c\n", answer[0]);
     }
     return 0;
 }
