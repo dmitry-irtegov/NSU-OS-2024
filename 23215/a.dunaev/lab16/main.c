ee7e868df1b540c5cd1d3b9a44b6deb435967f4e
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2024-12-09 15:42:43 +0700

    lab 16 insane tricks

--- 23215/a.dunaev/lab16/main.c
+++ 23215/a.dunaev/lab16/main.c
@@ -7,32 +7,46 @@
 struct termios old, new;
 char response;
 
-void handle(int sig){
-	if (tcgetattr(STDIN_FILENO, &old)){                             // E_BAD_F E_NO_TTY
-    	perror("Bad parameters");
-        exit(1);
+// Function to restore terminal settings on exit
+void restore_terminal() {
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &old)) {
+    	perror("Bad parameters or error in I/O");
+    	exit(1);	
     }
-	new = old;
-	new.c_lflag &= ~ICANON;
-	new.c_cc[VMIN] = 1;
-	if (tcsetattr(STDIN_FILENO, TCSANOW, &new)){        // E_BAD_F E_INTR E_IN_VAL E_NO_TTY  E_IO
-		perror("Bad parameters or error in I/O");
-	   	exit(1);
-	}
-	read(STDIN_FILENO, &response, 1);
-	if (tcsetattr(STDIN_FILENO, TCSANOW, &old)){
-		perror("Bad parameters or error in I/O");
-	    exit(1);
-	}
 }
 
-int main() {  
-    printf("Enter a single character: ");
-    fflush(stdout); 
-    
-    signal(SIGCONT, handle);    
+void handle(int sig) {
+    // Set terminal to non-canonical mode
+    if (tcgetattr(STDIN_FILENO, &old)) {
+        perror("Bad parameters");
+        exit(1);
+    }
+    new = old;
+    new.c_lflag &= ~ICANON;
+    new.c_cc[VMIN] = 1;
+    new.c_cc[VTIME] = 0;
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &new)) {
+        perror("Bad parameters or error in I/O");
+        exit(1);
+    }
+    read(STDIN_FILENO, &response, 1);
+}
 
+int main() {
+	printf("Enter a single character: ");
+    fflush(stdout);
+
+    // Set up signal handling
+    signal(SIGCONT, handle);
+
+    // Ensure terminal settings are restored on exit
+    atexit(restore_terminal);
+
+    // Initial terminal setup
     handle(SIGCONT);
+   
+    // Restore terminal settings before exiting
+    restore_terminal();
 
     printf("\nYou entered: %c\n", response);
 
