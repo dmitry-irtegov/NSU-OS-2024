1054973fafb29812fa0d344625f406ab53bcfe29
Nikolay Laptev <n.laptev1@g.nsu.ru>
2024-12-09 10:09:47 +0700

    lab16_sigcont_sol

--- 23215/n.laptev1/lab16/lab16.c
+++ 23215/n.laptev1/lab16/lab16.c
@@ -2,42 +2,50 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <signal.h>
+
+struct termios original_terminal_attributes;
+
+void sigcont_handler(int signo) {
+    tcsetattr(0, TCSANOW, &original_terminal_attributes);
+}
 
 int main() {
-    struct termios terminal_attributes, new_terminal_attributes;
-    
+    struct termios terminal_attributes;
     if (tcgetattr(0, &terminal_attributes) == -1) {
         perror("tcgetattr failed");
         exit(EXIT_FAILURE);
     }
 
-    new_terminal_attributes = terminal_attributes;
-    new_terminal_attributes.c_lflag &= ~(ICANON);
-    new_terminal_attributes.c_cc[VMIN] = 1;
-    
-    if (tcsetattr(0, TCSANOW, &new_terminal_attributes) == -1) {
+    original_terminal_attributes = terminal_attributes;
+
+    original_terminal_attributes.c_lflag &= ~(ICANON);
+    original_terminal_attributes.c_cc[VMIN] = 1;
+
+    if (tcsetattr(0, TCSANOW, &original_terminal_attributes) == -1) {
         perror("tcsetattr failed.");
         exit(EXIT_FAILURE);
     }
 
-    printf("How many team trophies does Harry Kane have? ");
+    signal(SIGCONT, sigcont_handler);
+
+    printf("How many team trophies does Harry Kane have?\n");
+
     char amount_of_cups = getchar();
 
-    
-    if (tcsetattr(0, TCSANOW, &terminal_attributes) == -1) {
-        perror("tcsetattr failed.");
-        exit(EXIT_FAILURE);
-    }
-
-    switch(amount_of_cups) {
+    switch (amount_of_cups) {
         case '0':
             printf("\nCorrect, he doesn't have any team cups.\n");
             break;
         default:
             printf("\nThat's not correct. He has 0 cups.\n");
             break;
-    } 
+    }
+
+    if (tcsetattr(0, TCSANOW, &terminal_attributes) == -1) {
+        perror("tcsetattr failed.");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
