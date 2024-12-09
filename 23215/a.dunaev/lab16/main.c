4bb4840d6bbb6d79a4ab43f7d967791270f5d449
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2024-12-09 14:32:39 +0700

    lab 16 (added ctrl+z)

--- 23215/a.dunaev/lab16/main.c
+++ 23215/a.dunaev/lab16/main.c
@@ -2,33 +2,49 @@
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
 
-int main() {
-    struct termios old, new;
-    
+struct termios old, new;
+char response;
+
+void handle(int sig){
+	new = old;
+	new.c_lflag &= ~ICANON;
+	new.c_cc[VMIN] = 1;
+	new.c_cc[VTIME] = 0;
+	if (tcsetattr(STDIN_FILENO, TCSANOW, &new)){        // E_BAD_F E_INTR E_IN_VAL E_NO_TTY  E_IO
+		perror("Bad parameters or error in I/O");
+	   	exit(1);
+	}
+	read(STDIN_FILENO, &response, 1);
+}
+
+int main() {  
     printf("Enter a single character: ");
-        fflush(stdout); 
+    fflush(stdout); 
         
-        if (tcgetattr(STDIN_FILENO, &old)){                             // E_BAD_F E_NO_TTY
-                perror("Bad parameters");
-                exit(1);
-        }
+    if (tcgetattr(STDIN_FILENO, &old)){                             // E_BAD_F E_NO_TTY
+    	perror("Bad parameters");
+        exit(1);
+    }
         
     new = old;
-        new.c_lflag &= ~ICANON;
-        new.c_cc[VMIN] = 1;
-        new.c_cc[VTIME] = 0;
+    new.c_lflag &= ~ICANON;
+    new.c_cc[VMIN] = 1;
+    new.c_cc[VTIME] = 0;
     if (tcsetattr(STDIN_FILENO, TCSANOW, &new)){        // E_BAD_F E_INTR E_IN_VAL E_NO_TTY  E_IO
         perror("Bad parameters or error in I/O");
-                exit(1);
+        exit(1);
     }
 
-    char response;
+	signal(SIGCONT, handle);
+	
     read(STDIN_FILENO, &response, 1);
-        if (tcsetattr(STDIN_FILENO, TCSANOW, &old)){
-                perror("Bad parameters or error in I/O");
-                exit(1);
-        }
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &old)){
+		perror("Bad parameters or error in I/O");
+       	exit(1);
+   	}
 
     printf("\nYou entered: %c\n", response);
 
