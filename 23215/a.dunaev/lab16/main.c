c7de854ca00dce87496517005ed475f85c44bf7f
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2024-12-09 15:11:57 +0700

    lab 16 test

--- 23215/a.dunaev/lab16/main.c
+++ 23215/a.dunaev/lab16/main.c
@@ -27,19 +27,10 @@ int main() {
     	perror("Bad parameters");
         exit(1);
     }
-        
-    new = old;
-    new.c_lflag &= ~ICANON;
-    new.c_cc[VMIN] = 1;
-    new.c_cc[VTIME] = 0;
-    if (tcsetattr(STDIN_FILENO, TCSANOW, &new)){        // E_BAD_F E_INTR E_IN_VAL E_NO_TTY  E_IO
-        perror("Bad parameters or error in I/O");
-        exit(1);
-    }
+    
+    signal(SIGCONT, handle);    
 
-	signal(SIGCONT, handle);
-	
-    read(STDIN_FILENO, &response, 1);
+    handle(SIGCONT);
 
     if (tcsetattr(STDIN_FILENO, TCSANOW, &old)){
 		perror("Bad parameters or error in I/O");
