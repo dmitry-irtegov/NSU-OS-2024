43b7ff9cf71cff5a34ccf8a650d4a345e466b393
Kirill <k.shelestov@g.nsu.ru>
2024-12-11 11:36:41 +0700

    lab18

--- 23213/k.shelestov/lab18/check_uid_max.c
+++ 23213/k.shelestov/lab18/check_uid_max.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <limits.h>
+
 int main() {
-    // Открываем файл, чтобы получить файловый дескриптор
-    int fd = open("/", O_RDONLY);  // Открываем корневой каталог
-
-    if (fd == -1) {
-        perror("open");
-        return 1;
-    }
-
-    long max_uid = fpathconf(fd, UID_MAX);  // Используем fpathconf
+    long max_uid = sysconf(_UID_MAX);
 
     if (max_uid == -1) {
-        perror("fpathconf");
-        close(fd);  // Закрываем файловый дескриптор
+        perror("sysconf");
         return 1;
     }
 
-    printf("Max UID: %ld\n", max_uid);
-    close(fd);  // Закрываем файловый дескриптор
+    printf("MAX UID: %ld\n", max_uid);
     return 0;
 }
 
