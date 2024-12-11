2cd104e4bf905bb4256a9d70a036a3ee7ff6cb4f
Kirill <k.shelestov@g.nsu.ru>
2024-12-11 11:17:50 +0700

    lab18

--- 23213/k.shelestov/lab18/check_uid_max.c
+++ 23213/k.shelestov/lab18/check_uid_max.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <limits.h>
 
 int main() {
-    long max_uid = sysconf(_SC_UID_MAX);
+    long max_uid = sysconf(UID_MAX);
 
     if (max_uid == -1) {
         perror("sysconf");
