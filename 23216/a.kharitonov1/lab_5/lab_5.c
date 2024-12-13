fa96e0b8490c91dc94c6ab1b7849c3579d8f2474
AlexCharitonov <alexcharitonov307@mail.ru>
2024-12-13 22:35:03 +0700

    lab_5

--- 23216/a.kharitonov1/lab_5/lab_5.c
+++ 23216/a.kharitonov1/lab_5/lab_5.c
@@ -16,18 +16,30 @@ typedef struct table_for_file {
     int cap;
 } table_for_file;
 
-int main(int argc, char *argv[]) {
-    table_for_file file_table;
-    file_table.elems = NULL;
-    if (argc != 2) {
-        perror("you have to give file name and nothing else");
-        exit(EXIT_FAILURE);
+int file;
+table_for_file file_table;
+
+void freeAndCloseOnFail(){
+    free(file_table.elems);
+    close(file);
+    exit(EXIT_FAILURE);
+}
+void addLine(off_t cur_len, off_t cur_off){
+    if (file_table.cap == file_table.cur) {
+        file_table.cap *= 2;
+        file_table.elems = realloc(file_table.elems, sizeof(elem_of_table_off_t) * file_table.cap);
+        if (file_table.elems == NULL) {
+            perror("problem in realloc");
+            close(file);
+            exit(EXIT_FAILURE);
+        }
     }
-    int file = open(argv[1], O_RDONLY);
-    if (file == -1){
-        perror("problem in open");
-        exit(EXIT_FAILURE);
-    }
+    file_table.elems[file_table.cur].len = cur_len;
+    file_table.elems[file_table.cur].off = cur_off;
+    file_table.cur++;
+}
+
+int makeTable(){
     file_table.cur = 0;
     file_table.cap = 20;
     file_table.elems = malloc(sizeof(elem_of_table_off_t) * 20);
@@ -46,18 +58,7 @@ int main(int argc, char *argv[]) {
         for (ssize_t i = 0; i < read_len; i++) {
             cur_len++;
             if (buf[i] == '\n') {
-                if (file_table.cap == file_table.cur) {
-                    file_table.cap *= 2;
-                    file_table.elems = realloc(file_table.elems, sizeof(elem_of_table_off_t) * file_table.cap);
-                    if (file_table.elems == NULL) {
-                        perror("problem in realloc");
-                        close(file);
-                        exit(EXIT_FAILURE);
-                    }
-                }
-                file_table.elems[file_table.cur].len = cur_len;
-                file_table.elems[file_table.cur].off = cur_off;
-                file_table.cur++;
+                addLine(cur_len, cur_off);
                 cur_off += cur_len;
                 cur_len = 0;
             }
@@ -65,26 +66,16 @@ int main(int argc, char *argv[]) {
     }
     if(read_len < 0){
         perror("problem in read");
-        free(file_table.elems);
-        close(file);
-        exit(EXIT_FAILURE);
+        freeAndCloseOnFail()
     }
     if (cur_len != 0){
-        if (file_table.cap == file_table.cur) {
-            file_table.cap *= 2;
-            file_table.elems = realloc(file_table.elems, sizeof(elem_of_table_off_t) * file_table.cap);
-            if (file_table.elems == NULL) {
-                perror("problem in realloc");
-                close(file);
-                exit(EXIT_FAILURE);
-            }
-        }
-        file_table.elems[file_table.cur].len = cur_len;
-        file_table.elems[file_table.cur].off = cur_off;
-        file_table.cur++;
+        addLine(cur_len, cur_off);
         cur_off += cur_len;
         cur_len = 0;
     }
+}
+
+void listenUser(){
     int num_of_line, res;
     char *string = NULL;
     puts("Enter line number (end programm enter 0)");
@@ -92,9 +83,7 @@ int main(int argc, char *argv[]) {
         res = scanf("%d", &num_of_line);
         if (res != 1){
             perror("problem in scanf, you should type 1 int number");
-            free(file_table.elems);
-            close(file);
-            exit(EXIT_FAILURE);
+            freeAndCloseOnFail()
         }
         if (num_of_line < 0){
             puts("wrong number");
@@ -108,29 +97,38 @@ int main(int argc, char *argv[]) {
             }
             string = calloc(file_table.elems[num_of_line - 1].len + 1, sizeof(char));
             if (string == NULL) {
-                perror("problem calloc");
-                free(file_table.elems);
-                close(file);
-                exit(EXIT_FAILURE);
+                perror("problem in calloc");
+                freeAndCloseOnFail()
             }
             if (lseek(file, file_table.elems[num_of_line - 1].off, SEEK_SET) == -1) {
-                perror("problem lseek");
-                free(file_table.elems);
+                perror("problem in lseek");
                 free(string);
-                close(file);
-                exit(EXIT_FAILURE);
+                freeAndCloseOnFail()
             }
             if (read(file, string, file_table.elems[num_of_line - 1].len) == -1) {
-                perror("problem lseek");
-                free(file_table.elems);
+                perror("problem in read");
                 free(string);
-                close(file);
-                exit(EXIT_FAILURE);
+                freeAndCloseOnFail()
             }
             printf("%s",string);
         }
         free(string);
     }
+}
+
+int main(int argc, char *argv[]) {
+    file_table.elems = NULL;
+    if (argc != 2) {
+        perror("you have to give file name and nothing else");
+        exit(EXIT_FAILURE);
+    }
+    file = open(argv[1], O_RDONLY);
+    if (file == -1){
+        perror("problem in open");
+        exit(EXIT_FAILURE);
+    }
+    makeTable();
+    listenUser();
     free(file_table.elems);
     close(file);
     exit(EXIT_SUCCESS);
