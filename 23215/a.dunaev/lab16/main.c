ed8d71cd51575a0dcc40f5235fc51158562a4260
7dorm <144042012+7dorm@users.noreply.github.com>
2024-12-16 09:52:08 +0700

    lab16 23215 Dunaev (#630)
    * lab16
    
    * lab 16 (added ctrl+z)
    
    * lab 16 test
    
    * lab16 remastered
    
    * lab16 ultra edition
    
    * LAB 16 MEGA SUPER EDITION
    
    * lab 16 test edition
    
    * lab 16 insane tricks
    
    * commit for lab16
    
    * test
    
    * Fixed read

---
+++ 23215/a.dunaev/lab16/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+struct termios old, new;
+char response;
+
+// Function to restore terminal settings on exit
+void restore_terminal() {
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &old)) {
+        perror("Bad parameters or error in I/O");
+        exit(1);        
+    }
+}
+
+void handle(int sig) {
+    // Set terminal to non-canonical mode
+    //if (tcgetattr(STDIN_FILENO, &old)) {
+    //    perror("Bad parameters");
+    //    exit(1);
+    //}
+    new = old;
+    new.c_lflag &= ~ICANON;
+    new.c_cc[VMIN] = 1;
+    new.c_cc[VTIME] = 0;
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &new)) {
+        perror("Bad parameters or error in I/O");
+        exit(1);
+    }
+}
+
+int main() {
+    if (tcgetattr(STDIN_FILENO, &old)) {
+        perror("bad Param");
+        exit(1);
+    }
+    printf("Enter a single character: ");
+    fflush(stdout);
+
+    // Set up signal handling
+    signal(SIGCONT, handle);
+
+    // Ensure terminal settings are restored on exit
+    atexit(restore_terminal);
+
+    // Initial terminal setup
+    handle(SIGCONT);
+    while (response == 0){
+    	read(STDIN_FILENO, &response, 1);
+   	}
+    // Restore terminal settings before exiting
+//    restore_terminal();
+
+    printf("\nYou entered: %c\n", response);
+
+    return 0;
+}
