fb5c8e3ace18cbc1ecc78d27c655cbbf130c0870
Ildar Fitkulin <159516390+filimonq@users.noreply.github.com>
2024-12-25 19:48:50 +0700

    lab30 Ildar FItkulin 23215 (#450)
    * lab30
    
    * makefile fix
    
    * sigint

---
+++ 23215/i.fitkulin/lab30/client.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <string.h>
+
+#define SIZE 1024
+char *socket_path = "socket";
+
+int main() {
+    struct sockaddr_un addr;
+
+    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd == -1) {
+        perror("socket error");
+        exit(EXIT_FAILURE);
+    }
+    
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
+
+    if (connect(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("connect error");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    
+    char buf[SIZE];
+    ssize_t bytes_read = 0;
+    while ((bytes_read = read(STDIN_FILENO, buf, SIZE)) > 0) {
+        if (write(fd, buf, bytes_read) == -1) {
+            perror("write error");
+            close(fd);
+            exit(EXIT_FAILURE);
+        }   
+    }
+
+    if (bytes_read == -1) {
+        perror("read error");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+    exit(EXIT_SUCCESS);
+}
---
+++ 23215/i.fitkulin/lab30/server.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <signal.h>
+
+#define SIZE 1024
+char *socket_path = "socket";
+
+void handle_sigint(int sig) {
+    unlink(socket_path);
+    _exit(EXIT_SUCCESS);
+}
+
+int main() {
+    struct sockaddr_un addr;
+
+    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd == -1) {
+        perror("socket error");
+        exit(EXIT_FAILURE);
+    }
+    
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
+
+    if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("bind error");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    
+    if (listen(fd, 1) == -1) {
+        perror("listen error");
+        close(fd);
+        unlink(socket_path);
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGINT, handle_sigint);
+
+    int accepted = accept(fd, NULL, NULL);
+    if (accepted == -1) {
+        perror("accept error");
+        close(fd);
+        unlink(socket_path);
+        exit(EXIT_FAILURE);
+    }
+
+    char buf[SIZE];
+    ssize_t read_bytes = 0;
+    while ((read_bytes = read(accepted, buf, SIZE)) > 0) {
+        for (int i = 0; i < read_bytes; i++) {
+            printf("%c", toupper(buf[i]));
+        }
+    }
+
+    if (read_bytes == -1) {
+        perror("read error");
+        close(accepted);
+        close(fd);
+        unlink(socket_path);
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+    unlink(socket_path);
+    exit(EXIT_SUCCESS);
+}
