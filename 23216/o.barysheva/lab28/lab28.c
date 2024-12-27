3d1d52ed53d3ed2529482acd7c9eae7d00421c8e
Olesia Barysheva <o.barysheva@g.nsu.ru>
2024-12-28 02:51:51 +0700

    lab28

--- 23216/o.barysheva/lab28/lab28.c
+++ 23216/o.barysheva/lab28/lab28.c
@@ -2,52 +2,47 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
-#include <errno.h>
 
 int main() {
-    srand((unsigned int)time(NULL));
+    srand(time(NULL));
 
     FILE* fp[2];
 
     if (p2open("sort -n", fp) == -1) {
         perror("Failed to open pipe");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
     for (int i = 0; i < 100; i++) {
         if (fprintf(fp[0], "%d\n", rand() % 100) < 0) {
             perror("Failed to write to pipe");
-            fclose(fp[0]);
-            fclose(fp[1]);
-            return EXIT_FAILURE;
+            exit(EXIT_FAILURE);
         }
     }
 
     if (fclose(fp[0]) == EOF) {
         perror("Failed to close input pipe");
-        fclose(fp[1]); 
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
-    int count = 0, number;
-    while (fscanf(fp[1], "%d", &number) == 1) {
+    int number;
+
+    for (int i = 1; i <= 100; i++) { 
+        if (fscanf(fp[1], "%d", &number) == EOF) {
+            perror("Failed to read from pipe");
+            exit(EXIT_FAILURE);
+        }
+        
         printf("%2d ", number);
-        count++;
-        if (count % 10 == 0) {
+        if (i % 10 == 0) {
             putchar('\n');
         }
     }
 
-    if (ferror(fp[1])) {
-        perror("Error reading from pipe");
-        fclose(fp[1]);
-        return EXIT_FAILURE;
-    }
-
     if (fclose(fp[1]) == EOF) {
         perror("Failed to close output pipe");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
-    return EXIT_SUCCESS;
+    exit(EXIT_SUCCESS);
 }
