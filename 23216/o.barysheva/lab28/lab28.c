0c2618b7b3c431c828fc8922aa46b7e215f22559
Olesia Barysheva <o.barysheva@g.nsu.ru>
2024-12-27 18:51:29 +0700

    lab28

--- 23216/o.barysheva/lab28/lab28.c
+++ 23216/o.barysheva/lab28/lab28.c
@@ -1,59 +1,53 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <libgen.h>
+#include <time.h>
 #include <errno.h>
 
 int main() {
-    // Инициализация генератора случайных чисел
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
 
-    // Открываем каналы для взаимодействия с sort
-    FILE* fd[2];
-    if (p2open("sort -n", fd) == -1) {
-        perror("p2open failed");
-        exit(EXIT_FAILURE);
+    FILE* fp[2];
+
+    if (p2open("sort -n", fp) == -1) {
+        perror("Failed to open pipe");
+        return EXIT_FAILURE;
     }
 
-    // Генерация и запись случайных чисел
     for (int i = 0; i < 100; i++) {
-        if (fprintf(fd[0], "%d\n", rand() % 100) < 0) {
+        if (fprintf(fp[0], "%d\n", rand() % 100) < 0) {
             perror("Failed to write to pipe");
-            fclose(fd[0]);
-            fclose(fd[1]);
-            exit(EXIT_FAILURE);
+            fclose(fp[0]);
+            fclose(fp[1]);
+            return EXIT_FAILURE;
         }
     }
 
-    // Закрытие канала записи
-    if (fclose(fd[0]) == EOF) {
-        perror("Failed to close fd[0]");
-        fclose(fd[1]); // Попытка закрыть выходной поток перед завершением
-        exit(EXIT_FAILURE);
+    if (fclose(fp[0]) == EOF) {
+        perror("Failed to close input pipe");
+        fclose(fp[1]); 
+        return EXIT_FAILURE;
     }
 
-    // Чтение и вывод отсортированных чисел
-    int number, count = 0;
-
-    while (fscanf(fd[1], "%d", &number) != EOF) {
-        if (fscanf(fd[1], "%d", &number) == 0) {  
-            perror("fscanf failed");
-            break;
-        }
-        printf("%d ", number);
+    int count = 0, number;
+    while (fscanf(fp[1], "%d", &number) == 1) {
+        printf("%2d ", number);
         count++;
         if (count % 10 == 0) {
             putchar('\n');
         }
     }
 
-    // Закрытие канала чтения
-    if (fclose(fd[1]) == EOF) {
-        perror("Failed to close fd[1]");
-        exit(EXIT_FAILURE);
+    if (ferror(fp[1])) {
+        perror("Error reading from pipe");
+        fclose(fp[1]);
+        return EXIT_FAILURE;
     }
 
-    // Завершение программы
+    if (fclose(fp[1]) == EOF) {
+        perror("Failed to close output pipe");
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
