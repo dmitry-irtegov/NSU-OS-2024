6733b2a162e0d75701baf91bf5b1da618aa8bf35
ErzhanKorzhan <r.badyrtenov1@g.nsu.ru>
2025-02-02 01:45:54 +0700

    lab2_2

---
+++ 23216/r.badyrtenov1/lab2_2/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printTexts(s string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(s)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		printTexts("Child Thread")
+		wg.Done()
+	}()
+
+	wg.Wait()
+	printTexts("Parent Thread")
+}
