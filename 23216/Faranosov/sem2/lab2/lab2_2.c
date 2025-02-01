2f9bda10f8d39196e117c021fe2203588c297b10
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-02-02 01:59:23 +0700

    lab2_2

---
+++ 23216/Faranosov/sem2/lab2/lab2_2.c
@@ -0,0 +1,33 @@
+#include "stdlib.h"
+#include "pthread.h"
+#include "stdio.h"
+
+void* thread_funk(void* param) {
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", i);
+	}
+	return NULL;
+}
+
+
+int main() {
+	pthread_t thread;
+
+	int checkRes = 0;
+
+
+	checkRes = pthread_create(&thread, NULL, thread_funk, NULL);
+	if (checkRes != 0) {
+		perror("pthread_create error");
+		exit(1);
+	}
+
+	checkRes = pthread_join(thread, NULL);
+	if (checkRes != 0) {
+		perror("pthread_join error");
+		exit(1);
+	}
+
+	thread_funk(NULL);
+	exit(0);
+}
