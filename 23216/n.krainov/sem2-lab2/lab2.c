bbaa3f3845eff010cd6bb82812063946c2cd1cda
Nikita Krainov <n.krainov@g.nsu.ru>
2025-02-03 13:14:21 +0700

    second lab

---
+++ 23216/n.krainov/sem2-lab2/lab2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void print_text() {
+    puts("text1");
+    puts("text2");
+    puts("text3");
+    puts("text4");
+    puts("text5");
+    puts("text6");
+    puts("text7");
+    puts("text8");
+    puts("text9");
+    puts("text10");
+}
+
+void* thread_func(void* param) {
+    print_text();
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t thread;
+
+    if (pthread_create(&thread, NULL, thread_func, NULL)) {
+        perror("pthread_create");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_join(thread, NULL)) {
+        perror("pthread_join");
+        exit(EXIT_SUCCESS);
+    }
+    print_text();
+
+    pthread_exit(NULL);
+}
