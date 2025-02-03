0f87f00f25a9626aa63c6b135cc27c466b5e7b12
Artem Chuvashov <a.chuvashov@g.nsu.ru>
2025-02-03 10:32:49 +0700

    made lab2

---
+++ 23213/a.chuvashov/sem2-lab2/lab2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+void* thread_body(void* param) {
+    for(int i = 0; i < 10; i++) {
+        printf("Child\n");
+    }
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t thread;
+    int code;
+
+    if ((code = pthread_create(&thread, NULL, thread_body, NULL)) != 0) {
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], strerror(code));
+        exit(1);
+    }
+
+    pthread_join(thread, NULL);
+
+    for (int i = 0; i < 10; i++) {
+        printf("Parent\n");
+    }
+    return 0;
+}
