2482e3f24c9842533556dcfd56dc5a3ac4d1b715
Alena Savushkina <a.savushkina@g.nsu.ru>
2025-02-04 03:00:33 +0700

    first task

---
+++ 23216/a.savushkina/sem2-lab1/lab1.c
@@ -0,0 +1,40 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <pthread.h> 
+#include <errno.h> 
+ 
+#define handle_error_en(en, msg) do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0) 
+ 
+void * print_lines(void* arg) { 
+    for (int i = 0; i < 10; i++) { 
+        printf("Нить: Строка %d\n", i); 
+    } 
+    pthread_exit(0); 
+} 
+ 
+int main() { 
+    pthread_t thread; 
+    pthread_attr_t attr;
+    int result;
+ 
+ 
+    result = pthread_attr_init(&attr); 
+    if (result != 0) 
+        handle_error_en(result, "pthread_attr_init"); 
+     
+    result = pthread_create(&thread, &attr, &print_lines, NULL); 
+    if (result != 0) 
+        handle_error_en(result, "pthread_create");
+
+    result = pthread_attr_destroy(&attr);
+    if (result != 0)
+        handle_error_en(result, "pthread_attr_destroy");
+        
+
+    for (int i = 0; i < 10; i++) { 
+        printf("Родительская нить: Строка %d\n", i); 
+    } 
+ 
+
+    pthread_exit(0);
+}
