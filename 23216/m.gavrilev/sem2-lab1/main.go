c10030401480ee7537071fc9f4f6596bfe58b7cf
Maxim <m.gavrilev@g.nsu.ru>
2025-02-04 15:18:43 +0700

    with go func

--- 23216/m.gavrilev/sem2-lab1/main.go
+++ 23216/m.gavrilev/sem2-lab1/main.go
@@ -5,23 +5,17 @@ import (
 	"sync"
 )
 
-func printer(input string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(input + " - " + fmt.Sprint(i))
-	}
-}
-
-func printerGoroutine(input string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := 0; i < 10; i++ {
-		fmt.Println(input + " - " + fmt.Sprint(i))
-	}
-}
-
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go printerGoroutine("thread", &wg)
-	printer("main")
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println("goroutine - " + fmt.Sprint(i))
+		}
+		wg.Done()
+	}()
+	for i := 0; i < 10; i++ {
+		fmt.Println("main - " + fmt.Sprint(i))
+	}
 	wg.Wait()
 }
