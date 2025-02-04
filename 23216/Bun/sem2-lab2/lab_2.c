4dea15e7acdca3c3854dea891ca55680ee0c3b5b
PavelBun <144594950+PavelBun@users.noreply.github.com>
2025-02-04 15:34:17 +0700

    Update lab_2.c

--- 23216/Bun/sem2-lab2/lab_2.c
+++ 23216/Bun/sem2-lab2/lab_2.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define handle_error(err, msg) do { errno = err; perror(msg); exit(EXIT_FAILURE); } while(0)
 void* thread_function(void* arg) {
     for (int i = 0; i < 10; i++) {
         printf("Hello World %s \n", (char*)arg);
@@ -12,17 +13,11 @@ int main() {
     pthread_t thread;
     int res;
     if ((res = pthread_create(&thread, NULL, thread_function, "from child")) != 0) {
-        char err[256];
-        strerror_r(res, err, sizeof(err));
-        fprintf(stderr, "Error creating thread: %s\n", err);
-        exit(EXIT_FAILURE);
+        handle_error(res, "error creating thread");
     }
 
     if ((res = pthread_join(thread, NULL)) != 0) {
-        char err[256];
-        strerror_r(res, err, sizeof(err));
-        fprintf(stderr, "Error  pthread_join: %s\n", err);
-        exit(EXIT_FAILURE);
+        handle_error(res, "error thread_join");
     }
     thread_function("from parent");
     return 0;
