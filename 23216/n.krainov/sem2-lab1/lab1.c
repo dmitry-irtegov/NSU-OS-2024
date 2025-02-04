1c725226264bbcf6ef6cd96914c0debb7bcc4641
Nikita Krainov <n.krainov@g.nsu.ru>
2025-02-04 15:43:41 +0700

    fix

--- 23216/n.krainov/sem2-lab1/lab1.c
+++ 23216/n.krainov/sem2-lab1/lab1.c
@@ -20,12 +20,16 @@ void* thread_func(void* param) {
     pthread_exit(NULL);
 }
 
+void my_perror(char* text, int code) {
+    fprintf(stderr, "%s: %s \n", text, strerror(code));
+}
+
 int main() {
     pthread_t thread;
     int code = 0;
     char* name = "child";
     if ((code = pthread_create(&thread, NULL, thread_func, name)) != 0) {
-        fprintf(stderr, "pthread_create error: %d", code);
+        my_perror("pthread_create error", code);
         exit(EXIT_FAILURE);
     }
     print_text("parent");
