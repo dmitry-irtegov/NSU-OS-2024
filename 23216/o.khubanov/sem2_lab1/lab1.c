2e6d208f11b3c4eedba3f2cd31bb8048c635f50d
Ochir Khubanov <o.khubanov@g.nsu.ru>
2025-02-04 15:27:52 +0700

    lab1_5

--- 23216/o.khubanov/sem2_lab1/lab1.c
+++ 23216/o.khubanov/sem2_lab1/lab1.c
@@ -1,31 +1,36 @@
-#include <stdio.h>     // Для puts
-#include <stdlib.h>    // Для NULL
-#include <stddef.h>    // Для NULL (дополнительно)
-#include <pthread.h>   // Для потоков
+#include <stdio.h>    
+#include <stdlib.h>   
+#include <pthread.h>  
 
 void* print_lines() {
     for (int i = 0; i < 10; i++) {
-        printf("%d\n",i);
+        printf("%d\n", i);
+        fflush(stdout); // Принудительный сброс буфера
     }
     return NULL;
 }
 
 int main() {
     pthread_t thread;
-    
-    // Создание нового потока
-    val = pthread_create(&thread, NULL, print_lines, NULL);
-    if (ret != 0) {
+    pthread_attr_t attr;
+
+    // Инициализация атрибутов потока
+    pthread_attr_init(&attr);
+
+    // Создание потока с установленными атрибутами
+    int val = pthread_create(&thread, &attr, print_lines, NULL);
+    if (val != 0) {
         fprintf(stderr, "Ошибка при создании потока: %d\n", val);
+        pthread_attr_destroy(&attr); // Освобождаем атрибуты перед выходом
         return EXIT_FAILURE;
     }
-    
+
+    pthread_attr_destroy(&attr);
+
     // Выполнение кода в главном потоке
-    print_lines();
-    
-    // Ожидание завершения дочернего потока
+    print_lines(NULL);
+
+    // Завершаем главный поток, позволяя дочернему завершиться
     pthread_exit(NULL);
-    
-    return 0;
 }
 
