97a72dcf1ad4bc09d86a68209ed890188ed532f3
Nikita Krainov <n.krainov@g.nsu.ru>
2025-02-04 09:10:13 +0700

    lab1

--- 23216/n.krainov/sem2-lab1/lab1.c
+++ 23216/n.krainov/sem2-lab1/lab1.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void print_text() {
-    puts("text1");
-    puts("text2");
-    puts("text3");
-    puts("text4");
-    puts("text5");
-    puts("text6");
-    puts("text7");
-    puts("text8");
-    puts("text9");
-    puts("text10");
+void print_text(char* name) {
+    printf("text1 %s\n", name);
+    printf("text2 %s\n", name);
+    printf("text3 %s\n", name);
+    printf("text4 %s\n", name);
+    printf("text5 %s\n", name);
+    printf("text6 %s\n", name);
+    printf("text7 %s\n", name);
+    printf("text8 %s\n", name);
+    printf("text9 %s\n", name);
+    printf("text10 %s\n", name);
 }
 
 void* thread_func(void* param) {
-    print_text();
+    print_text("child");
     pthread_exit(NULL);
 }
 
@@ -27,7 +27,7 @@ int main() {
         fprintf(stderr, "pthread_create error: %d", code);
         exit(EXIT_FAILURE);
     }
-    print_text();
+    print_text("parent");
 
     pthread_exit(NULL);
 }
