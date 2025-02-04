0a8c06303ee4030786184dcf515ed0e8e283720e
PavelBun <144594950+PavelBun@users.noreply.github.com>
2025-02-04 13:55:54 +0700

    Update lab_2.c

--- 23216/Bun/sem2-lab2/lab_2.c
+++ 23216/Bun/sem2-lab2/lab_2.c
@@ -1,7 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 void* thread_function(void* arg) {
     for (int i = 0; i < 10; i++) {
         printf("Hello World %s \n", (char*)arg);
