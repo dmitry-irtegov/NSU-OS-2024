130fa233adc8b95e2b1747d646437ebc06db90e6
PavelBun <144594950+PavelBun@users.noreply.github.com>
2025-02-04 13:53:18 +0700

    Update lab_1.c

--- 23216/Bun/sem2-lab1/lab_1.c
+++ 23216/Bun/sem2-lab1/lab_1.c
@@ -1,21 +1,23 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 void* thread_function() {
-  for (int i = 0; i < 10; i++) {
-    printf("Hello World\n");
-  }
-  return NULL;
+    for (int i = 0; i < 10; i++) {
+        printf("Hello World\n");
+    }
+    return NULL;
 }
 int main() {
-  pthread_t thread;
-  if (pthread_create(&thread, NULL, thread_function, NULL) != 0) {
-    perror("error creating thread");
-    exit(EXIT_FAILURE);
-  }
-  thread_function();
+    pthread_t thread;
+    int res;
+    if ((res = pthread_create(&thread, NULL, thread_function, NULL)) != 0) {
+        char err[256];
+        strerror_r(res, err, sizeof(err));
+        fprintf(stderr, "Error creating thread: %s\n", err);
+        exit(EXIT_FAILURE);
+    }
+    thread_function();
 
-  pthread_exit(NULL);
-
+    pthread_exit(NULL);
 }
