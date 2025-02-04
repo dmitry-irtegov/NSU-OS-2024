29f78c30b86555182b838db4a94bfd2a246173f4
ttomilov <t.tomilov@g.nsu.ru>
2025-02-04 13:30:15 +0700

    fix err

--- 23216/t.tomilov/sem2-lab1/sem2-lab1.c
+++ 23216/t.tomilov/sem2-lab1/sem2-lab1.c
@@ -11,20 +11,32 @@ void* cat(void* threadData){
 
 int main(){
 	char* str = NULL;
-    size_t strSize;
+    size_t strSize = 0;
+	int errID = 0;
 	pthread_t* thread = malloc(sizeof(pthread_t) * 11);
+	if (thread == NULL){
+		perror("ERROR: failed to allocate memory!");
+		exit(EXIT_FAILURE);
+	}
 	for (int i = 0; i < 10; i++){
 		if (getline(&str, &strSize, stdin) == -1){
-			perror("ERROR: failed in getline!");
+			fprintf(stderr, "ERROR: failed in getline");
 			exit(EXIT_FAILURE);
 		}
-		if (pthread_create(&(thread[i]), NULL, cat, str) != 0){
-			perror("ERROR: failed in pthread_create!");
+		if ((errID = pthread_create(&(thread[i]), NULL, cat, str)) != 0){
+			char buf[256];
+			strerror_r(errID, buf, 256);
+			fprintf(stderr, "ERROR: %s", buf);
 			exit(EXIT_FAILURE);
 		}
 	}
 	for (int i = 0; i < 10; i++){
-		pthread_join(thread[i], NULL);
+		if ((errID = pthread_join(thread[i], NULL)) != 0){
+			char buf[256];
+			strerror_r(errID, buf, 256);
+			fprintf(stderr, "ERROR: %s", buf);
+			exit(EXIT_FAILURE);
+		}
 	}
 	free(thread);
 	free(str);
