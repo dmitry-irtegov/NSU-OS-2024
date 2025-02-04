e4529157cf47b1361e32aa10586e850055ef2282
A-GigaC <90515245+A-GigaC@users.noreply.github.com>
2025-02-04 10:03:10 +0700

    add sem2 lab1.c

---
+++ 23216/f.pivovarov/sem2-lab1/lab1.c
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+void *printTenStrings();
+
+int main() {
+    int code;
+    pthread_t thread;
+    if ((code = pthread_create(&thread, NULL, printTenStrings, NULL)) != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "At creating thread: %s\n", buf);
+        exit(EXIT_FAILURE);
+    }
+    printTenStrings();
+
+    exit(EXIT_SUCCESS);
+}
+
+void *printTenStrings() {
+    for (int i = 1; i <= 10; i++) {
+        printf("Print number %d\n", i);
+    }
+
+    return NULL;
+}
