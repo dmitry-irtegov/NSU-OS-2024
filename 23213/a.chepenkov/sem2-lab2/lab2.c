0ba3aeddfa134fcd6702498776179c1154dbff06
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2025-02-05 10:12:48 +0700

    Artem Chepenkov 23213 sem2-lab2

---
+++ 23213/a.chepenkov/sem2-lab2/lab2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+void* thread_func(void* param) {
+    for (int i = 0; i < 10; i++)
+        printf("Child\n");
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t thread;
+
+    int err = pthread_create(&thread, NULL, thread_func, NULL);
+    if (err != 0) {
+        fprintf(stderr, "Error while creating: %s\n", strerror(err));
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_join(thread, NULL);
+
+    for (int i = 0; i < 10; i++)
+        printf("Parent\n");
+
+    return (EXIT_SUCCESS);
+}
