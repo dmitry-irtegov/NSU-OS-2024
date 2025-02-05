0f2d122bbd3abe71b45a6c4b2cc98ea34cc8726f
Vovadinamik8913 <v.abramenko@g.nsu.ru>
2025-02-05 13:19:02 +0700

    lab4 (#803)

---
+++ 23213/v.abramenko/sem2-lab4/lab4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+
+void* thread_body(void* param) {
+    while(1) { 
+        write(0, "Child\n", 6);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t thread;
+    int code;
+
+    if ((code = pthread_create(&thread, NULL, thread_body, NULL)) !=0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "%s: creating: %s\n", argv[0], buf);
+        exit(-1);
+    }
+
+    sleep(2);
+
+    printf("Trying to cancel\n");
+    
+    if ((code=pthread_cancel(thread))!=0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "%s: cancelling: %s\n", argv[0], buf);
+        exit(-1);
+    }
+
+    pthread_join(thread, NULL);
+    printf("Cancelled\n");
+    
+    pthread_exit(NULL);
+}
