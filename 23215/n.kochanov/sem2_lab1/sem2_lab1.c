354bd022e5745c9ef50e8f97a25cb4f49dd2a909
Nikita Kochanov <n.kochanov@g.nsu.ru>
2025-02-06 14:02:13 +0700

    KochanovNikita_23215_sem2_lab1 (#809)
    * test commit
    
    * Delete hamcrest-core-1.3.jar
    
    * init lab2

---
+++ 23215/n.kochanov/sem2_lab1/sem2_lab1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h> 
+
+void* childPthread(void* arg) {
+    for(int i = 0; i < 10; i++) {
+        printf("Child thread: line %d\n", i + 1);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+
+    int newPthread = pthread_create(&thread, NULL, childPthread, NULL);
+
+    if(newPthread != 0) {
+        fprintf(stderr, "Error creating thread\n");
+        return 1;
+    }
+
+    for(int i = 0; i < 10; i++) {
+        printf("Parent thread: line %d\n", i + 1);
+    }
+
+    pthread_exit(NULL);
+
+}
