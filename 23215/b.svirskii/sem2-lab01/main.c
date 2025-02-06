18d4990753aa938fbba88ae483feee9c41fb86e8
Svirsky Bogdan <74711431+BogdanSvirsky@users.noreply.github.com>
2025-02-06 13:05:13 +0700

    sem2-lab01: done (#805)

---
+++ 23215/b.svirskii/sem2-lab01/main.c
@@ -0,0 +1,21 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void* thread_func(void* args) {
+    for (int i = 0; i < 10; i++) {
+        printf("Hello from child thread!\n");
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t th;
+    pthread_create(&th, NULL, thread_func, NULL);
+
+    for (int i = 0; i < 10; i++) {
+        printf("Hello from main thread!\n");
+    }
+
+    pthread_exit(NULL); 
+}
