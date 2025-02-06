dadc9baa8d0cfe5eb838056006e90b2ae5f9a046
Dimandr <Dmitry221556@yandex.ru>
2025-02-06 13:43:46 +0700

    sem2-lab6

--- 23215/d.stolyarov/sem2-lab6/lab6.c
+++ 23215/d.stolyarov/sem2-lab6/lab6.c
@@ -4,19 +4,24 @@
 #include <stdlib.h>
 #include <pthread.h>
 int coef;
-char ar[101][11];
+char ar[100][11];
 
 void * sleepsort(void *line){
-    sleep(coef * strlen(line));
+    usleep(coef * strlen(line));
     printf("%s", line);
     return 0;
 }
 
 int main(){
-    coef = 1;
+    coef = 1000;
     pthread_t thread;
     int k;
-    for(k = 0; k < 100 && fgets(ar[k], 10, stdin); k++);
+    for(k = 0; k < 100; k++){
+        char* c = fgets(ar[k], 10, stdin);
+        if(c == NULL){
+            break;
+        }
+    }
     printf("\n________\n");
     for(int i = 0; i < k; i++){
         if(pthread_create(&thread, NULL, sleepsort, ar[i]) != 0){
