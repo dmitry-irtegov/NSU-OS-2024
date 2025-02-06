fa1b686212afef941204ecdb8f82ee9dd68ff91b
YuliaVyaznikova <y.vyaznikova@g.nsu.ru>
2025-02-06 11:33:52 +0700

    23215 y.vyaznikova sem2-lab1

---
+++ 23215/y.vyaznikova/sem2-lab1/task1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+void* thread_body(void* param) {
+    for (int i = 1; i < 11; i++) {
+        printf("Child thread %d\n", i);
+    }
+
+	return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t thread;
+    int code;
+    
+    code = pthread_create(&thread, NULL, thread_body, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+    
+    for (int i = 1; i < 11; i++) {
+        printf("Parent thread %d\n", i);
+    }
+    
+    pthread_exit(NULL);   
+}
