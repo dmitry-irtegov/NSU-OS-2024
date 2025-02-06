39faf0844f7509192fa91f47a1b895919fc0a9b1
Ilya Pereverzev <141811344+Takymu@users.noreply.github.com>
2025-02-06 13:44:29 +0700

    23215 i.pereverzev2 sem2 lab4 (#817)
    * lab 2 finished
    
    * lab 4 created
    
    * lab 4 minor changes
    
    * lab 4 finished
    
    * folder renamed

---
+++ 23215/i.pereverzev2/sem2_lab4/main.c
@@ -0,0 +1,36 @@
+#define _REENTRANT
+#include <pthread.h>
+#include <thread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+void* print_text(void* arg)
+{
+    int i = 0;
+    while(1) {
+        printf("%d, created thread, line\n", i);
+        i++;
+    }
+}
+
+int main()
+{
+    pthread_t th;
+    int code = pthread_create(&th, NULL, print_text, NULL);
+    if(code){
+        char buf[256];
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "unable to create thread: %s", buf);
+        return 1;
+    }
+    sleep(2);
+    code = pthread_cancel(th);
+    if(code){
+        char buf[256];
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "unable to cancel thread: %s", buf);
+        return 1;
+    }
+    return 0;
+}
