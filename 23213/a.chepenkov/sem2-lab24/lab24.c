f517ba050c21dfbf9ad78335518ae5e4ffa7241f
ArtemChepenkov <82212767+ArtemChepenkov@users.noreply.github.com>
2025-02-12 13:57:12 +0700

    Artem Chepenkov 23213 sem2-lab24 (#836)

---
+++ 23213/a.chepenkov/sem2-lab24/lab24.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+sem_t semA, semB, semC, semAB;
+
+void* createA(void* argv) {
+    while (1) {
+        sleep(1);
+        sem_post(&semA);
+        printf("new A\n");
+    }
+}
+
+void* createB(void* argv) {
+    while (1) {
+        sleep(2);
+        sem_post(&semB);
+        printf("new B\n");
+    }
+}
+
+void* createC(void* argv) {
+    while (1) {
+        sleep(3);
+        sem_post(&semC);
+        printf("new C\n");
+    }
+}
+
+void* createAB(void* argv) {
+    while (1) {
+        sem_wait(&semA);
+        sem_wait(&semB);
+        sem_post(&semAB);
+        printf("new AB\n");
+    }
+}
+
+void* createWidget() {
+    while (1) {
+        sem_wait(&semAB);
+        sem_wait(&semC);
+        printf("new WIDGET\n");
+    }
+}
+
+int main() {
+    pthread_t threadA;
+    pthread_t threadB;
+    pthread_t threadC;
+    pthread_t threadAB;
+
+    sem_init(&semA, 0, 0);
+    sem_init(&semB, 0, 0);
+    sem_init(&semC, 0, 0);
+    sem_init(&semAB, 0, 0);
+
+    pthread_create(&threadA, NULL, createA, NULL);
+    pthread_create(&threadB, NULL, createB, NULL);
+    pthread_create(&threadC, NULL, createC, NULL);
+    pthread_create(&threadAB, NULL, createAB, NULL);
+    createWidget();
+
+
+}
