e69ba8b1d41e18e6e563805acc5b5a4a1d289501
Kirill Kozlov <159503378+kozlovwv@users.noreply.github.com>
2025-02-13 13:00:21 +0700

    sem2-lab1 (#870)

---
+++ 23215/k.kozlov2/sem2-lab1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+void * thread_body(void *param) {
+    for(int i = 0; i < 5; i++) 
+        printf("Child thread\n");
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t thread;
+    int code;
+
+    code = pthread_create(&thread, NULL, thread_body, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i = 0; i < 5; i++) 
+        printf("Parent thread\n");
+
+    pthread_exit(NULL);
+}
