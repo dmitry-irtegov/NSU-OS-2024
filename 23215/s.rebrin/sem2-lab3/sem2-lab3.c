923867737fd653e9f95954f41e4228b27cd9fb5a
Sergei Rebrin <s.rebrin@g.nsu.ru>
2025-02-13 10:32:35 +0700

    a

---
+++ 23215/s.rebrin/sem2-lab3/sem2-lab3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+typedef struct {
+    char** strings;
+} strings;
+
+void* print_strings(void* arg) {
+    strings* data = (strings*)arg;
+    for (int i = 0; data->strings[i]; i++) {
+        printf("%s\n",data->strings[i]);
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+
+    char* strings1[] = { "one", "uno", "odin", "1", NULL };
+    char* strings2[] = { "two", "dos", "dwa", "2", NULL };
+    char* strings3[] = { "three", "tres", "tri", "3", NULL };
+    char* strings4[] = { "four", "quadro", "chetyre", "4", NULL };
+
+    char** thread_strings[] = { strings1, strings2, strings3, strings4};
+
+    pthread_t threads[4];
+
+    strings thread_data[4];
+
+    for (int i = 0; i < 4; i++) {
+        thread_data[i].strings = thread_strings[i];
+
+        int rc = pthread_create(&threads[i], NULL, print_strings, (void*)&thread_data[i]);
+        if (rc) {
+            fprintf(stderr, "Error: Unable to create thread %d, return code %d\n", i + 1, rc);
+            exit(1);
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
