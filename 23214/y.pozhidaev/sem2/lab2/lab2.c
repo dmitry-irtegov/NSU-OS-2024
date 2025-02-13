be4390681d05af7f746460a2d379ee968ff864b3
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-02-13 11:41:21 +0700

    sem2_lab2

---
+++ 23214/y.pozhidaev/sem2/lab2/lab2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+void *thread_body(void *param) {
+    for(int i = 0; i < 10; i++) {
+        printf("Child\n");
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    char buf[256];
+    pthread_t thread;
+    int code;
+
+    code = pthread_create(&thread, NULL, thread_body, NULL);
+    if (code != 0) {
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+
+    code = pthread_join(thread, NULL);
+    if (code != 0) {
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "%s: joining thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+    
+    for(int i = 0; i < 10; i++) {
+        printf("Parent\n");
+    }
+
+    pthread_exit(NULL);
+}
+
