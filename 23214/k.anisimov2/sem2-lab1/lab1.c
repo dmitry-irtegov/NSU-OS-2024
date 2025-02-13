d7330eb3a781cced70cdcd803cad46b3207ce362
Kirill Anisimov <159605360+animovscw@users.noreply.github.com>
2025-02-13 14:40:56 +0700

    lab1 (#824)

---
+++ 23214/k.anisimov2/sem2-lab1/lab1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+
+void *thread_body(void *param)
+{
+    for (int i = 1; i < 110; ++i)
+    {
+        printf("Newly created thread: line %d\n", i);
+    }
+    return NULL;
+}
+
+int main()
+{
+    pthread_t thread;
+    int code;
+
+    if ((code = pthread_create(&thread, NULL, thread_body, NULL)) != 0)
+    {
+        fprintf(stderr, "ERROR creating thread: %s\n", strerror(code));
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 1; i < 101; ++i)
+    {
+        printf("Parent thread: line %d\n", i);
+    }
+
+    pthread_exit(EXIT_SUCCESS);
+}
