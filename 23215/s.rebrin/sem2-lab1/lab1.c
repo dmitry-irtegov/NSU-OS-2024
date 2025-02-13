1547bd3f7cb366286472e557439d438960c4be79
ssrebrin <159110330+ssrebrin@users.noreply.github.com>
2025-02-13 12:46:29 +0700

    Rebrin_lab1 (#821)
    * a
    
    * a
    
    * a
    
    * a

---
+++ 23215/s.rebrin/sem2-lab1/lab1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+
+void* thread_body(void* param) {
+
+    for (int i = 0;i < 10;i++) {
+        printf("left\n");
+    }
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t thread;
+    int code;
+
+    code = pthread_create(&thread, NULL, thread_body, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+    else {
+        for (int i = 0;i < 10;i++) {
+            printf("right\n");
+        }
+    }
+
+
+    pthread_exit(NULL);
+}
+
