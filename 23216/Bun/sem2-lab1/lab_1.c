e3fee17b9ca540a27f23b109eb6c3f0b6d188586
PavelBun <144594950+PavelBun@users.noreply.github.com>
2025-02-18 15:47:29 +0700

    Lab1 (#765)
    accepted

---
+++ 23216/Bun/sem2-lab1/lab_1.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void* thread_function() {
+    for (int i = 0; i < 10; i++) {
+        printf("Hello World\n");
+    }
+    return NULL;
+}
+int main() {
+    pthread_t thread;
+    int res;
+    if ((res = pthread_create(&thread, NULL, thread_function, NULL)) != 0) {
+        char err[256];
+        strerror_r(res, err, sizeof(err));
+        fprintf(stderr, "Error creating thread: %s\n", err);
+        exit(EXIT_FAILURE);
+    }
+    thread_function();
+
+    pthread_exit(NULL);
+}
