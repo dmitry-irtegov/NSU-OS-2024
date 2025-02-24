201e86352a8c8e8511f3c5c610d75b8968061954
Ildar Fitkulin <159516390+filimonq@users.noreply.github.com>
2025-02-24 11:48:37 +0700

    sem2-lab24 23215 Ildar Fitkulin (#913)
    * lab24 ready
    
    * ~1bolt in 1 sec
    
    * fix

---
+++ 23215/i.fitkulin/sem2-lab24/sem2-lab24.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+sem_t semA, semB, semC, semAB;
+
+void* createA(void* argv) {
+    while(1) {
+        sleep(1);
+        sem_post(&semA);    
+        printf("new A\n");
+    }
+}
+
+void* createB(void* argv) {
+    while(1) {
+        sleep(2);
+        sem_post(&semB);    
+        printf("new B\n");
+    }
+}
+
+void* createC(void* argv) {
+    while(1) {
+        sleep(3);
+        sem_post(&semC);    
+        printf("new C\n");
+    }
+}
+
+void* createAB(void* argv) {
+    while(1) {
+        sem_wait(&semA);
+        sem_wait(&semB);
+        sem_post(&semAB);
+        printf("new AB\n");
+    }
+}
+
+void* createWidget() {
+    while(1) {
+        sem_wait(&semAB);
+        sem_wait(&semC);
+        printf("=== new BOLT ===\n");
+    }
+}
+
+int main() {
+    pthread_t threadA;    
+    pthread_t threadB;
+    pthread_t threadB2;
+    pthread_t threadC;
+    pthread_t threadC2;
+    pthread_t threadC3;
+    pthread_t threadAB;
+    
+    sem_init(&semA, 0, 0);
+    sem_init(&semB, 0, 0);
+    sem_init(&semC, 0, 0);
+    sem_init(&semAB, 0, 0);
+
+    pthread_create(&threadA, NULL, createA, NULL);
+    pthread_create(&threadB, NULL, createB, NULL);
+    pthread_create(&threadB2, NULL, createB, NULL);
+    pthread_create(&threadC, NULL, createC, NULL);
+    pthread_create(&threadC2, NULL, createC, NULL);
+    pthread_create(&threadC3, NULL, createC, NULL);
+    pthread_create(&threadAB, NULL, createAB, NULL);
+    
+    createWidget();    
+}
