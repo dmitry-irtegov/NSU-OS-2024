617f6d3f1139182605be5288dc562c031bd6b510
PolinaBasargina <96342723+VisTheB@users.noreply.github.com>
2025-02-27 16:12:41 +0700

    finish sem2-lab1 (#812)

---
+++ 23214/p.basargina/sem2-lab1/sem2-lab1.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void *thread_function(void *arg) {
+	for (int i = 0; i < 10; i++) {
+        	printf("Child thread: %d  :-)\n", i + 1);
+        }
+        pthread_exit(NULL);
+}
+
+int main() {
+        pthread_t thread;
+        int errCode;
+
+        if ((errCode = pthread_create(&thread, NULL, thread_function, NULL)) != 0) {
+        	char* buf = strerror(errCode);
+        	fprintf(stderr, "Failed to create thread: %s\n", buf);
+        	exit(1);
+        }
+
+        for (int i = 0; i < 10; i++) {
+        	printf("Parent thread: %d  :-)\n", i + 1);
+        }
+
+        pthread_exit(NULL);
+}
