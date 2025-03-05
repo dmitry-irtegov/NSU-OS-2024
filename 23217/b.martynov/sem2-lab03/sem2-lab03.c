c4b81d69b9013d0c4e3a24ed77d3cce43af72dc1
Bogdan Martynov <b.martynov@g.nsu.ru>
2025-03-05 11:29:08 +0700

    beauty code

--- 23217/b.martynov/sem2-lab03/sem2-lab03.c
+++ 23217/b.martynov/sem2-lab03/sem2-lab03.c
@@ -4,24 +4,24 @@
 #include <unistd.h>
 #include <string.h>
 
-typedef struct myStruct {
+typedef struct argStruct {
     char** arr;
-    size_t cnt;
-    size_t size;
-    size_t num;
-} myStruct;
-
+    size_t threadsCnt;
+    size_t cntOfStr;
+    size_t threadNum;
+} argStruct;
 
 void* func(void* paramPamPam) {
-    myStruct* tctc = (myStruct*)paramPamPam;
-    char** strings = tctc->arr;
-    size_t cnt = tctc->cnt;
-    size_t size = tctc->size;
-    size_t num = tctc->num;
-    size_t helper = (size / cnt) + ((size % cnt) != 0);
+    argStruct* t = (argStruct*)paramPamPam;
+    char** strings    = t->arr;
+    size_t threadsCnt = t->threadsCnt;
+    size_t cntOfStr   = t->cntOfStr;
+    size_t threadNum  = t->threadNum;
 
-    for (int i = num * helper; i < ((num+1)*helper) && i < size; i++) {
-        int j = num;
+    size_t iterCount = (cntOfStr / threadsCnt) + ((cntOfStr % threadsCnt) != 0);
+
+    for (int i = threadNum * iterCount; i < ((num+1) * iterCount) && i < cntOfStr; i++) {
+        int j = threadNum;
         while (j--) {
             printf("\t");
         }
@@ -31,25 +31,24 @@ void* func(void* paramPamPam) {
 }
 
 #define CNT 4
-myStruct meow[CNT];
+argStruct meow[CNT];
 
 int main() {
     pthread_t threads[CNT];
 
     char* str[] = {"a1", "a2", "a3", "a4", "a5", "a6", "A7", "A8", "A9", "A10", "A11", "A12", "A13", "A14", "A15",
-                "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10", "b11", "b12",
-                "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12",
-                "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "aboba"};
+                   "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "b10", "b11", "b12",
+                   "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12",
+                   "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "aboba"};
 
-    #define STRCNT (sizeof(str) / sizeof(char**))
-    printf("%d\n\n\n", STRCNT);
-
+    size_t STRCNT = (sizeof(str) / sizeof(char**));
 
     for (int i = 0; i < CNT; i++) {
-            meow[i].arr  = str;
-            meow[i].cnt  = CNT;
-            meow[i].size = STRCNT;
-            meow[i].num  = i;
+        meow[i].arr        = str;
+        meow[i].threadsCnt = CNT;
+        meow[i].cntOfStr   = STRCNT;
+        meow[i].threadNum  = i;
+        
         int thrErr = pthread_create(threads + i, NULL, func, meow + i);
         if (thrErr != 0) {
             char buf[256];
@@ -61,4 +60,3 @@ int main() {
 
     pthread_exit(NULL);
 }
-
