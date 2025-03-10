d95749e3b1649496e543067cc3c30533483fc555
Sergei Rebrin <s.rebrin@g.nsu.ru>
2025-03-10 10:57:01 +0700

    a

--- 23215/s.rebrin/sem2-lab11/sem2-lab11.c
+++ 23215/s.rebrin/sem2-lab11/sem2-lab11.c
@@ -3,21 +3,30 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
-#include <sched.h>
+#include <stdatomic.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-int turn = 0; // 0 - main 
+#define M 3
+
+pthread_mutex_t mutex[M];
+atomic_int turn = 0; // 0 - main 
+
+int add(int a, int b) {
+    return (a + b) % M;
+}
 
 void* thread_body(void* param) {
+    int cur = 2;
+
+    pthread_mutex_lock(&mutex[cur]);
+    cur = add(cur, 1);
+    atomic_store(&turn, 1);
     for (int i = 0; i < 10; i++) {
-        pthread_mutex_lock(&mutex);
-        while (turn != 1) {
-            pthread_mutex_unlock(&mutex);
-            pthread_mutex_lock(&mutex);
-        }
+        pthread_mutex_lock(&mutex[cur]);
+
         printf("left\n");
-        pthread_mutex_unlock(&mutex);
-        sched_yield();
+        atomic_store(&turn, 0);
+        pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
+        cur = add(cur, 1);
     }
     return NULL;
 }
@@ -25,30 +34,49 @@ void* thread_body(void* param) {
 int main(int argc, char* argv[]) {
     pthread_t thread;
     int code;
+    int mc;
 
-    pthread_mutex_init(&mutex, NULL);
+    for (int i = 0; i < M; i++) {
+        mc = pthread_mutex_init(&mutex[i], NULL);
 
-    code = pthread_create(&thread, NULL, thread_body, NULL);
-    if (code) {
-        char buf[256];
-        strerror_r(code, buf, sizeof(buf));
-        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
-        exit(1);
-    }
-    else {
-        for (int i = 0; i < 10; i++) {
-            pthread_mutex_lock(&mutex);
-            while (turn != 0) {
-                pthread_mutex_unlock(&mutex);
-                pthread_mutex_lock(&mutex);
+        if (mc != 0) {
+            printf("pthread_mutex_init error.\n");
+            for (int ii = 0; ii < i; ii++) {
+                pthread_mutex_destroy(&mutex[ii]);
             }
-            printf("right\n");
-            pthread_mutex_unlock(&mutex);
-            sched_yield();
+
+            exit(-1);
         }
     }
 
+    int cur = 0;
+
+    pthread_mutex_lock(&mutex[cur++]);
+
+    if ((code = pthread_create(&thread, NULL, thread_body, NULL)) != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof(buf));
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        for (int i = 0; i < M; i++) {
+            pthread_mutex_destroy(&mutex[i]);
+        }
+        exit(1);
+    }
+    else {
+        while (!atomic_load(&turn));
+        for (int i = 0; i < 10; i++) {
+            pthread_mutex_lock(&mutex[cur]);
+
+            printf("right\n");
+
+            pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
+            cur = add(cur, 1);
+        }
+    }
+    pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
     pthread_join(thread, NULL);
-    pthread_mutex_destroy(&mutex);
+    for (int ii = 0; ii < M; ii++) {
+        pthread_mutex_destroy(&mutex[ii]);
+    }
     pthread_exit(NULL);
 }
