be8acbbe077c057fecbfdfb1cbfec32f59372b23
Sergei Rebrin <s.rebrin@g.nsu.ru>
2025-03-13 13:28:37 +0700

    a

--- 23215/s.rebrin/sem2-lab11/sem2-lab11.c
+++ 23215/s.rebrin/sem2-lab11/sem2-lab11.c
@@ -17,16 +17,36 @@ int add(int a, int b) {
 void* thread_body(void* param) {
     int cur = 2;
 
-    pthread_mutex_lock(&mutex[cur]);
+    int lock_result1 = pthread_mutex_lock(&mutex[cur]);
+    if (lock_result1 != 0) {
+        fprintf(stderr, "pthread_mutex_lock error in thread: %s\n", strerror(lock_result1));
+        pthread_exit(NULL);
+    }
+
+    int unlock_result;
     cur = add(cur, 1);
     atomic_store(&turn, 1);
     for (int i = 0; i < 10; i++) {
-        pthread_mutex_lock(&mutex[cur]);
+        lock_result1 = pthread_mutex_lock(&mutex[cur]);
+        if (lock_result1 != 0) {
+            fprintf(stderr, "pthread_mutex_lock error in thread: %s\n", strerror(lock_result1));
+            pthread_exit(NULL);
+        }
 
         printf("left\n");
         atomic_store(&turn, 0);
-        pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
+        int unlock_result = pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
+        if (unlock_result != 0) {
+            fprintf(stderr, "pthread_mutex_unlock error in thread: %s\n", strerror(unlock_result));
+            pthread_exit(NULL);
+        }
         cur = add(cur, 1);
+    }
+
+    unlock_result = pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
+    if (unlock_result != 0) {
+        fprintf(stderr, "pthread_mutex_unlock error in thread: %s\n", strerror(unlock_result));
+        pthread_exit(NULL);
     }
     return NULL;
 }
@@ -36,22 +56,35 @@ int main(int argc, char* argv[]) {
     int code;
     int mc;
 
+    // Инициализация атрибутов мьютекса
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+
     for (int i = 0; i < M; i++) {
-        mc = pthread_mutex_init(&mutex[i], NULL);
+        // Инициализация мьютекса с атрибутом PTHREAD_MUTEX_ERRORCHECK
+        mc = pthread_mutex_init(&mutex[i], &attr);
 
         if (mc != 0) {
             printf("pthread_mutex_init error.\n");
             for (int ii = 0; ii < i; ii++) {
                 pthread_mutex_destroy(&mutex[ii]);
             }
-
+            pthread_mutexattr_destroy(&attr);
             exit(-1);
         }
     }
 
-    int cur = 0;
 
-    pthread_mutex_lock(&mutex[cur++]);
+    pthread_mutexattr_destroy(&attr);
+
+    int cur = 0;
+    int unlock_result1;
+    int lock_result = pthread_mutex_lock(&mutex[cur++]);
+    if (lock_result != 0) {
+        fprintf(stderr, "pthread_mutex_lock error in thread: %s\n", strerror(lock_result));
+        pthread_exit(NULL);
+    }
 
     if ((code = pthread_create(&thread, NULL, thread_body, NULL)) != 0) {
         char buf[256];
@@ -65,16 +98,31 @@ int main(int argc, char* argv[]) {
     else {
         while (!atomic_load(&turn));
         for (int i = 0; i < 10; i++) {
-            pthread_mutex_lock(&mutex[cur]);
+            lock_result = pthread_mutex_lock(&mutex[cur]);
+            if (lock_result != 0) {
+                fprintf(stderr, "pthread_mutex_lock error in thread: %s\n", strerror(lock_result));
+                pthread_exit(NULL);
+            }
+            
 
             printf("right\n");
 
-            pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
+
+            unlock_result1 = pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
+            if (unlock_result1 != 0) {
+                fprintf(stderr, "pthread_mutex_unlock error in thread: %s\n", strerror(unlock_result1));
+                pthread_exit(NULL);
+            }
             cur = add(cur, 1);
         }
     }
-    pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
-    pthread_join(thread, NULL);
+
+    unlock_result1 = pthread_mutex_unlock(&mutex[add(cur, M - 1)]);
+    if (unlock_result1 != 0) {
+        fprintf(stderr, "pthread_mutex_unlock error in thread: %s\n", strerror(unlock_result1));
+        pthread_exit(NULL);
+    }
+       pthread_join(thread, NULL);
     for (int ii = 0; ii < M; ii++) {
         pthread_mutex_destroy(&mutex[ii]);
     }
