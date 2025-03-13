8aa5db7f961ecd17a4221410bea7a40655efdbce
MCBivis <Artemiybox@mail.ru>
2025-03-13 15:36:11 +0700

    rework

--- 23216/a.kuleshov/sem2-lab11/mutex.c
+++ 23216/a.kuleshov/sem2-lab11/mutex.c
@@ -1,67 +1,57 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
+char flag = 1;
 pthread_mutex_t mutex;
-pthread_cond_t cond;
-int turn = 0; // 0 - основная нить, 1 - дочерняя
 
-// Функция вывода 10 строк
-void ten_str(const char* name, int thread_id) {
-    for (int i = 0; i < 10; i++) {
-        pthread_mutex_lock(&mutex);
-        while (turn != thread_id) {
-            pthread_cond_wait(&cond, &mutex);
-        }
-        printf("[%s] Сообщение %d\n", name, i + 1);
-        turn = 1 - thread_id;
-        pthread_cond_signal(&cond);
-        pthread_mutex_unlock(&mutex);
+int tryToPrint(char* text, int i) {
+    if (pthread_mutex_lock(&mutex)) {
+        return 1;
     }
+
+    printf("%s %d\n", text, i);
+
+    pthread_mutex_unlock(&mutex);
+
+    return 0;
 }
 
-// Функция, выполняемая в новой нити
-void *thread_function() {
-    ten_str("Нить", 1);
-    return NULL;
-}
+void* threadFunc(void* ignored) {
+    int count = 0;
 
-// Функция вывода ошибки
-void err_print(int error) {
-    fprintf(stderr, "Ошибка: %s\n", strerror(error));
-    _exit(EXIT_FAILURE);
+    while (count < 11) {
+        if (flag == 0) {
+            if (tryToPrint("thread text", count) == 1) {
+                continue;
+            }
+
+            flag = 1;
+            count++;
+        }
+    }
 }
 
 int main() {
     pthread_t thread;
-    int err;
 
     pthread_mutexattr_t attr;
     pthread_mutexattr_init(&attr);
     pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
     pthread_mutex_init(&mutex, &attr);
-    pthread_cond_init(&cond, NULL);
+    pthread_create(&thread, NULL, threadFunc, NULL);
 
-    // Создание новой нити
-    err = pthread_create(&thread, NULL, thread_function, NULL);
-    if (err != 0) {
-        err_print(err);
+    int count = 0;
+
+    while (count < 11) {
+        if (flag == 1) {
+            if (tryToPrint("main text", count) == 1) {
+                continue;
+            }
+
+            count++;
+            flag = 0;
+        }
     }
 
-    // Код основной нити
-    ten_str("Основная нить", 0);
-
-    // Ожидание завершения созданной нити
-    err = pthread_join(thread, NULL);
-    if (err != 0) {
-        err_print(err);
-    }
-
-    pthread_mutex_destroy(&mutex);
-    pthread_cond_destroy(&cond);
-    pthread_mutexattr_destroy(&attr);
-
-    return EXIT_SUCCESS;
+    pthread_exit(NULL);
 }
