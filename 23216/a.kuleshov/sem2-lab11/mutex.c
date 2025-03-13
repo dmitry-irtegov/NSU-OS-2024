9077def2ecab030f27b4108ab8e0069e68ae07c3
MCBivis <Artemiybox@mail.ru>
2025-03-13 19:09:17 +0700

    add

--- 23216/a.kuleshov/sem2-lab11/mutex.c
+++ 23216/a.kuleshov/sem2-lab11/mutex.c
@@ -1,57 +1,66 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-char flag = 1;
 pthread_mutex_t mutex;
+bool turn = true; // true - основная нить false - дочерняя
 
-int tryToPrint(char* text, int i) {
-    if (pthread_mutex_lock(&mutex)) {
-        return 1;
+// Функция вывода 10 строк
+void ten_str(const char* name, bool thread_id) {
+    int cnt = 0;
+    while (cnt < 10) {
+        if (thread_id != turn) {
+            continue;
+        }
+        pthread_mutex_lock(&mutex);
+        printf("[%s] Сообщение %d\n", name, cnt + 1);
+        turn = !turn;
+        cnt++;
+        pthread_mutex_unlock(&mutex);
     }
-
-    printf("%s %d\n", text, i);
-
-    pthread_mutex_unlock(&mutex);
-
-    return 0;
 }
 
-void* threadFunc(void* ignored) {
-    int count = 0;
+// Функция, выполняемая в новой нити
+void *thread_function() {
+    ten_str("Нить", false);
+    return NULL;
+}
 
-    while (count < 11) {
-        if (flag == 0) {
-            if (tryToPrint("thread text", count) == 1) {
-                continue;
-            }
-
-            flag = 1;
-            count++;
-        }
-    }
+// Функция вывода ошибки
+void err_print(int error) {
+    fprintf(stderr, "Ошибка: %s\n", strerror(error));
+    _exit(EXIT_FAILURE);
 }
 
 int main() {
     pthread_t thread;
+    int err;
 
     pthread_mutexattr_t attr;
     pthread_mutexattr_init(&attr);
     pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
     pthread_mutex_init(&mutex, &attr);
-    pthread_create(&thread, NULL, threadFunc, NULL);
 
-    int count = 0;
-
-    while (count < 11) {
-        if (flag == 1) {
-            if (tryToPrint("main text", count) == 1) {
-                continue;
-            }
-
-            count++;
-            flag = 0;
-        }
+    // Создание новой нити
+    err = pthread_create(&thread, NULL, thread_function, NULL);
+    if (err != 0) {
+        err_print(err);
     }
 
-    pthread_exit(NULL);
+    // Код основной нити
+    ten_str("Основная нить", true);
+
+    // Ожидание завершения созданной нити
+    err = pthread_join(thread, NULL);
+    if (err != 0) {
+        err_print(err);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    pthread_mutexattr_destroy(&attr);
+
+    return EXIT_SUCCESS;
 }
