c953354f2444375d2b73ae2733d7f34dc33c77fe
[RomanKomarovv] <r.komarov@g.nsu.ru>
2025-03-17 16:31:48 +0700

    add lab_1

---
+++ 23217/r.komarov/sem2-lab1/lab_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void* thread_function(void* arg) {
+    for (int i = 1; i <= 10; i++) {
+        printf("thread: %d\n", i);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread_id;
+
+    if (pthread_create(&thread_id, NULL, thread_function, NULL) != 0) {
+        perror("thread creation error");
+        return 1;
+    }
+
+    for (int i = 1; i <= 10; i++) {
+        printf("parent: %d\n", i);
+    }
+
+    return 0;
+}
+
