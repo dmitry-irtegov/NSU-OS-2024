f8ee49d1993067da61018bf336e304b01776aac1
A-GigaC <90515245+A-GigaC@users.noreply.github.com>
2025-03-18 15:43:56 +0700

    Create lab4.c

---
+++ 23216/f.pivovarov/sem2-lab4/lab4.c
@@ -0,0 +1,47 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void *printALotOfStrings();
+void errorHandler(int code, char *errorText);
+
+int main() {
+    int code;
+    pthread_t thread;
+    pthread_attr_t attr;
+
+    if ((code = pthread_attr_init(&attr)) != 0) {
+        errorHandler(code, "Attr initialization");
+    }
+
+    if ((code = pthread_create(&thread, &attr, printALotOfStrings, NULL)) != 0) {
+        errorHandler(code, "Creating thread");
+    }
+
+    sleep(2);
+
+    if ((code = pthread_cancel(thread)) != 0) {
+        errorHandler(code, "Troubles with pthread cancellation");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+void *printALotOfStrings() {
+    int i = 1;
+    while (i) {
+        printf("Print's number %d\n", i++);
+    }
+
+    pthread_exit(NULL);
+}
+
+void errorHandler(int code, char *errorText) {
+    char buf[256];
+    strerror_r(code, buf, sizeof(buf));
+    fprintf(stderr, "%s : %s\n", errorText, buf);
+    exit(EXIT_FAILURE);
+}
