d309a383edd376a42b8f778de75c6b5503d82ccd
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-03-18 09:08:09 +0700

    lab2_9

--- 23216/Faranosov/sem2/lab2_9/lab2_9.c
+++ 23216/Faranosov/sem2/lab2_9/lab2_9.c
@@ -15,8 +15,12 @@ int cntThr = 0;
 pthread_t* threads;
 data** datas;
 pthread_attr_t attr;
-pthread_mutex_t mutex;
+pthread_mutex_t mainMutex, thrsMutex;
 int isWork;
+int maxIt;
+int cntSleep;
+pthread_cond_t mainCond;
+pthread_cond_t thrsCond;
 
 
 void handler(char str[], int num) {
@@ -34,7 +38,7 @@ void* func(void* param) {
     printf("Thread start_i == %d, end_i == %d\n", data->start_i, data->end_i);
 #endif
     int start = data->start_i;
-    int finish = start + 200;
+    int finish = start + 100;
     int needWork = 1;
 
     while (needWork) {
@@ -48,13 +52,41 @@ void* func(void* param) {
         start = finish;
         finish = start + 200;
 
-        res = pthread_mutex_lock(&mutex);
-        if (res) handler("mutex lock error", res);
 
         needWork = isWork;
 
-        res = pthread_mutex_unlock(&mutex);
-        if (res) handler("mutex unlock error", res);
+
+    }
+
+    res = pthread_mutex_lock(&thrsMutex);
+    if (res) handler("mutex thrs lock error", res);
+
+
+
+    res = pthread_mutex_lock(&mainMutex);
+    if (res) handler("mutex main (thrs) lock error", res);
+
+    cntSleep++;
+    maxIt = start > maxIt ? start : maxIt;
+
+    res = pthread_cond_signal(&mainCond);
+    if (res) handler("cond mian signal error", res);
+
+    res = pthread_mutex_unlock(&mainMutex);
+    if (res) handler("mutex main (thrs) unlock error", res);
+
+
+
+
+    res = pthread_cond_wait(&thrsCond, &thrsMutex);
+    if (res) handler("cond thrs wait error", res);
+
+    res = pthread_mutex_unlock(&thrsMutex);
+    if (res) handler("mutex thrs lock error", res);
+
+    for (int i = start; i < maxIt; i += data->step) {
+        data->res += 1.0 / (i * 4.0 + 1.0);
+        data->res -= 1.0 / (i * 4.0 + 3.0);
     }
 
 
@@ -78,54 +110,75 @@ void free_after_bad_malloc(int j, pthread_t* t, pthread_attr_t *attr, data** dat
 }
 
 
-void sig_handler() {
-    
+void destroy() {
     int res = 0;
-    double pi = 0;
-
-    res = pthread_mutex_lock(&mutex);
-    if (res) handler("mutex lock error", res);
-
-    isWork = 0;
-
-    res = pthread_mutex_unlock(&mutex);
-    if (res) handler("mutex unlock error", res);
-
-    for (int i = 0; i < cntThr; i++) {
-        res = pthread_join(threads[i], NULL);
-        if (res) handler("join", res);
-    }
-
-    for (int i = 0; i < cntThr; i++) {
-        
-        pi += datas[i]->res;
-        free(datas[i]);
-    }
-    free(threads);
-
-    pi = pi * 4;
-    printf("\npi done - %.15g \n", pi);
 
     res = pthread_attr_destroy(&attr);
     if (res) handler("attr destroy with error", res);
 
-    res = pthread_mutex_destroy(&mutex);
-    if (res) handler("mutex destroy with error", res);
+    res = pthread_mutex_destroy(&mainMutex);
+    if (res) handler("mutex main destroy with error", res);
 
-    exit(EXIT_SUCCESS);
+    res = pthread_mutex_destroy(&thrsMutex);
+    if (res) handler("mutex thrs destroy with error", res);
+
+    res = pthread_cond_destroy(&mainCond);
+    if (res) handler("cond main destroy with error", res);
+
+    res = pthread_cond_destroy(&thrsCond);
+    if (res) handler("cond threads destroy with error", res);
 }
 
+void sig_handler() {
+   
+    isWork = 0;
+}
+
+void init() {
+
+    int check = 0;
+
+
+    check = pthread_mutex_init(&mainMutex, NULL);
+    if (check != 0) {
+        handler("mutex main init", check);
+    }
+
+    check = pthread_mutex_init(&thrsMutex, NULL);
+    if (check != 0) {
+        handler("mutex thrs init", check);
+    }
+
+    check = pthread_cond_init(&mainCond, NULL);
+    if (check != 0) {
+        handler("cond main init", check);
+    }
+
+    check = pthread_cond_init(&thrsCond, NULL);
+    if (check != 0) {
+        handler("cond threads init", check);
+    }
+
+    check = pthread_attr_init(&attr);
+    if (check != 0) {
+        handler("init", check);
+    }
+
+
+}
 
 int main(int argc, char** argv) {
     isWork = 1;
+    maxIt = 0;
+    cntSleep = 0;
+    int check = 0;
+    double pi = 0;
 
 
     if (sigset(SIGINT, sig_handler) == SIG_ERR) {
         perror("sigset error");
         exit(EXIT_FAILURE);
     }
-       
-   
 
     if (argc != 2) {
         fprintf(stderr, "wrong args");
@@ -142,17 +195,6 @@ int main(int argc, char** argv) {
     printf("cntThr == %d\n", cntThr);
 #endif
 
-    int check = 0;
-
-    check = pthread_mutex_init(&mutex, NULL);
-    if (check != 0) {
-        handler("mutex init", check);
-    }
-
-    check = pthread_attr_init(&attr);
-    if (check != 0) {
-        handler("init", check);
-    }
 
     threads = NULL;
     threads = malloc(sizeof(pthread_t) * cntThr);
@@ -192,15 +234,46 @@ int main(int argc, char** argv) {
 
     }
 
-    for (int j = 0; j < cntThr; j++) {
-        data* d = NULL;
 
-        check = pthread_join(threads[j], (void**)&d);
-        if (check != 0) {
-            handler("join error", check);
-        }
+    check = pthread_mutex_lock(&mainMutex);
+    if (check) handler("mutex main lock error", check);
+
+    while (cntSleep < cntThr) {
+
+        check = pthread_cond_wait(&mainCond, &mainMutex);
+        if (check) handler("cond main wait error", check);
+
     }
 
+    check = pthread_mutex_unlock(&mainMutex);
+    if (check) handler("mutex main unlock error", check);
 
-    return (EXIT_SUCCESS);
+    check = pthread_mutex_lock(&thrsMutex);
+    if (check) handler("mutex thrs (main) lock error", check);
+
+    check = pthread_cond_broadcast(&thrsCond);
+    if (check) handler("broadcast error", check);
+
+    check = pthread_mutex_unlock(&thrsMutex);
+    if (check) handler("mutex thrs (main) unlock error", check);
+
+
+    for (int i = 0; i < cntThr; i++) {
+        check = pthread_join(threads[i], NULL);
+        if (check) handler("join", check);
+    }
+
+    for (int i = 0; i < cntThr; i++) {
+
+        pi += datas[i]->res;
+        free(datas[i]);
+    }
+    free(threads);
+
+    pi = pi * 4;
+    printf("\npi done - %.15g \n", pi);
+
+    destroy();
+
+    exit(EXIT_SUCCESS);
 }
