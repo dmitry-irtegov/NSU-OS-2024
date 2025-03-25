a7543da281c2ec0e869e41ecbbf555edc242391b
Cordis <108444115+Cordip@users.noreply.github.com>
2025-03-25 15:20:01 +0700

    refactor 5

--- 23216/m.gavrilev/sem2-lab8/main.go
+++ 23216/m.gavrilev/sem2-lab8/main.go
@@ -1,67 +1,65 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"sync"
-	"strconv"
+        "fmt"
+        "os"
+        "sync"
+        "strconv"
 )
 
 func calculatePi(start int, number_of_thread int, limit int, chanPi chan float64) {
 
-	var pi float64
+        var pi float64
 
-	for i := start; i < limit; i += number_of_thread {
+        for i := start; i < limit; i += number_of_thread {
 
-		pi += float64(1.0 / (float64(i*4.0) + 1.0))
-		pi -= float64(1.0 / (float64(i*4.0) + 3.0))
-	}
+                pi += float64(1.0 / (float64(i*4.0) + 1.0))
+                pi -= float64(1.0 / (float64(i*4.0) + 3.0))
+        }
 
-	pi = pi * 4.0
-	// fmt.Println("part of pi - ", pi)
-	chanPi <- pi
+        pi = pi * 4.0
+        // fmt.Println("part of pi - ", pi)
+        chanPi <- pi
 }
 
 func main() {
-	num_steps := 20000000
-	var wg sync.WaitGroup
+        num_steps := 20000000
+        var wg sync.WaitGroup
 
-	if len(os.Args) < 2 {
-		fmt.Println("write number of threads")
-		return
-	}
+        if len(os.Args) < 2 {
+                fmt.Println("write number of threads")
+                return
+        }
 
-	number_of_thread, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Println("error with parsing to int: number_of_thread", err)
-		return
-	}
-	if number_of_thread < 1 {
-		fmt.Println("illegal argument for number_of_threads (write more than 0)")
-		return
-	}
-	chanPi := make(chan float64, number_of_thread)
+        number_of_thread, err := strconv.Atoi(os.Args[1])
+        if err != nil {
+                fmt.Println("error with parsing to int: number_of_thread", err)
+                return
+        }
+        if number_of_thread < 1 {
+                fmt.Println("illegal argument for number_of_threads (write more than 0)")
+                return
+        }
+        chanPi := make(chan float64, number_of_thread)
 
-	for i := 0; i < number_of_thread; i++ {
-		wg.Add(1)
+        for i := 0; i < number_of_thread; i++ {
+                wg.Add(1)
 		go func(i int) {
-			defer wg.Done()
-			calculatePi(i, number_of_thread, num_steps, chanPi)
-		}(i)
-	}
+                        defer wg.Done()
+                        calculatePi(i, number_of_thread, num_steps, chanPi)
+                }(i)
+        }
 
-	var pi float64
+        var pi float64
 
-	wg.Wait()
+        wg.Wait()
 
-	for {
-		select {
-		case tmp := <-chanPi:
-			pi += tmp
-		case <-chanPi:
-			break
-		}
-	}
+        for i := 0; i < number_of_thread; i++  {
+                tmp := <-chanPi
+                pi += tmp
+        }
 
-	fmt.Println("pi done - ", pi)
+        close(chanPi)
+
+        fmt.Println("pi done - ", pi)
 }
