e639da2ef34d103157b4bb8a215da5f72ae77495
Maksim <83348729+povidllo@users.noreply.github.com>
2025-03-31 16:05:29 +0700

    sem2-lab10 (#1049)

---
+++ 23217/m.kuzminov/sem2-lab10/din_phil.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define PHILO 5
+#define DELAY 30000
+#define FOOD 50
+
+pthread_mutex_t forks[PHILO];
+pthread_t phils[PHILO];
+void *philosopher (void *id);
+int food_on_table ();
+void get_fork (int, int, char *);
+void down_forks (int, int);
+pthread_mutex_t foodlock;
+
+int sleep_seconds = 0;
+
+int main(int argn, char **argv) {
+  int i;
+
+  if (argn == 2)
+    sleep_seconds = atoi (argv[1]);
+
+  pthread_mutex_init (&foodlock, NULL);
+  for (i = 0; i < PHILO; i++)
+    pthread_mutex_init (&forks[i], NULL);
+  for (i = 0; i < PHILO; i++)
+    if(pthread_create (&phils[i], NULL, philosopher, (void *)i) != 0) {
+      perror("thread create");
+      exit(1);
+    }
+  for (i = 0; i < PHILO; i++)
+    if(pthread_join (phils[i], NULL) != 0) {
+      perror("join");
+      exit(1);
+    }
+
+  for(i = 0; i < PHILO; i++) {
+    if(pthread_mutex_destroy(&forks[i]) != 0) {
+      perror("mutex, destroy");
+      exit(1);
+    }
+  }
+  
+  if(pthread_mutex_destroy(&foodlock) != 0) {
+    perror("mutex, destroy");
+    exit(1);
+  }
+
+  return 0;
+}
+
+void* philosopher (void *num) {
+  int id;
+  int left_fork, right_fork, f;
+
+  id = (int)num;
+  printf ("Philosopher %d sitting down to dinner.\n", id);
+  right_fork = id;
+  left_fork = id + 1;
+ 
+  /* Wrap around the forks. */
+  if (left_fork == PHILO)
+    left_fork = 0;
+ 
+  while (f = food_on_table ()) {
+    if(id == PHILO - 1) {
+      printf ("Philosopher %d: get dish %d.\n", id, f);
+      get_fork (id, right_fork, "right");
+      get_fork (id, left_fork, "left ");
+  
+      printf ("Philosopher %d: eating.\n", id);
+      usleep (DELAY * (FOOD - f + 1));
+      down_forks (left_fork, right_fork);
+    } else {
+      printf ("Philosopher %d: get dish %d.\n", id, f);
+      get_fork (id, left_fork, "left");
+      get_fork (id, right_fork, "right ");
+  
+      printf ("Philosopher %d: eating.\n", id);
+      usleep (DELAY * (FOOD - f + 1));
+      down_forks (left_fork, right_fork);
+    }
+  }
+  printf ("Philosopher %d is done eating.\n", id);
+  return (NULL);
+}
+
+int food_on_table () {
+  static int food = FOOD;
+  int myfood;
+
+  if(pthread_mutex_lock (&foodlock) != 0) {
+    perror("lock");
+    exit(1);
+  }
+  if (food > 0) {
+    food--;
+  }
+  myfood = food;
+  if(pthread_mutex_unlock (&foodlock) != 0) {
+    perror("unlock)");
+    exit(1);
+  }
+  return myfood;
+}
+
+void get_fork (int phil, int fork, char *hand) {
+  if(pthread_mutex_lock (&forks[fork]) != 0) {
+    perror("unlock)");
+    exit(1);
+  }
+  printf ("Philosopher %d: got %s fork %d\n", phil, hand, fork);
+}
+
+void down_forks (int f1, int f2) {
+  if(pthread_mutex_unlock (&forks[f1]) != 0) {
+    perror("unlock)");
+    exit(1);
+  }
+  if(pthread_mutex_unlock (&forks[f2]) != 0) {
+    perror("unlock)");
+    exit(1);
+  }
+
+}
