bb7d1dcb11c0e19a0c5cb4dc3d0331a2608ad0ba
Bu1nk <104312454+Bu1nk@users.noreply.github.com>
2025-03-31 16:16:09 +0700

    23217/Abramkin_Nikita/sem2-lab1 (#962)
    * Added core files, .DS_Store and .nfs* to .gitignore
    
    Update .gitignore
    
    add lab2
    
    * add lab3
    
    * remove lab2/
    
    * add lab9
    
    * add lab9
    
    * add lab10
    
    * rm lab3 lab9
    
    * old gitignore
    
    * add lab25
    
    * add lab26
    
    * add lab30
    
    * plus h
    
    * allwhile
    
    * s and cl lab30
    
    * not fail server after first client
    
    * --amend
    
    * shutting down server by stopword
    
    * --amend
    
    * generating socket file
    
    * --amend
    
    * --amend
    
    * --amend
    
    * --amend
    
    * --amend
    
    * cycle for server work
    
    * add lab31
    
    * add sem2-lab1
    
    * add sem2-lab1
    
    * rm usleep
    
    ---------
    
    Co-authored-by: Dmitry Irtegov <dirtegov@eastbanctech.com>

---
+++ 23217/n.abramkin/sem2-lab1/sem2-lab1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* thread_function(void* arg) {
+    for (int i = 0; i < 10; i++) {
+        printf("[Child Thread] Line %d\n", i + 1);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+    
+    pthread_create(&thread, NULL, thread_function, NULL);
+
+    for (int i = 0; i < 10; i++) {
+        printf("[Main Thread] Line %d\n", i + 1);
+    }
+
+    pthread_exit(NULL);
+}
