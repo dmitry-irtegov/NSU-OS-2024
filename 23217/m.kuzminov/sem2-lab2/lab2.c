3292df55fd491b446bb0559ac03cc6e4bf92f07e
Maksim <83348729+povidllo@users.noreply.github.com>
2025-03-31 15:17:22 +0700

    sem2-lab2 (#961)

---
+++ 23217/m.kuzminov/sem2-lab2/lab2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* thread_func(void* param) {
+    for(int i = 0; i < 10; i++) {
+        printf("%d:new thread\n", i);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t thread;
+
+    int code = pthread_create(&thread, NULL, thread_func, NULL);
+    if (code != 0) {
+        perror("creating thread");
+        exit(1);
+    }
+
+    if (pthread_join(thread, NULL) != 0) {
+        perror("join");
+        exit(1);
+    }
+
+    for(int i = 0; i < 10; i++) {
+        printf("%d:main thread\n", i);
+    }
+}
