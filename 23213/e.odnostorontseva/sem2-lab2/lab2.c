fc3edfbe0b655ec78407f2b237577542cfbdac18
Odnostorontseva Evgenia <e.odnostorontseva@g.nsu.ru>
2025-04-02 12:39:15 +0700

    Odnostorontseva Evgenia lab2

---
+++ 23213/e.odnostorontseva/sem2-lab2/lab2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+
+void* func(void* arg) {
+    for (int i = 0; i < 10; i++) {
+        printf("New thread: string of text\n");
+    }
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t thread;
+
+    int res = pthread_create(&thread, NULL, func, NULL);
+    if (res != 0) {
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], strerror(res));
+        exit(1);
+    }
+
+    pthread_join(thread, NULL);
+
+    for (int i = 0; i < 10; i++) {
+        printf("Main thread: string of text\n");
+    }
+
+    return 0;
+}
