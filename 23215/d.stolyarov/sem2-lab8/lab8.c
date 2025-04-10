e8c74b3898dcda2ebeaa903516f4168eb55f505d
Dimandr <Dmitry221556@yandex.ru>
2025-04-10 13:28:32 +0700

    upd

--- 23215/d.stolyarov/sem2-lab8/lab8.c
+++ 23215/d.stolyarov/sem2-lab8/lab8.c
@@ -4,19 +4,22 @@
 #include <string.h>
 
 #define num_steps 800000000
-void * calc_pi(void * params){
+typedef struct params{
+    int left;
+    int right;
+    double res;
+}params;
+void * calc_pi(void * arg){
+    params * p = (params*) arg;
     double piPart = 0;
-    int from = *((int*) ((void**)params)[0]);
-    int to = *((int*) ((void**)params)[1]);
 
-    for (int i = from; i < to ; i++) {
+    for (int i = p->left; i < p->right ; i++) {
         piPart += 1.0/(i*4.0 + 1.0);
         piPart -= 1.0/(i*4.0 + 3.0);
     }
-    *((double*)((void**)params)[2]) = piPart;
+    p->res = piPart;
     pthread_exit(0);
 }
-
 int main(int argc, char** argv) {
     
     if(argc < 2){
@@ -38,33 +41,30 @@ int main(int argc, char** argv) {
         exit(2);
     }
 
-    void *** threadsParams = (void***) malloc(sizeof(void**) *  threadAmount);
+    params * threadsParams = (params*) calloc(threadAmount, sizeof(params));
     pthread_t * threads = (pthread_t*) malloc(sizeof(pthread_t) * threadAmount);
     
     int threadPart = (steps - 1) / threadAmount + 1;
     for(int i = 0; i < threadAmount; i++){
-        void ** params = (void **)malloc(sizeof(void *) * 3);
-        params[0] = (int*)malloc(sizeof(int)); //вычисляем пи, начиная с элемента под таким номером
+        params p = threadsParams[i];
+        /*
+        p->left = (int*)malloc(sizeof(int)); //вычисляем пи, начиная с элемента под таким номером
         params[1] = (int*)malloc(sizeof(int)); //и заканчиваем этим номером
-        params[2] = (double*)malloc(sizeof(double)); //сюда положим результат
-        *((int*)params[0]) = i * threadPart;
+        params[2] = (double*)malloc(sizeof(double)); //сюда положим результат*/
+        p.left = i * threadPart;
         if(i == threadAmount - 1){
-            *((int*)params[1]) = steps;
+            p.right = steps;
         }
         else{
-            *((int*)params[1]) = (i+1) * threadPart;
+            p.right = (i+1) * threadPart;
         }
-        pthread_create(threads + i, NULL, calc_pi, params);
-        threadsParams[i] = params;
+        threadsParams[i] = p;
+        pthread_create(threads + i, NULL, calc_pi, threadsParams+i);
     }
 
     for(int i = 0; i < threadAmount; i++){
         pthread_join(threads[i], NULL);
-        pi += *((double*)threadsParams[i][2]);
-        free(threadsParams[i][0]);
-        free(threadsParams[i][1]);
-        free(threadsParams[i][2]);
-        free(threadsParams[i]);
+        pi += threadsParams[i].res;
     }
 
     pi = pi * 4.0;
