936f5736e35f0a806ac102aadfb3edeeac896ba0
Daria Lavitskaya <d.lavitskaya@g.nsu.ru>
2025-04-28 11:06:15 +0700

    sem2-lab5

---
+++ 23214/d.lavitskaya/sem2-lab5/sem2-lab5.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+
+void cleanup_handler(void *arg) {
+    printf("[Thread] Cleanup: Thread is exiting...\n");
+}
+
+void *print_messages(void *arg) {
+    pthread_cleanup_push(cleanup_handler, NULL);
+
+    while (1) {
+        printf("[Thread] Running...\n");
+    }
+
+    pthread_cleanup_pop(0); 
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t thread;
+    int code;
+    char* buf;
+
+    code = pthread_create(&thread, NULL, print_messages, NULL);
+    if (code != 0) {
+        buf = strerror(code);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+
+    sleep(2);
+
+    printf("[Main] Cancelling thread...\n");
+    code = pthread_cancel(thread);
+    if (code != 0) {
+        buf = strerror(code);
+        fprintf(stderr, "%s: canceling thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+
+    code = pthread_join(thread, NULL);
+    if (code != 0) {
+        buf = strerror(code);
+        fprintf(stderr, "%s: joining thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+
+    printf("[Main] Thread cancelled, exiting program.\n");
+    return 0;
+}
