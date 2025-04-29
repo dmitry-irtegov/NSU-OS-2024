71092eba19a01df25d4572b09e4ad79d2bb8ea24
Nikita Kochanov <n.kochanov@g.nsu.ru>
2025-04-30 00:32:17 +0700

    fixed

--- 23215/n.kochanov/sem2_lab28/sem2_lab28.c
+++ 23215/n.kochanov/sem2_lab28/sem2_lab28.c
@@ -3,10 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <termios.h>
-#include <fcntl.h>
 #include <netdb.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <arpa/inet.h>
 #include <sys/select.h>
 
@@ -24,23 +22,34 @@ void disable_raw_mode(struct termios *original_termios) {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, original_termios);
 }
 
-void parse_url(const char *url, char *host, char *path) {
+void parse_url(const char *url, char *host, char *path, int *port) {
+    *port = PORT;
+
     if (strncmp(url, "http://", 7) == 0) {
         url += 7;
     }
 
     const char *slash = strchr(url, '/');
+    const char *host_end = slash ? slash : url + strlen(url);
+
+    const char *colon = strchr(url, ':');
+    if (colon && colon < host_end) {
+        strncpy(host, url, colon - url);
+        host[colon - url] = '\0';
+        *port = atoi(colon + 1);
+    } else {
+        strncpy(host, url, host_end - url);
+        host[host_end - url] = '\0';
+    }
+
     if (slash) {
-        strncpy(host, url, slash - url);
-        host[slash - url] = '\0';
         strcpy(path, slash);
     } else {
-        strcpy(host, url);
         strcpy(path, "/");
     }
 }
 
-int create_socket(const char *host) {
+int create_socket(const char *host, int port) {
     struct hostent *server = gethostbyname(host);
     if (!server) {
         fprintf(stderr, "No such host: %s\n", host);
@@ -55,7 +64,7 @@ int create_socket(const char *host) {
 
     struct sockaddr_in serv_addr = {0};
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(port);
     memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
 
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
@@ -74,9 +83,10 @@ int main(int argc, char *argv[]) {
     }
 
     char host[256], path[1024];
-    parse_url(argv[1], host, path);
+    int port;
+    parse_url(argv[1], host, path, &port);
 
-    int sockfd = create_socket(host);
+    int sockfd = create_socket(host, port);
     if (sockfd < 0) return 1;
 
     char request[2048];
@@ -85,58 +95,63 @@ int main(int argc, char *argv[]) {
              path, host);
     send(sockfd, request, strlen(request), 0);
 
-    fd_set readfds;
-    char buffer[BUFFER_SIZE];
-    int lines_printed = 0;
-    int show_more = 1;
+    size_t total_size = 0;
+    size_t capacity = BUFFER_SIZE;
+    char *response = malloc(capacity);
+    if (!response) {
+        fprintf(stderr, "Memory allocation failed\n");
+        close(sockfd);
+        return 1;
+    }
+
+    while (1) {
+        if (total_size + BUFFER_SIZE >= capacity) {
+            capacity *= 2;
+            response = realloc(response, capacity);
+            if (!response) {
+                fprintf(stderr, "Memory reallocation failed\n");
+                close(sockfd);
+                return 1;
+            }
+        }
+        ssize_t n = read(sockfd, response + total_size, BUFFER_SIZE);
+        if (n <= 0) break;
+        total_size += n;
+    }
+    response[total_size] = '\0';
+    close(sockfd);
+
+    char **lines = NULL;
+    size_t num_lines = 0;
+    char *line = strtok(response, "\n");
+    while (line) {
+        lines = realloc(lines, sizeof(char*) * (num_lines + 1));
+        lines[num_lines++] = line;
+        line = strtok(NULL, "\n");
+    }
 
     struct termios original_termios;
     tcgetattr(STDIN_FILENO, &original_termios);
     enable_raw_mode(&original_termios);
 
-    while (1) {
-        FD_ZERO(&readfds);
-        FD_SET(sockfd, &readfds);
-        FD_SET(STDIN_FILENO, &readfds);
-        int maxfd = sockfd > STDIN_FILENO ? sockfd : STDIN_FILENO;
-
-        if (select(maxfd + 1, &readfds, NULL, NULL, NULL) < 0) {
-            perror("select");
-            break;
+    size_t current_line = 0;
+    while (current_line < num_lines) {
+        size_t lines_to_print = LINES_PER_PAGE;
+        while (lines_to_print-- && current_line < num_lines) {
+            printf("%s\n", lines[current_line++]);
         }
 
-        if (FD_ISSET(sockfd, &readfds) && show_more) {
-            int n = read(sockfd, buffer, sizeof(buffer) - 1);
-            if (n <= 0) break;
-            buffer[n] = '\0';
+        if (current_line < num_lines) {
+            printf("\n--- Press space to scroll down, any other key to quit ---\n");
+            fflush(stdout);
 
-            char *line = strtok(buffer, "\n");
-            while (line) {
-                printf("%s\n", line);
-                lines_printed++;
-
-                if (lines_printed >= LINES_PER_PAGE) {
-                    printf("\n--- Press space to scroll down ---\n");
-                    fflush(stdout);
-                    show_more = 0;
-                    break;
-                }
-                line = strtok(NULL, "\n");
-            }
-        }
-
-        if (FD_ISSET(STDIN_FILENO, &readfds)) {
             char c;
-            if (read(STDIN_FILENO, &c, 1) > 0) {
-                if (c == ' ') {
-                    lines_printed = 0;
-                    show_more = 1;
-                }
-            }
+            if (read(STDIN_FILENO, &c, 1) <= 0 || c != ' ') break;
         }
     }
 
     disable_raw_mode(&original_termios);
-    close(sockfd);
+    free(response);
+    free(lines);
     return 0;
 }
