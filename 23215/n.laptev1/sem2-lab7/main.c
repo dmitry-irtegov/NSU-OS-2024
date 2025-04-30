87a32077128dba2a6b80c9931b66e92687b48c21
champion <n.laptev1@g.nsu.ru>
2025-04-30 17:56:44 +0700

    logs

--- 23215/n.laptev1/sem2-lab7/main.c
+++ 23215/n.laptev1/sem2-lab7/main.c
@@ -8,8 +8,10 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/resource.h>
 
 #define BUFSIZE 4096
+#define FILEDESCLIMIT 5
 
 char *srcdir, *dstdir;
 
@@ -20,9 +22,13 @@ int create_thread_with_retry(pthread_t *threadp, void *(*body)(void *), void *ar
     int err;
     do {
         err = pthread_create(threadp, NULL, body, arg);
-        if (err != EAGAIN) return err;
-        printf("Waiting for free threads\n");
-        sleep(1);
+        if (err == EAGAIN) {
+            printf("[create_thread] Resource busy, retrying...\n");
+            sleep(1);
+        } else if (err != 0) {
+            fprintf(stderr, "[create_thread] pthread_create error: %s\n", strerror(err));
+            return err;
+        }
     } while (err != 0);
     pthread_detach(*threadp);
     return 0;
@@ -32,7 +38,7 @@ void *copy_file(void *param) {
     char *filename = (char *)param;
     char srcpathname[strlen(srcdir) + strlen(filename) + 2];
     char dstpathname[strlen(dstdir) + strlen(filename) + 2];
-    int srcfile, dstfile;
+    int srcfile = -1, dstfile = -1;
     char buf[BUFSIZE];
     struct stat stat_buf;
     ssize_t was_read;
@@ -41,40 +47,58 @@ void *copy_file(void *param) {
     snprintf(dstpathname, sizeof(dstpathname), "%s/%s", dstdir, filename);
     free(param);
 
+    printf("[copy_file] Trying to open src '%s' and dst '%s'\n", srcpathname, dstpathname);
     pthread_mutex_lock(&fileopen_mutex);
     do {
         srcfile = open(srcpathname, O_RDONLY);
-        if (srcfile < 0 && errno != EMFILE) {
-            perror(srcpathname);
-            free(filename);
-            pthread_mutex_unlock(&fileopen_mutex);
-            return NULL;
-        }
-        if (srcfile >= 0) {
-            fstat(srcfile, &stat_buf);
-            dstfile = open(dstpathname, O_WRONLY | O_CREAT, stat_buf.st_mode);// Тип файла и права доступа (File type and mode).
-            if (dstfile < 0 && errno != EMFILE) {
-                perror(dstpathname);
-                close(srcfile);
+        if (srcfile < 0) {
+            if (errno == EMFILE) {
+                printf("[copy_file] EMFILE when opening src '%s', waiting...\n", srcpathname);
+            } else {
+                perror("[copy_file] open src error");
                 pthread_mutex_unlock(&fileopen_mutex);
-                pthread_cond_signal(&fileopen_cond);
+                free(filename);
                 return NULL;
+            }
+        } else {
+            printf("[copy_file] Opened src '%s' (fd=%d)\n", srcpathname, srcfile);
+            if (fstat(srcfile, &stat_buf) < 0) {
+                perror("[copy_file] fstat error");
+            }
+            dstfile = open(dstpathname, O_WRONLY | O_CREAT, stat_buf.st_mode);
+            if (dstfile < 0) {
+                if (errno == EMFILE) {
+                    printf("[copy_file] EMFILE when opening dst '%s'...\n", dstpathname);
+                    close(srcfile);
+                    srcfile = -1;
+                } else {
+                    perror("[copy_file] open dst error");
+                    close(srcfile);
+                    pthread_mutex_unlock(&fileopen_mutex);
+                    pthread_cond_signal(&fileopen_cond);
+                    return NULL;
+                }
+            } else {
+                printf("[copy_file] Opened dst '%s' (fd=%d)\n", dstpathname, dstfile);
             }
         }
         if (srcfile < 0 || dstfile < 0) {
-            if (srcfile >= 0) {
-                close(srcfile);
-            }    
             pthread_cond_wait(&fileopen_cond, &fileopen_mutex);
         }
     } while (srcfile < 0 || dstfile < 0);
     pthread_mutex_unlock(&fileopen_mutex);
+    printf("[copy_file] Starting copy '%s' -> '%s'\n", srcpathname, dstpathname);
 
-    while ((was_read = read(srcfile, buf, sizeof buf)) > 0) {
-        if (write(dstfile, buf, was_read) < 0) break;
+    while ((was_read = read(srcfile, buf, BUFSIZE)) > 0) {
+        if (write(dstfile, buf, was_read) < 0) {
+            perror("[copy_file] write error");
+            break;
+        }
     }
+
     close(srcfile);
     close(dstfile);
+    printf("[copy_file] Finished copy '%s'\n", srcpathname, dstpathname);
     pthread_cond_signal(&fileopen_cond);
     return NULL;
 }
@@ -83,36 +107,40 @@ void *copy_directory(void *param) {
     char *dirname = (char *)param;
     char srcpathname[strlen(srcdir) + strlen(dirname) + 2];
     char dstpathname[strlen(dstdir) + strlen(dirname) + 2];
-    DIR *srcdirp;
+    DIR *srcdirp = NULL;
     struct stat stat_buf;
 
     snprintf(srcpathname, sizeof(srcpathname), "%s/%s", srcdir, dirname);
     snprintf(dstpathname, sizeof(dstpathname), "%s/%s", dstdir, dirname);
     free(param);
 
+    printf("[copy_dir] Trying to open dir '%s'\n", srcpathname);
     pthread_mutex_lock(&fileopen_mutex);
     do {
         srcdirp = opendir(srcpathname);
-        if (srcdirp == NULL && errno != EMFILE) {
-            perror(srcpathname);
-            pthread_mutex_unlock(&fileopen_mutex);
-            return NULL;
-        }
-        if (srcdirp == NULL && errno == EMFILE) {
-            pthread_cond_wait(&fileopen_cond, &fileopen_mutex);
+        if (srcdirp == NULL) {
+            if (errno == EMFILE) {
+                printf("[copy_dir] EMFILE when opening dir '%s', waiting...\n", srcpathname);
+                pthread_cond_wait(&fileopen_cond, &fileopen_mutex);
+            } else {
+                perror("[copy_dir] opendir error");
+                pthread_mutex_unlock(&fileopen_mutex);
+                return NULL;
+            }
+        } else {
+            printf("[copy_dir] Opened dir '%s'\n", srcpathname);
         }
     } while (srcdirp == NULL);
     pthread_mutex_unlock(&fileopen_mutex);
 
-    mkdir(dstpathname, 0755);
+    if (mkdir(dstpathname, 0755) == 0) {
+        printf("[copy_dir] Created directory '%s'\n", dstpathname);
+    }
 
     size_t max_fname_length = pathconf(srcpathname, _PC_NAME_MAX);
-    if (max_fname_length == -1) {
-        perror("pathconf");
-        return NULL;
-    }
     struct dirent *entry = malloc(sizeof(struct dirent) + max_fname_length + 2);
     struct dirent *result;
+
     while (readdir_r(srcdirp, entry, &result) == 0 && result != NULL) {
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) continue;
         char absolutepathname[strlen(srcpathname) + strlen(entry->d_name) + 2];
@@ -128,13 +156,19 @@ void *copy_directory(void *param) {
             free(relativepathname);
         }
     }
+
     free(entry);
     closedir(srcdirp);
+    printf("[copy_dir] Closed dir '%s'\n", srcpathname);
     pthread_cond_signal(&fileopen_cond);
     return NULL;
 }
 
+
 int main(int argc, char **argv) {
+    struct rlimit rl;
+    rl.rlim_cur = rl.rlim_max = FILEDESCLIMIT;  // Ограничим до 5 дескрипторов
+    setrlimit(RLIMIT_NOFILE, &rl);
     if (argc < 3) {
         fprintf(stderr, "Usage: %s srcdir dstdir\n", argv[0]);
         exit(0);
