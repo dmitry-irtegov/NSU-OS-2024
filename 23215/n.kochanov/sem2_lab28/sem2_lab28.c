dd70986aee370fa026b7bbf9c9a9634fba8e876f
Nikita Kochanov <n.kochanov@g.nsu.ru>
2025-04-30 21:08:55 +0700

    fixed5

--- 23215/n.kochanov/sem2_lab28/sem2_lab28.c
+++ 23215/n.kochanov/sem2_lab28/sem2_lab28.c
@@ -9,6 +9,14 @@
 
 #define SCREEN_LINES 25
 #define DEFAULT_PORT 80
+#define BUFFER_SIZE 65536
+
+typedef struct {
+    char *data;
+    size_t size;
+    size_t capacity;
+    size_t display_pos;
+} ContentBuffer;
 
 void parse_url(const char *url, char *host, char *path, int *port) {
     *port = DEFAULT_PORT;
@@ -52,13 +60,35 @@ void configure_terminal(int enable) {
     }
 }
 
+void buffer_append(ContentBuffer *buf, const char *data, size_t len) {
+    while (buf->size + len + 1 > buf->capacity) {
+        buf->capacity = buf->capacity ? buf->capacity * 2 : 4096;
+        buf->data = realloc(buf->data, buf->capacity);
+    }
+    memcpy(buf->data + buf->size, data, len);
+    buf->size += len;
+    buf->data[buf->size] = '\0';
+}
+
+void buffer_init(ContentBuffer *buf) {
+    buf->data = NULL;
+    buf->size = 0;
+    buf->capacity = 0;
+    buf->display_pos = 0;
+}
+
+void buffer_free(ContentBuffer *buf) {
+    free(buf->data);
+    buf->data = NULL;
+}
+
 int main(int argc, char *argv[]) {
     char host[256] = {0};
     char path[1024] = {0};
     int port;
     char request[2048];
-    char buffer[4096];
     char port_str[6] = {0};
+    ContentBuffer content = {0};
     
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <URL>\n", argv[0]);
@@ -104,77 +134,86 @@ int main(int argc, char *argv[]) {
     send(connection_fd, request, strlen(request), 0);
 
     configure_terminal(1);
+    buffer_init(&content);
 
     int line_count = 0;
     int pause_mode = 0;
-    int body_started = 0;
+    int header_processed = 0;
     fd_set descriptors;
 
-    while (1) {
-        FD_ZERO(&descriptors);
-        FD_SET(connection_fd, &descriptors);
-        if (pause_mode) FD_SET(STDIN_FILENO, &descriptors);
+int connection_closed = 0;
 
-        if (select(connection_fd + 1, &descriptors, NULL, NULL, NULL) < 0) {
-            perror("Selection error");
-            break;
-        }
+while (1) {
+    FD_ZERO(&descriptors);
+    if (!connection_closed) FD_SET(connection_fd, &descriptors);
+    FD_SET(STDIN_FILENO, &descriptors);
 
-        if (FD_ISSET(connection_fd, &descriptors) && !pause_mode) {
-            int bytes = recv(connection_fd, buffer, sizeof(buffer) - 1, 0);
-            if (bytes <= 0) break;
+    if (select(connection_fd + 1, &descriptors, NULL, NULL, NULL) < 0) {
+        perror("Selection error");
+        break;
+    }
+
+    if (!connection_closed && FD_ISSET(connection_fd, &descriptors)) {
+        char buffer[4096];
+        int bytes = recv(connection_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes <= 0) {
+            connection_closed = 1;
+            shutdown(connection_fd, SHUT_RD);
+        } else {
             buffer[bytes] = '\0';
-
-            if (!body_started) {
-                char *header_end = strstr(buffer, "\r\n\r\n");
-                if (header_end) {
-                    body_started = 1;
-                    char *body = header_end + 4;
-                    int body_len = bytes - (body - buffer);
-                    for (int i = 0; i < body_len; i++) {
-                        putchar(body[i]);
-                        if (body[i] == '\n') {
-                            if (++line_count >= SCREEN_LINES) {
-                                printf("\n-- PAUSED (press space) --");
-                                fflush(stdout);
-                                pause_mode = 1;
-                                line_count = 0;
-                                break;
-                            }
-                        }
-                        if (pause_mode) break;
-                    }
-                }
-            } else {
-                for (int i = 0; i < bytes; i++) {
-                    putchar(buffer[i]);
-                    if (buffer[i] == '\n') {
-                        if (++line_count >= SCREEN_LINES) {
-                            printf("\n-- PAUSED (press space) --");
-                            fflush(stdout);
-                            pause_mode = 1;
-                            line_count = 0;
-                            break;
-                        }
-                    }
-                    if (pause_mode) break;
-                }
-            }
-            fflush(stdout);
-        }
-
-        if (pause_mode && FD_ISSET(STDIN_FILENO, &descriptors)) {
-            char input;
-            read(STDIN_FILENO, &input, 1);
-            if (input == ' ') {
-                pause_mode = 0;
-                printf("\n");
-                fflush(stdout);
-            }
+            buffer_append(&content, buffer, bytes);
         }
     }
 
+    if (!header_processed) {
+        char *header_end = strstr(content.data, "\r\n\r\n");
+        if (header_end) {
+            content.display_pos = header_end - content.data + 4;
+            header_processed = 1;
+        }
+    }
+
+    if (header_processed && !pause_mode) {
+        size_t start = content.display_pos;
+        size_t printed = 0;
+        int lines_printed = 0;
+
+        while (start + printed < content.size) {
+            if (content.data[start + printed] == '\n') {
+                lines_printed++;
+                if (lines_printed >= SCREEN_LINES) {
+                    printed++;
+                    break;
+                }
+            }
+            printed++;
+        }
+
+        if (printed > 0) {
+            write(STDOUT_FILENO, content.data + start, printed);
+            content.display_pos += printed;
+
+            pause_mode = 1;
+            printf("\n-- PAUSED (press space) --");
+            fflush(stdout);
+        } else if (connection_closed) {
+            break;
+        }
+    }
+
+    if (FD_ISSET(STDIN_FILENO, &descriptors) && pause_mode) {
+        char input;
+        read(STDIN_FILENO, &input, 1);
+        if (input == ' ') {
+            printf("\n");
+            fflush(stdout);
+            pause_mode = 0;
+        }
+    }
+}
+
     close(connection_fd);
     configure_terminal(0);
+    buffer_free(&content);
     return 0;
 }
