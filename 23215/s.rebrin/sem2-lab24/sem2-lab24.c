3149e867c14cdcae55b7b00d21eeec7694025180
ssrebrin <159110330+ssrebrin@users.noreply.github.com>
2025-04-30 18:47:18 +0700

    Rebrin Sem2-lab24 (#1195)
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a

---
+++ 23215/s.rebrin/sem2-lab24/sem2-lab24.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+sem_t A;
+sem_t B;
+sem_t C;
+sem_t module;
+
+void* thread_body_a(void* param) {
+
+    while (1) {
+        sleep(1);
+        sem_post(&A);
+        write(0, "Part A is manufactured\n", 23);
+    }
+    return NULL;
+}
+void* thread_body_b(void* param) {
+
+    while (1) {
+        sleep(2);
+        sem_post(&B);
+        write(0, "Part B is manufactured\n", 23);
+    }
+    return NULL;
+}
+void* thread_body_c(void* param) {
+
+    while (1) {
+        sleep(3);
+        sem_post(&C);
+        write(0, "Part C is manufactured\n", 23);
+    }
+    return NULL;
+}
+void* thread_body_module(void* param) {
+
+    while (1) {
+        sem_wait(&A);
+        sem_wait(&B);
+        sem_post(&module);
+        write(0, "Module is manufactured\n", 23);
+    }
+    return NULL;
+}
+void* thread_body_widget(void* param) {
+
+    while (1) {
+        sem_wait(&module);
+        sem_wait(&C);
+        write(0, "Widget is manufactured\n", 23);
+    }
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t thread[5];
+    int code;
+
+    sem_init(&A, 0, 0);
+    sem_init(&B, 0, 0);
+    sem_init(&C, 0, 0);
+    sem_init(&module, 0, 0);
+
+    code = pthread_create(&thread[0], NULL, thread_body_a, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+    code = pthread_create(&thread[1], NULL, thread_body_b, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+    code = pthread_create(&thread[2], NULL, thread_body_c, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+    code = pthread_create(&thread[3], NULL, thread_body_module, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+    code = pthread_create(&thread[4], NULL, thread_body_widget, NULL);
+    if (code != 0) {
+        char buf[256];
+        strerror_r(code, buf, sizeof buf);
+        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
+        exit(1);
+    }
+
+
+    pthread_exit(NULL);
+}
+
