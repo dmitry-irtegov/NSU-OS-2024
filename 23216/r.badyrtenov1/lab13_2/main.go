e12cf5112d5f80b0682d89fce9f9598e12918292
ErzhanKorzhan <r.badyrtenov1@g.nsu.ru>
2025-05-12 21:00:27 +0700

    all

--- 23216/r.badyrtenov1/lab13_2/main.go
+++ 23216/r.badyrtenov1/lab13_2/main.go
@@ -5,32 +5,31 @@ import (
 	"sync"
 )
 
-func printTexts(s string, lock *sync.Mutex, turn *bool, myTurn bool) {
+func printTexts(s string, cond *sync.Cond, turn *bool, myTurn bool) {
 	for i := 1; i <= 10; i++ {
-		for {
-			lock.Lock()
-			if *turn == myTurn {
-				fmt.Println(s, i)
-				*turn = !(*turn)
-				lock.Unlock()
-				break
-			}
-			lock.Unlock()
+		cond.L.Lock()
+		for *turn != myTurn {
+			cond.Wait()
 		}
+		fmt.Println(s, i)
+		*turn = !(*turn)
+		cond.L.Unlock()
+		cond.Signal()
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
 	var lock sync.Mutex
+	cond := sync.NewCond(&lock)
 	flagThread := false
 
 	wg.Add(1)
 	go func() {
-		printTexts("Child Thread", &lock, &flagThread, true)
+		printTexts("Child Thread", cond, &flagThread, true)
 		wg.Done()
 	}()
 
-	printTexts("Parent Thread", &lock, &flagThread, false)
+	printTexts("Parent Thread", cond, &flagThread, false)
 	wg.Wait()
 }
