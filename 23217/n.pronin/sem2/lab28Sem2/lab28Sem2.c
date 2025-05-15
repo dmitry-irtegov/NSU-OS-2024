4ad1ff22e9e945ba4346605d382a63f2b5a7542e
Nikolay Pronin <n.pronin@g.nsu.ru>
2025-05-15 15:42:47 +0700

    lab28

--- 23217/n.pronin/sem2/lab28Sem2/lab28Sem2.c
+++ 23217/n.pronin/sem2/lab28Sem2/lab28Sem2.c
@@ -2,77 +2,139 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <netdb.h>
 #include <fcntl.h>
-#include <iconv.h>
-#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <ctype.h>
 
-#define BUF 8192
+#define BUFFER_SIZE 4096
+#define LINES_PER_PAGE 25
 
-void error(const char *msg) { perror(msg); exit(1); }
-
-char *my_strcasestr(const char *haystack, const char *needle) {
-    for (; *haystack; haystack++) {
-        if (strncasecmp(haystack, needle, strlen(needle)) == 0)
-            return (char *)haystack;
+int parse_url(const char *url, char *host, char *path) {
+    const char *p;
+    if (strncmp(url, "http://", 7) == 0) {
+        p = url + 7;
+    } else {
+        p = url;
     }
-    return NULL;
+
+    const char *slash = strchr(p, '/');
+    if (slash) {
+        strncpy(host, p, slash - p);
+        host[slash - p] = '\0';
+        strcpy(path, slash);
+    } else {
+        strcpy(host, p);
+        strcpy(path, "/");
+    }
+
+    return 0;
 }
 
-char *get_charset(const char *hdrs) {
-    char *p = my_strcasestr(hdrs, "charset=");
-    if (!p) return "UTF-8";
-    p += 8;
-    static char cs[32];
-    sscanf(p, "%31[^;\r\n]", cs);
-    return cs;
+int connect_to_host(const char *host) {
+    struct addrinfo hints, *res, *rp;
+    int sockfd;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(host, "80", &hints, &res) != 0) {
+        perror("getaddrinfo");
+        return -1;
+    }
+
+    for (rp = res; rp != NULL; rp = rp->ai_next) {
+        sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (sockfd == -1) continue;
+        if (connect(sockfd, rp->ai_addr, rp->ai_addrlen) == 0) break;
+        close(sockfd);
+    }
+
+    freeaddrinfo(res);
+    if (rp == NULL) return -1;
+    return sockfd;
 }
 
-void convert(const char *from, const char *in, size_t inlen) {
-    iconv_t cd = iconv_open("UTF-8", from);
-    if (cd == (iconv_t)-1) { fwrite(in, 1, inlen, stdout); return; }
-    char out[BUF*2], *outp = out;
-    size_t outlen = sizeof(out);
-    char *inp = (char *)in;
-    iconv(cd, &inp, &inlen, &outp, &outlen);
-    fwrite(out, 1, sizeof(out)-outlen, stdout);
-    iconv_close(cd);
-}
-
-int main(int argc, char **argv) {
-    if (argc != 2) { fprintf(stderr, "Usage: %s <url>\n", argv[0]); return 1; }
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        const char *usage = "Usage: ./client http://host/path\n";
+        write(STDERR_FILENO, usage, strlen(usage));
+        return 1;
+    }
 
     char host[256], path[1024];
-    memset(path, 0, sizeof(path));
-    sscanf(argv[1], "http://%255[^/]%1023s", host, path);
-    if (path[0] == '\0') strcpy(path, "/");
+    parse_url(argv[1], host, path);
+    int sockfd = connect_to_host(host);
+    if (sockfd < 0) {
+        perror("connect");
+        return 1;
+    }
 
-    struct hostent *he = gethostbyname(host);
-    if (!he) error("host");
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in sa = { .sin_family=AF_INET, .sin_port=htons(80) };
-    memcpy(&sa.sin_addr, he->h_addr, he->h_length);
-    connect(s, (struct sockaddr*)&sa, sizeof sa);
+    char request[2048];
+    snprintf(request, sizeof(request),
+             "GET %s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n",
+             path, host);
+    write(sockfd, request, strlen(request));
 
-    char req[1024];
-    snprintf(req, sizeof(req), "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n", path, host);
-    write(s, req, strlen(req));
+    fd_set readfds;
+    char buffer[BUFFER_SIZE];
+    int lines_printed = 0;
+    int in_body = 0;
 
-    char buf[BUF], *body;
-    int n = recv(s, buf, BUF-1, 0); buf[n] = 0;
+    while (1) {
+        FD_ZERO(&readfds);
+        FD_SET(sockfd, &readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+        int maxfd = sockfd > STDIN_FILENO ? sockfd : STDIN_FILENO;
 
-    char *hdr_end = strstr(buf, "\r\n\r\n");
-    if (!hdr_end) error("no headers");
-    *hdr_end = 0;
-    char *charset = get_charset(buf);
+        if (select(maxfd + 1, &readfds, NULL, NULL, NULL) < 0) {
+            perror("select");
+            break;
+        }
 
-    body = hdr_end + 4;
-    convert(charset, body, n - (body - buf));
+        if (FD_ISSET(sockfd, &readfds)) {
+            ssize_t n = read(sockfd, buffer, sizeof(buffer) - 1);
+            if (n <= 0) break;
+            buffer[n] = '\0';
 
-    while ((n = read(s, buf, BUF)) > 0)
-        convert(charset, buf, n);
+            char *line = strtok(buffer, "\n");
+            while (line) {
+                if (!in_body) {
+                    if (strcmp(line, "\r") == 0 || strcmp(line, "") == 0) {
+                        in_body = 1;
+                    }
+                } else {
+                    size_t len = strlen(line);
+                    write(STDOUT_FILENO, line, len);
+                    write(STDOUT_FILENO, "\n", 1);
+                    lines_printed++;
+                    if (lines_printed >= LINES_PER_PAGE) {
+                        const char *prompt = "\nPress space to scroll down\n";
+                        write(STDOUT_FILENO, prompt, strlen(prompt));
+                        while (1) {
+                            FD_ZERO(&readfds);
+                            FD_SET(STDIN_FILENO, &readfds);
+                            select(STDIN_FILENO + 1, &readfds, NULL, NULL, NULL);
+                            if (FD_ISSET(STDIN_FILENO, &readfds)) {
+                                char c;
+                                read(STDIN_FILENO, &c, 1);
+                                if (c == ' ') break;
+                            }
+                        }
+                        lines_printed = 0;
+                    }
+                }
+                line = strtok(NULL, "\n");
+            }
+        }
+    }
 
-    close(s);
+    close(sockfd);
     return 0;
 }
