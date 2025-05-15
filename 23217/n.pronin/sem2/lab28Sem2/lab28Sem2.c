11e65db551141edb608bdd0140d46f12b603a7fb
Nikolay Pronin <n.pronin@g.nsu.ru>
2025-05-15 14:36:15 +0700

    lab28

--- 23217/n.pronin/sem2/lab28Sem2/lab28Sem2.c
+++ 23217/n.pronin/sem2/lab28Sem2/lab28Sem2.c
@@ -2,82 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <netdb.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <sys/select.h>
-#include <termios.h>
-#include <signal.h>
 #include <iconv.h>
 
-#define PORT 80
 #define BUFFER_SIZE 4096
 #define LINES_PER_PAGE 25
 
-struct termios orig_termios;
-
-void disable_raw_mode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
-
-void handle_signal(int sig) {
-    disable_raw_mode();
-    printf("\n[Exiting on signal %d]\n", sig);
+void error(const char *msg) {
+    perror(msg);
     exit(1);
 }
 
-void setup_signal_handlers() {
-    signal(SIGINT, handle_signal);
-    signal(SIGTERM, handle_signal);
-    signal(SIGHUP, handle_signal);
-}
-
-void enable_raw_mode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disable_raw_mode);
-    struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ICANON | ECHO);
-    raw.c_cc[VMIN] = 1;
-    raw.c_cc[VTIME] = 0;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-
-char *convert_encoding(const char *input, const char *from_charset, const char *to_charset) {
-    iconv_t cd = iconv_open(to_charset, from_charset);
-    if (cd == (iconv_t)(-1)) {
-        perror("iconv_open");
-        return NULL;
-    }
-
-    size_t inbytesleft = strlen(input);
-    size_t outbytesleft = inbytesleft * 4;
-
-    char *output = malloc(outbytesleft + 1);
-    if (!output) {
-        iconv_close(cd);
-        return NULL;
-    }
-
-    const char *inbuf = input;  // input не изменяем
-    const char **pinbuf = &inbuf; // указатель на константный указатель
-
-    char *outptr = output;
-
-    if (iconv(cd, (char **)pinbuf, &inbytesleft, &outptr, &outbytesleft) == (size_t)-1) {
-        perror("iconv");
-        free(output);
-        iconv_close(cd);
-        return NULL;
-    }
-
-    *outptr = '\0';
-    iconv_close(cd);
-    return output;
-}
-
+// Парсинг URL: разбиваем на хост и путь
 void parse_url(const char *url, char *host, char *path) {
-    const char *p = strstr(url, "http://");
-    if (p) url += strlen("http://");
+    if (strncmp(url, "http://", 7) == 0)
+        url += 7;
 
     const char *slash = strchr(url, '/');
     if (slash) {
@@ -90,119 +33,150 @@ void parse_url(const char *url, char *host, char *path) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s http://host/path\n", argv[0]);
-        exit(1);
+// Создание TCP-соединения по хосту
+int create_connection(const char *host) {
+    struct hostent *server = gethostbyname(host);
+    if (!server) error("No such host");
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("Error opening socket");
+
+    struct sockaddr_in serv_addr = {0};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(80);
+    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("Connection failed");
+
+    return sockfd;
+}
+
+// Отправка HTTP GET-запроса
+void send_request(int sockfd, const char *host, const char *path) {
+    char request[1024];
+    snprintf(request, sizeof(request),
+             "GET %s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n",
+             path, host);
+    write(sockfd, request, strlen(request));
+}
+
+// Преобразование из Windows-1251 в UTF-8
+size_t convert_encoding(char *input, size_t input_len, char *output, size_t output_len) {
+    iconv_t cd = iconv_open("UTF-8", "WINDOWS-1251");
+    if (cd == (iconv_t)(-1)) {
+        perror("iconv_open");
+        return 0;
     }
 
-    setup_signal_handlers();
+    const char *in_buf_const = input;
+    char *in_buf = (char *)in_buf_const;
+    char *out_buf = output;
+    size_t in_bytes_left = input_len;
+    size_t out_bytes_left = output_len;
+
+    size_t res = iconv(cd, (const char **)&in_buf, &in_bytes_left, &out_buf, &out_bytes_left);
+    if (res == (size_t)-1) {
+        perror("iconv");
+    }
+
+    iconv_close(cd);
+    return output_len - out_bytes_left;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <URL>\n", argv[0]);
+        exit(1);
+    }
 
     char host[256], path[1024];
     parse_url(argv[1], host, path);
 
-    struct hostent *server = gethostbyname(host);
-    if (!server) {
-        perror("gethostbyname");
-        exit(1);
-    }
+    int sockfd = create_connection(host);
+    send_request(sockfd, host, path);
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        perror("socket");
-        exit(1);
-    }
-
-    struct sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    memcpy(&serv_addr.sin_addr.s_addr, server->h_addr, server->h_length);
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("connect");
-        close(sock);
-        exit(1);
-    }
-
-    char request[1024];
-    int len = snprintf(request, sizeof(request),
-                       "GET %s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n",
-                       path, host);
-    if (len >= sizeof(request)) {
-        fprintf(stderr, "Request too long!\n");
-        exit(1);
-    }
-
-    send(sock, request, strlen(request), 0);
-    enable_raw_mode();
-
+    fd_set read_fds;
     char buffer[BUFFER_SIZE];
-    int line_count = 0;
+    char leftover[BUFFER_SIZE] = {0};
+    int lines_printed = 0;
+    int paused = 0;
     int header_parsed = 0;
-    int use_iconv = 0;
+
+    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
 
     while (1) {
-        fd_set readfds;
-        FD_ZERO(&readfds);
-        FD_SET(sock, &readfds);
-        FD_SET(STDIN_FILENO, &readfds);
+        FD_ZERO(&read_fds);
+        FD_SET(sockfd, &read_fds);
+        FD_SET(STDIN_FILENO, &read_fds);
+        int maxfd = sockfd > STDIN_FILENO ? sockfd : STDIN_FILENO;
 
-        int maxfd = (sock > STDIN_FILENO ? sock : STDIN_FILENO) + 1;
-        int ready = select(maxfd, &readfds, NULL, NULL, NULL);
-        if (ready < 0) {
-            perror("select");
-            break;
-        }
+        int activity = select(maxfd + 1, &read_fds, NULL, NULL, NULL);
+        if (activity < 0) error("select");
 
-        if (FD_ISSET(sock, &readfds)) {
-            int bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
-            if (bytes <= 0) break;
-            buffer[bytes] = '\0';
+        // Чтение с сокета
+        if (FD_ISSET(sockfd, &read_fds) && !paused) {
+            int n = read(sockfd, buffer, sizeof(buffer));
+            if (n <= 0) break;
 
-            char *content = buffer;
+            char *data = buffer;
+            int data_len = n;
 
             if (!header_parsed) {
                 char *header_end = strstr(buffer, "\r\n\r\n");
                 if (header_end) {
-                    *header_end = '\0';
-                    if (strstr(buffer, "charset=windows-1251") || strstr(buffer, "charset=WINDOWS-1251")) {
-                        use_iconv = 1;
-                    }
-                    content = header_end + 4;
                     header_parsed = 1;
+                    data = header_end + 4;
+                    data_len = n - (data - buffer);
                 } else {
                     continue;
                 }
             }
 
-            char *line = strtok(content, "\n");
+            char utf8_buffer[BUFFER_SIZE * 2] = {0};
+            size_t utf8_len = convert_encoding(data, data_len, utf8_buffer, sizeof(utf8_buffer) - 1);
+            utf8_buffer[utf8_len] = '\0';
+
+            char combined[BUFFER_SIZE * 3];
+            snprintf(combined, sizeof(combined), "%s%s", leftover, utf8_buffer);
+
+            // Построчный вывод
+            char *saveptr;
+            char *line = strtok_r(combined, "\n", &saveptr);
+            leftover[0] = '\0';
+
             while (line) {
-                char *output = use_iconv ? convert_encoding(line, "WINDOWS-1251", "UTF-8") : NULL;
-                printf("%s\n", output ? output : line);
-                if (output) free(output);
-
-                line_count++;
-                if (line_count >= LINES_PER_PAGE) {
-                    printf("\n-- Press space to scroll down --\n");
+                printf("%s\n", line);
+                fflush(stdout);
+                lines_printed++;
+                if (lines_printed >= LINES_PER_PAGE) {
+                    printf("Press space to scroll down...\n");
                     fflush(stdout);
-                    while (1) {
-                        FD_ZERO(&readfds);
-                        FD_SET(STDIN_FILENO, &readfds);
-                        select(STDIN_FILENO + 1, &readfds, NULL, NULL, NULL);
-                        char ch;
-                        read(STDIN_FILENO, &ch, 1);
-                        if (ch == ' ') {
-                            line_count = 0;
-                            break;
-                        }
-                    }
+                    paused = 1;
+                    break;
                 }
+                line = strtok_r(NULL, "\n", &saveptr);
+            }
 
-                line = strtok(NULL, "\n");
+            // Сохраняем остаток, если есть
+            if (line == NULL && saveptr && strlen(saveptr) < sizeof(leftover)) {
+                strncpy(leftover, saveptr, sizeof(leftover) - 1);
+                leftover[sizeof(leftover) - 1] = '\0';
+            }
+        }
+
+        // Обработка клавиши
+        if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+            char ch;
+            if (read(STDIN_FILENO, &ch, 1) > 0) {
+                if (paused && ch == ' ') {
+                    lines_printed = 0;
+                    paused = 0;
+                }
             }
         }
     }
 
-    close(sock);
+    close(sockfd);
     return 0;
 }
