05ebe22dd8356618ca7a66582745682aa7ad3b39
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-05-20 15:50:47 +0700

    lab2_4

--- 23216/Faranosov/sem2/lab4/lab4.c
+++ 23216/Faranosov/sem2/lab4/lab4.c
@@ -39,7 +39,7 @@ int main() {
 
 	int arg = 0;
 
-	checkRes = pthread_create(&thread, &attr, thread_funk, (void*)arg);
+	checkRes = pthread_create(&thread, &attr, thread_funk, arg);
 	if (checkRes != 0) {
 		handler("create", checkRes);
 	}
