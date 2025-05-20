af14580654d7d163201c3bc71d1a23350055a32e
Dmitrii Faranosov <d.faranosov@g.nsu.ru>
2025-05-20 10:47:18 +0700

    lab2_4

--- 23216/Faranosov/sem2/lab4/lab4.c
+++ 23216/Faranosov/sem2/lab4/lab4.c
@@ -1,32 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
+#include "stdlib.h"
+#include "pthread.h"
+#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
 
-void* func(void* param) {
-	char* str = param;
-	printf("%s\n", str);
-	return NULL;
+
+void* thread_funk() {
+
+	for (long long  i = 0;; i++) {
+		printf("%lld\n", i);
+	}
+	
+	pthread_exit(NULL);
 }
 
+void handler(char str[], int num) {
+	char buf[256];
+	strerror_r(num, buf, 256);
+	fprintf(stderr, "%s error: %s", str, buf);
+	exit(1);
+}
 
 int main() {
 	pthread_t thread;
-	int res;
+	pthread_attr_t attr;
 
-	res = pthread_create(&thread, NULL, func, "abc");
-	if (res != 0) {
-		perror("create error");
+	int checkRes = 0;
+
+	checkRes = pthread_attr_init(&attr);
+	if (checkRes != 0) {
+		handler("attr_init", checkRes);
+	}
+
+	int arg = 0;
+
+	checkRes = pthread_create(&thread, &attr, thread_funk, (void*)arg);
+	if (checkRes != 0) {
+		handler("create", checkRes);
+	}
+
+
+	checkRes = pthread_attr_destroy(&attr);
+	if (checkRes != 0) {
+		handler("destroy", checkRes);
+	}
+
+	
+	checkRes = sleep(2);
+	if (checkRes != 0) {
+		fprintf(stderr, "sleep error: time elapsed == %u", checkRes);
 		exit(EXIT_FAILURE);
 	}
 
-	sleep(2);
-	res = pthread_cancel(thread);
-	if (res != 0) {
-		perror("cancel error");
-		exit(EXIT_FAILURE);
+	checkRes = pthread_cancel(thread);
+	if (checkRes != 0) {
+		handler("cancel", checkRes);
 	}
 
-
+	
 	exit(EXIT_SUCCESS);
 }
