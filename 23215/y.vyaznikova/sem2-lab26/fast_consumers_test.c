152137970097d8c5ec390e5670c2442577fb9860
YuliaVyaznikova <twthlmn898@gmail.com>
2025-05-22 14:06:23 +0700

    23215 y.vyaznikova sem2-lab26 (#1228)
    * on cond
    
    * style fixed
    
    * message changed
    
    * str about sending message added

---
+++ 23215/y.vyaznikova/sem2-lab26/fast_consumers_test.c
@@ -0,0 +1,63 @@
+#include <assert.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#if defined(USE_SEMAPHORES)
+#include "task25_sem.h"
+#else
+#include "task25_cond.h"
+#endif
+
+void my_assert(int expr) {
+    assert(expr);
+}
+
+queue q;
+
+void* producer(void* arg) {
+    int putted;
+    char buff[100];
+    int msg_num = 0;
+    do {
+        sprintf(buff, "message %d from producer %lu", msg_num++, (unsigned long) arg);
+        printf("producer %lu sent message %d\n", (unsigned long) arg, msg_num - 1);
+        putted = mymsgput(&q, buff);
+        usleep(500000);
+    } while (putted > 0);
+    return NULL;
+}
+
+void* consumer(void* arg) {
+    int count;
+    char buff[100];
+    while ((count = mymsgget(&q, buff, 100)) > 0) { 
+        buff[count] = 0;
+        printf("consumer %lu has received \"%s\"\n", (unsigned long) arg, buff);
+    };
+    return NULL;
+}
+
+int main() {
+    mymsginit(&q);
+    
+    pthread_t prod1, prod2, cons1, cons2;
+
+    my_assert(0 == pthread_create(&prod1, NULL, producer, (void*) 1));
+    my_assert(0 == pthread_create(&prod2, NULL, producer, (void*) 2));
+    my_assert(0 == pthread_create(&cons1, NULL, consumer, (void*) 1));
+    my_assert(0 == pthread_create(&cons2, NULL, consumer, (void*) 2));
+    
+    sleep(10);
+
+    mymsqdrop(&q);
+
+    pthread_join(prod1, NULL);
+    pthread_join(prod2, NULL);
+    pthread_join(cons1, NULL);
+    pthread_join(cons2, NULL);
+
+    mymsgdestroy(&q);    
+    return 0;
+}
