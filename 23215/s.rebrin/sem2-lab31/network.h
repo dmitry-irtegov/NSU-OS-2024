884ecd4e97a9db19740a38937127c0165b21a1fc
Sergei Rebrin <s.rebrin@g.nsu.ru>
2025-05-22 11:29:20 +0700

    description to client

--- 23215/s.rebrin/sem2-lab31/network.h
+++ 23215/s.rebrin/sem2-lab31/network.h
@@ -25,25 +25,25 @@ typedef struct cache {
 } cache;
 
 typedef struct client {
-    int cli_fd;
-    int inet_fd;
-    char* host;
-    int tot;
-    int len;
-    int header_len; 
-    int writing_to_client;
-    char buffer[4096];
-    int writing;
-    int writing_to_client_total;
-    cache* cur_cache;
-    data* cur_data;
-    time_t last_activity;
-    int using_cache;
-    int caching;
-    int headers_len;
-    int collect_headers;
-    char* headers_collectors;
-    struct client* next;
+    int cli_fd;              /* File descriptor for the client socket */
+    int inet_fd;             /* File descriptor for the socket connected to the target host/server */
+    char* host;              /* Pointer to a string containing the hostname of the target server */
+    int tot;                 /* Total number of bytes read from the target server */
+    int len;                 /* Expected length of the response body (content length) parsed from HTTP headers */
+    int header_len;          /* Length of the HTTP headers in the response (up to \r\n\r\n) */
+    int writing_to_client;   /* Number of bytes already written to the client socket in the current write operation */
+    char buffer[4096];       /* Buffer to store data read from the target server or client */
+    int writing;             /* Flag indicating whether the client is currently processing a request (1 - active, 0 - finished) */
+    int writing_to_client_total; /* Total number of bytes to be written to the client socket in the current operation */
+    cache* cur_cache;        /* Pointer to the current cache entry associated with the client's request */
+    data* cur_data;          /* Pointer to the current data block from the cache (used when reading from cache) */
+    time_t last_activity;    /* Timestamp of the last activity from the client (for timeout checking) */
+    int using_cache;         /* Flag indicating whether the client is using cached data (1 - yes, 0 - no) */
+    int caching;             /* Flag indicating whether the current response is being cached (1 - yes, 0 - no) */
+    int headers_len;         /* Total length of the HTTP headers (including \r\n\r\n), used to determine the start of the body */
+    int collect_headers;     /* Counter of bytes collected in headers_collectors, or -1 if header collection is complete */
+    char* headers_collectors;/* Buffer for accumulating HTTP headers until fully parsed (e.g., until \r\n\r\n is found) */
+    struct client* next;     /* Pointer to the next client in the linked list */
 } client;
 
 void con_to_host(int* sockfd, char* host);
