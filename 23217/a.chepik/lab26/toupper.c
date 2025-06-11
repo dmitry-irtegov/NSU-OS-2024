95f2ff445d2df521959bf0e1735c7b78f3b8cb83
Coilyun <90952801+Coilyun@users.noreply.github.com>
2025-06-11 13:42:15 +0700

    Delete 23217 directory

--- 23217/a.chepik/lab26/toupper.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#define STRING_SIZE 12
-
-int main() {
-    char test_string[STRING_SIZE] = "test_string";
-    write(STDOUT_FILENO, test_string, STRING_SIZE);
-}
